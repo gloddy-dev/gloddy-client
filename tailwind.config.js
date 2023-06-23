36817d4f7a96d88ab94f4f65dda7c3559c276510
Kuesung Park <gueit214@naver.com>
2023-06-23 18:51:04 +0900

    feat : tailwindconfig 초기 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const { colors } = require('./style/theme/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: { colors },
   },
-  plugins: [],
+  // plugins: [require('@tailwindcss/forms'), require('@headlessui/tailwindcss')],
 }
 
