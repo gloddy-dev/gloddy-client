d533a120730ab826c85a8dfd02d3acdb7ef43341
Kuesung Park <gueit214@naver.com>
2023-06-25 12:18:15 +0900

    feat : 버튼 공통 컴포넌트 생성

---
+++ components/common/Button.tsx
@@ -0,0 +1,17 @@
+interface ButtonProps {
+  text: string;
+  onClick?: () => void;
+  type?: "default" | "login";
+}
+
+export default function Button({
+  text,
+  onClick,
+  type = "default",
+}: ButtonProps) {
+  return (
+    <div className="w-full bg-main h-[3.75rem] rounded-xl text-center flex justify-center items-center text-white font-bold">
+      {text}
+    </div>
+  );
+}
