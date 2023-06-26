4f0f8c3cf729ac7f786b15cd06815ca8e1beaeaa
Kuesung Park <gueit214@naver.com>
2023-06-26 18:19:57 +0900

    feat : 버튼 컴포넌트 props에 따라 색 다르게

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,17 +1,17 @@
 interface ButtonProps {
   text: string;
   onClick?: () => void;
-  type?: "default" | "login";
+  color?: "blue" | "orange";
 }
+import { clsx } from "clsx";
 
-export default function Button({
-  text,
-  onClick,
-  type = "default",
-}: ButtonProps) {
+export default function Button({ text, onClick, color = "blue" }: ButtonProps) {
   return (
     <div
-      className="w-full bg-main h-[3.75rem] rounded-xl text-center flex justify-center items-center text-white font-bold"
+      className={clsx(
+        "w-full h-[3.75rem] rounded-xl text-center flex justify-center items-center text-white font-bold",
+        color === "blue" ? "bg-blue" : "bg-orange"
+      )}
       onClick={onClick}
     >
       {text}
