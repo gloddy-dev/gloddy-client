f53e670aa1e71a9f00eebb79f0eb9db9547b21fd
Kuesung Park <gueit214@naver.com>
2023-06-26 18:50:29 +0900

    feat : layout 및 global.css 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="flex h-screen w-screen justify-center bg-slate-50">
+        <div className="relative h-full w-full max-w-[26.25rem] overflow-y-scroll bg-white px-[1.5rem]">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
