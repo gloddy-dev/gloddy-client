7bea7610e5e4c6e406b55d93c7b731492bc871a6
Kuesung Park <gueit214@naver.com>
2023-06-26 18:19:44 +0900

    refactor : colors 한 계층 내림

--- style/theme/colors.ts
+++ style/theme/colors.ts
@@ -1,14 +1,10 @@
 export const colors = {
-  main: "#1249FC",
+  blue: "#1249FC",
   sub: "#ABC1FC",
-  point: {
-    yellow: "#FFE605",
-    orange: "#D7533E",
-  },
-  text: {
-    black: "#1A1A1A",
-    gray: "3a3a3a",
-  },
+  yellow: "#FFE605",
+  orange: "#D7533E",
+  black: "#1A1A1A",
+  gray: "3a3a3a",
   info: "#AAAAAA",
   non: "#CDCDCD",
   white: "#F7F7F7",
