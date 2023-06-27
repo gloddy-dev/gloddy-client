4b90858087a5e6eba88f8c0780cae075c8fb1687
Kuesung Park <gueit214@naver.com>
2023-06-27 15:58:06 +0900

    refactor : 버튼 컴포넌트 button/link에 따라 구분

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,22 +1,42 @@
 import { clsx } from 'clsx';
+import Link from 'next/link';
 import { ButtonHTMLAttributes } from 'react';
+
+type ButtonColor = 'blue' | 'orange';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
-  color?: 'blue' | 'orange';
+  color?: ButtonColor;
+  onClick?: () => void;
+  href?: string;
 }
 
 export default function Button(props: ButtonProps) {
-  const { text, color = 'blue', ...rest } = props;
-  return (
+  const { text, color = 'blue', onClick, href, ...rest } = props;
+
+  return !!href ? (
+    <div
+      className={clsx(
+        'w-full h-[3.75rem] rounded-xl text-center flex justify-center items-center ',
+        `${color === 'blue' ? 'bg-blue' : 'bg-orange'}`
+      )}
+    >
+      <Link href={href} className="text-white font-bold">
+        {text}
+      </Link>
+    </div>
+  ) : !!onClick ? (
     <button
       className={clsx(
         'w-full h-[3.75rem] rounded-xl text-center flex justify-center items-center text-white font-bold',
         `${color === 'blue' ? 'bg-blue' : 'bg-orange'}`
       )}
+      onClick={onClick}
       {...rest}
     >
       {text}
     </button>
+  ) : (
+    <div></div>
   );
 }
