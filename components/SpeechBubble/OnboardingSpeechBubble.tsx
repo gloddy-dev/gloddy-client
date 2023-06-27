66823df3157fa6a484ad0479af2b5a9f4070c44e
Kuesung Park <gueit214@naver.com>
2023-06-27 14:30:25 +0900

    feat : 말풍선 컴포넌트 구현

---
+++ components/SpeechBubble/OnboardingSpeechBubble.tsx
@@ -0,0 +1,11 @@
+interface OnboardingSpeechBubbleProps {
+  text: string;
+}
+export default function OnboardingSpeechBubble({ text }: OnboardingSpeechBubbleProps) {
+  return (
+    <div className="flex items-center flex-col h-110">
+      <p className="bg-[#F5F5F5] px-36 py-18 rounded-[10rem] font-700 text-lg">{text}</p>
+      <div className="bg-[#F5F5F5] rotate-45 w-15 h-15 -translate-y-7 rounded"></div>
+    </div>
+  );
+}
