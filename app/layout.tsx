a6b4075a1c138372c159195b1996dacc967d5c79
Salvation_sub <kyman19940214@gmail.com>
2023-06-27 16:14:08 +0900

    refactor: layout 분할 적용 및 metadata 재업로드

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-'use client';
-
 import './globals.css';
-import BottomNavigation from '@/components/common/BottomNavigation';
-import { usePathname } from 'next/navigation';
 
+export const metadata = {
+  title: 'Gloddy',
+  description: '조금 더 믿을 만한 모임을 할 수 있도록 준비했어요!',
+  icons: {
+    icon: { url: '/assets/profile_fill.svg', type: 'image/svg' },
+  },
+};
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const bottomNavPathName = ['/grouping', '/board', '/meeting', '/profile'];
   return (
     <html lang="ko">
       <body className="flex h-screen w-screen justify-center bg-slate-50">
         <div className="relative h-full w-full max-w-[26.25rem] overflow-y-scroll bg-white px-24">
           {children}
         </div>
-        {bottomNavPathName.includes(pathname) && <BottomNavigation />}
       </body>
     </html>
   );
