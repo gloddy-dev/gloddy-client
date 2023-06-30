0aff5e4e8ed2cb2687df0f572a931eaec00a3f8f
Kuesung Park <gueit214@naver.com>
2023-06-30 19:42:55 +0900

    refactor : 상단 네비게이션바 삭제(height값이 늘어남)

--- app/join/layout.tsx
+++ app/join/layout.tsx
@@ -1,12 +1,5 @@
 'use client';
-import TopNavigationBar from '@/components/common/NavigationBar/TopNavigationBar';
 
 export default function GroupingLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="p-20">
-      <TopNavigationBar text="회원가입" />
-      <div className="h-30" />
-      {children}
-    </div>
-  );
+  return <div className="p-20 h-full ">{children}</div>;
 }
