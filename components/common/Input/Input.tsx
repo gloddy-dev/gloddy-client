bb3e666b4158b405020347e522991e98142c9eb8
Kuesung Park <gueit214@naver.com>
2023-06-30 14:02:42 +0900

    refactor : 색 tailwind로 설정

--- components/common/Input/Input.tsx
+++ components/common/Input/Input.tsx
@@ -9,7 +9,7 @@ export default function Input({ placeholder, ...rest }: InputProps) {
   return (
     <input
       placeholder={placeholder}
-      className="bg-[#f5f5f5] text-black outline-none text-16 font-500 pl-23 w-full rounded-lg h-50"
+      className="bg-gray5 text-black outline-none text-16 font-500 pl-23 w-full rounded-lg h-50"
       {...rest}
       type="text"
     />
