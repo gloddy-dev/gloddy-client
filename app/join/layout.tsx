d8cb683fe278280fbd49ac807f8174ad02b95a6a
Kuesung Park <gueit214@naver.com>
2023-06-30 18:18:04 +0900

    refactor : 오른쪽 아이콘 제거(디자인 수정)

--- app/join/layout.tsx
+++ app/join/layout.tsx
@@ -1,14 +1,10 @@
 'use client';
 import TopNavigationBar from '@/components/common/NavigationBar/TopNavigationBar';
-import Image from 'next/image';
 
 export default function GroupingLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="p-20">
-      <TopNavigationBar
-        text="회원가입"
-        right={<Image alt="" src="/assets/alert.svg" width={20} height={30} />}
-      />
+      <TopNavigationBar text="회원가입" />
       <div className="h-30" />
       {children}
     </div>
