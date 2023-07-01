cc86c7ec7ea24d64f5d29e6b7db7ddb6b33835c4
Kuesung Park <gueit214@naver.com>
2023-07-01 11:04:46 +0900

    feat : personalitylist

---
+++ constants/personalityList.ts
@@ -0,0 +1,63 @@
+type Personality = {
+  id: string;
+  text: string;
+};
+
+export const personalityList: Personality[] = [
+  {
+    id: '1',
+    text: '외향적인',
+  },
+  {
+    id: '2',
+    text: '내향적인',
+  },
+  {
+    id: '3',
+    text: '신중한',
+  },
+  {
+    id: '4',
+    text: '친절한',
+  },
+  {
+    id: '5',
+    text: '유머러스한',
+  },
+  {
+    id: '6',
+    text: '낙천적인',
+  },
+  {
+    id: '7',
+    text: '사교적인',
+  },
+  {
+    id: '8',
+    text: '솔직한',
+  },
+  {
+    id: '9',
+    text: '책임감 있는',
+  },
+  {
+    id: '10',
+    text: '차분한',
+  },
+  {
+    id: '11',
+    text: '활동적인',
+  },
+  {
+    id: '12',
+    text: '센스있는',
+  },
+  {
+    id: '13',
+    text: '엉뚱한',
+  },
+  {
+    id: '14',
+    text: '리더십 있는',
+  },
+];
