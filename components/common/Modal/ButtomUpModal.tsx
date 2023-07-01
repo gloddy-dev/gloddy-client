836d88fe4d5fd665f44f5c95a48b169fffd76a66
Kuesung Park <gueit214@naver.com>
2023-07-01 11:03:11 +0900

    feat : BottomUpModal 구현

---
+++ components/common/Modal/ButtomUpModal.tsx
@@ -0,0 +1,10 @@
+import ModalWrapper from './ModalWrapper';
+
+interface ButtomUpModalProps {}
+export default function ButtomUpModal() {
+  return (
+    <div>
+      <ModalWrapper />
+    </div>
+  );
+}
---
+++ components/common/Modal/ModalWrapper.tsx
@@ -0,0 +1,5 @@
+export default function ModalWrapper() {
+  return (
+    <div className="fixed top-0 left-1/2 opacity-60 bg-black max-w-[26.25rem] -translate-x-1/2 w-full h-full overflow-y-sroll z-10" />
+  );
+}
