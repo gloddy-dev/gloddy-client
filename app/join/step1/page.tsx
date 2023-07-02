95d602c8d0c23191d57cfdd37f6bb3247b81b2e3
Kuesung Park <gueit214@naver.com>
2023-07-02 13:43:08 +0900

    refactor : 간격 일괄 적용

--- app/join/step1/page.tsx
+++ app/join/step1/page.tsx
@@ -78,7 +78,6 @@ export default function Step1Page() {
       <section>
         <TitleTextMessage text={`휴대폰 번호를\n인증해주세요`} />
       </section>
-      <div className="h-30" />
 
       <form onSubmit={handleSubmit(onSubmitPhoneNumber)}>
         <AuthInput
