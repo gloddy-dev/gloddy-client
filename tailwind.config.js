eb7ab67b1495678f4b8a1a419c0a8475c07ef6b9
Kuesung Park <gueit214@naver.com>
2023-07-02 23:22:28 +0900

    feat : animation 모듈화하여 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 const { colors } = require('./style/theme/colors');
 const { pxToRem } = require('./style/theme/spacing');
+const { animations } = require('./style/theme/animations');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      colors, spacing: pxToRem, fontSize: pxToRem, borderRadius: pxToRem,
-      keyframes: {
-        slideUp: {
-          '0%': { transform: 'translateY(50%)' },
-          '100%': { transform: 'translateY(0)' },
-        },
-      },
+      colors,
+      spacing: pxToRem,
+      fontSize: pxToRem,
+      borderRadius: pxToRem,
+      keyframes: animations,
       animation: {
         slideUp: 'slideUp 0.5s ease-in-out',
       },
