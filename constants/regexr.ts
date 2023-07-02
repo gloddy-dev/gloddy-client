0c9699bce6cb59d44bbdeef0c99edcb9d53ffc8c
Kuesung Park <gueit214@naver.com>
2023-07-02 16:37:31 +0900

    refactor : 정규표현식 수정

--- constants/regexr.ts
+++ constants/regexr.ts
@@ -1,6 +1,6 @@
 const regexr = {
   phoneNumber: /^010 - \d{4} - \d{4}$/,
-  email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z]*)@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/,
+  email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z]*)@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.ac.kr$/,
 };
 
 export default regexr;
