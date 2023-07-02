86d5fd636d50461ae9d4bdeb3b6568bbec1e4107
Kuesung Park <gueit214@naver.com>
2023-07-02 13:40:59 +0900

    feat : ...rest 추가

--- components/common/Checkbox/CircleCheckbox.tsx
+++ components/common/Checkbox/CircleCheckbox.tsx
@@ -4,10 +4,10 @@ interface CircleCheckboxProps {
   text?: React.ReactNode;
   checked?: boolean;
 }
-export default function CircleCheckbox({ text, checked = false }: CircleCheckboxProps) {
+export default function CircleCheckbox({ text, checked = false, ...rest }: CircleCheckboxProps) {
   const checkboxImageAsset = `/assets/checkbox_circle${checked ? '_checked' : ''}.svg`;
   return (
-    <div className="flex">
+    <div className="flex" {...rest}>
       <Image alt="checkbox" src={checkboxImageAsset} width={15} height={30} className="mx-10" />
       {text}
     </div>
