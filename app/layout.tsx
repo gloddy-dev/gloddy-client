538ffbb2a8dd3e075e3d9a5188f8e7477d72a11d
Kuesung Park <gueit214@naver.com>
2023-07-03 17:03:27 +0900

    feat : MetaData 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,45 @@
 import './globals.css';
 
-export const metadata = {
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
   title: 'Gloddy',
   description: '조금 더 믿을 만한 모임을 할 수 있도록 준비했어요!',
+  openGraph: {
+    title: 'Gloddy',
+    description: '조금 더 믿을 만한 모임을 할 수 있도록 준비했어요!',
+    url: '', // 웹사이트 URL
+    siteName: 'Gloddy',
+    images: [
+      {
+        url: '', // 이미지 URL
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: 'ko-KR',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: 'Gloddy',
+    card: 'summary_large_image',
+  },
   icons: {
-    icon: { url: '/assets/profile_fill.svg', type: 'image/svg' },
+    shortcut: '/favicon.ico',
   },
 };
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
