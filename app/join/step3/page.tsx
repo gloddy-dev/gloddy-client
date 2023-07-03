ea0a3f2a9734b03c16bca3245845db3c6c471b3c
Kuesung Park <gueit214@naver.com>
2023-07-03 11:24:20 +0900

    feat : routing 추가

--- app/join/step3/page.tsx
+++ app/join/step3/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import clsx from 'clsx';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
@@ -17,6 +18,7 @@ type Inputs = {
   certificateNumber: number;
 };
 export default function Step3Page() {
+  const router = useRouter();
   const {
     register,
     formState: { errors },
@@ -26,58 +28,72 @@ export default function Step3Page() {
 
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-  const handleSubmitEmail: SubmitHandler<Inputs> = (data: Inputs) => {
+  const onSubmitEmail: SubmitHandler<Inputs> = (data: Inputs) => {
     console.log(data.email);
-    // 인증번호 전송
     setIsModalOpen(true);
+    // 인증번호 전송
   };
-  const handleSubmitCertificateNumber: SubmitHandler<Inputs> = (data: Inputs) => {
+  const onSubmitCertificateNumber: SubmitHandler<Inputs> = (data: Inputs) => {
     console.log(data.certificateNumber);
     // 인증번호 확인
-    setIsModalOpen(false);
+    router.push('/join/step4');
   };
 
   return (
     <div className="relative h-full">
-      <BottomUpModal
-        isModalOpen={isModalOpen}
-        setIsModalOpen={setIsModalOpen}
-        snap={400}
-        isCloseButton
-      >
-        <section>
-          <p className="text-20 font-700">회원님의 이메일로 </p>
-          <p className="text-20 font-700">인증번호를 전송하였습니다.</p>
-        </section>
-
-        <form onSubmit={handleSubmit(handleSubmitCertificateNumber)}>
-          <section className="my-20">
-            <AuthInput
-              text="인증번호"
-              register={register('certificateNumber', {
-                required: true,
-              })}
-              maxLength={6}
-              type="number"
-            />
-            <div className="p-10 flex justify-between">
-              <p className="underline text-14 text-gray3 ">재전송하기</p>
-              <p className="text-orange">02:59</p>
-            </div>
+      {isModalOpen && (
+        <BottomUpModal
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          snap={400}
+          isCloseButton
+        >
+          <section className="text-20 font-700">
+            <p>회원님의 이메일로 </p>
+            <p>인증번호를 전송하였습니다.</p>
           </section>
 
-          <section>
-            <Button text="완료" disabled={watch('certificateNumber')?.length < 6} />
-          </section>
-        </form>
-      </BottomUpModal>
+          <form onSubmit={handleSubmit(onSubmitCertificateNumber)}>
+            <section className="my-20">
+              <AuthInput
+                text="인증번호"
+                register={register('certificateNumber', {
+                  required: true,
+                  pattern: {
+                    value: regexr.certificateNumber,
+                    message: '인증 번호를 다시 확인해주세요.',
+                  },
+                })}
+                maxLength={6}
+                type="number"
+              />
+              <div className="p-10 flex justify-between">
+                <p className="underline text-14 text-gray3 ">재전송하기</p>
+                <p className="text-orange">02:59</p>
+              </div>
+            </section>
+
+            <section>
+              <Button
+                text="완료"
+                disabled={
+                  String(watch('certificateNumber'))?.length < 6 ||
+                  watch('certificateNumber') === undefined
+                }
+                type="submit"
+              />
+            </section>
+          </form>
+        </BottomUpModal>
+      )}
+
       <TopNavigationBar text="회원가입" />
 
       <section>
         <TitleTextMessage text={`재학생 인증을 위해\n학교 이메일을 입력해주세요`} />
       </section>
 
-      <form onSubmit={handleSubmit(handleSubmitEmail)}>
+      <form onSubmit={handleSubmit(onSubmitEmail)}>
         <section>
           <AuthInput
             text="ID"
@@ -90,7 +106,6 @@ export default function Step3Page() {
             })}
           />
         </section>
-
         <p
           className={clsx('flex justify-center text-orange text-13 font-500 gap-5 float', {
             invisible: !errors.email,
@@ -132,7 +147,7 @@ export default function Step3Page() {
         </section>
 
         <section className="absolute bottom-0 w-full">
-          <Button text="인증하기" disabled={watch('email')?.length === 0} type="submit" />
+          <Button text="인증하기" type="submit" />
 
           <div className="h-8" />
 
