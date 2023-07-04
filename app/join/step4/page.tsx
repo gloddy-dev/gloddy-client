7d2d88a80f70d17ecf598e0132cb1c41a36b55a0
Kuesung Park <gueit214@naver.com>
2023-07-04 10:27:53 +0900

    feat : 이미지 삽입 구현

--- app/join/step4/page.tsx
+++ app/join/step4/page.tsx
@@ -1,50 +1,25 @@
 'use client';
-import Button from '@/components/common/Button';
-import AuthInput from '@/components/common/Input/AuthInput';
-import TopNavigationBar from '@/components/common/NavigationBar/TopNavigationBar';
 import Image from 'next/image';
 import { useRef, useState } from 'react';
+
+import Button from '@/components/common/Button';
+import CircleImageFrame from '@/components/common/ImageFrame/CircleImageFrame';
+import AuthInput from '@/components/common/Input/AuthInput';
+import TopNavigationBar from '@/components/common/NavigationBar/TopNavigationBar';
 
 export default function Step5Page() {
   const [profileImage, setProfileImage] = useState('');
   const imgRef = useRef();
 
-  const saveImage = () => {
-    const file = imgRef.current.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      setProfileImage(reader.result);
-    };
-  };
-
-  console.log(profileImage);
-
   return (
     <div className="relative h-full ">
       <TopNavigationBar text="회원가입" />
 
-      <section className="flex justify-center items-center relative h-160">
-        <label className="relative" for="profileImage">
-          <div className="w-100 h-100 bg-gray5 rounded-full"></div>
-          <Image
-            alt="plus"
-            src="/assets/plus.svg"
-            width={20}
-            height={30}
-            className="absolute right-5 bottom-5"
-          />
-        </label>
-
-        <input
-          className="hidden"
-          type="file"
-          accept="image/*"
-          id="profileImage"
-          onChange={saveImage}
-          ref={imgRef}
-        />
-      </section>
+      <CircleImageFrame
+        setProfileImage={setProfileImage}
+        imgRef={imgRef}
+        profileImage={profileImage}
+      />
 
       <section className="gap-10 flex flex-col">
         <article className="gap-5 flex flex-col">
