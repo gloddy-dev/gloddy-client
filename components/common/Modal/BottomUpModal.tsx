e2e7ddd6b009606abcbe9372111767fb5be26c1e
Kuesung Park <gueit214@naver.com>
2023-07-04 13:48:29 +0900

    refactor : prettier

--- components/common/Modal/BottomUpModal.tsx
+++ components/common/Modal/BottomUpModal.tsx
@@ -38,13 +38,13 @@ export default function BottomUpModal({
         snapPoints={[snap, 0]}
         initialSnap={0}
         disableDrag={disableDrag}
-        className="animate-slideUp"
+        className="animate-slideUp "
       >
-        <Sheet.Container className="!rounded-t-30">
-          <div className="flex justify-center w-full">
+        <Sheet.Container className="!rounded-t-30 ">
+          <div className="flex justify-center w-full ">
             <div className="relative h-full w-[26.25rem]  bg-white rounded-t-30">
               <Sheet.Content>
-                <div className="p-25">
+                <div className="p-25 ">
                   <Sheet.Header>
                     <div className="relative  justify-center h-50 items-center">
                       {isLeftButton && (
