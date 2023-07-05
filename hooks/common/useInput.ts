3e880c365d7b47ebea3ab4171968cb4a714c1192
Kuesung Park <gueit214@naver.com>
2023-07-05 21:05:44 +0900

    feat : input훅

---
+++ hooks/common/useInput.ts
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+export type InputType = {
+  value: string;
+  onChange: ({ target }: { target: HTMLInputElement | HTMLTextAreaElement }) => void;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const useInput = (defaultValue: string, filter?: (text: string) => string): InputType => {
+  const [value, setValue] = useState(filter ? filter(defaultValue) : defaultValue);
+
+  const onChange = ({ target }: { target: HTMLInputElement | HTMLTextAreaElement }) => {
+    const { value } = target;
+    setValue(filter ? filter(target.value) : value);
+  };
+
+  return { value, onChange, setValue };
+};
+
+export default useInput;
