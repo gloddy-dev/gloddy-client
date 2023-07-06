53dc0317b18b752dd3c620b348e5322563971557
Kuesung Park <gueit214@naver.com>
2023-07-06 14:15:48 +0900

    refactor : 불필요 속성 제거

--- app/grouping/create-meeting/layout.tsx
+++ app/grouping/create-meeting/layout.tsx
@@ -1,21 +1,13 @@
 'use client';
 
-import { SafeArea } from 'antd-mobile';
-
 import TopNavigationBar from '@/components/common/NavigationBar/TopNavigationBar';
 
 export default function GroupingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen  bg-white ">
-      <SafeArea position="top" />
-      <div className="h-full w-full">
-        <div className="px-20 pt-20">
-          <TopNavigationBar isLeft={true} text="모임 개설" />
-        </div>
+    <div className="h-full px-20">
+      <TopNavigationBar isLeft text="모임 개설" />
 
-        <div className="">{children}</div>
-      </div>
-      <SafeArea position="bottom" />
+      {children}
     </div>
   );
 }
