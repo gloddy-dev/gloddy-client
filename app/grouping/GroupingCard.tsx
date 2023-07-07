d5ca7c815dfdf312402723419b7072a6599b6e41
Kuesung Park <gueit214@naver.com>
2023-07-07 15:36:50 +0900

    refactor : padding값 수정

--- app/grouping/GroupingCard.tsx
+++ app/grouping/GroupingCard.tsx
@@ -16,7 +16,7 @@ const GroupingCard = ({ ...props }: GroupingCardProps) => {
   const { title, description, current, total, location, time, onClick } = props;
   return (
     <div
-      className="mb-18 flex h-126 w-full flex-col justify-between rounded-8 bg-white px-43 py-16 pl-14"
+      className="mb-18 flex h-126 w-full flex-col justify-between rounded-8 bg-white px-20 py-16 pl-14"
       onClick={onClick}
     >
       <div className="flex w-full flex-row">
