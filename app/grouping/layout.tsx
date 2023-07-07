4290b2539af23c54e32d430818418a30a6a99a18
Kuesung Park <gueit214@naver.com>
2023-07-07 16:34:10 +0900

    refactor : 불필요 코드 제거

--- app/grouping/layout.tsx
+++ app/grouping/layout.tsx
@@ -1,42 +1,5 @@
 'use client';
 
-import { NavBar, SafeArea } from 'antd-mobile';
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-
-import BottomNavigation from '@/components/common/NavigationBar/BottomNavigation';
-
 export default function GroupingLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const bottomNavPathName = ['/grouping', '/board', '/meeting', '/profile'];
-
-  const navTitle = <div className="flex justify-start text-16 font-700">그루핑</div>;
-  const searchIcon = (
-    <div className="flex justify-end">
-      <Image src="/assets/search_navbar.svg" alt="search" width={15} height={15} />
-    </div>
-  );
-
-  return (
-    <div className="h-full w-full bg-white3">
-      <div>
-        <SafeArea position="top" />
-        {pathname === '/grouping' && (
-          <NavBar
-            left={navTitle}
-            right={searchIcon}
-            backArrow={false}
-            style={{
-              '--height': '57px',
-            }}
-          />
-        )}
-
-        <div>{children}</div>
-
-        {bottomNavPathName.includes(pathname) && <BottomNavigation />}
-        <SafeArea position="bottom" />
-      </div>
-    </div>
-  );
+  return <div className="h-full p-20">{children}</div>;
 }
