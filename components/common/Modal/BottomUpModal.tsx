d06b42af6a6f9fd019206576a115418a81d94639
kang <kangju2000@naver.com>
2023-07-08 22:34:18 +0900

    refactor: BottomUpModal 컴포넌트 리팩토링

--- components/common/Modal/BottomUpModal.tsx
+++ components/common/Modal/BottomUpModal.tsx
@@ -1,19 +1,17 @@
 import Image from 'next/image';
 import { useRef } from 'react';
-import Sheet, { SheetRef } from 'react-modal-sheet';
-
-import ModalWrapper from './ModalWrapper';
+import Sheet, { type SheetRef } from 'react-modal-sheet';
 
 interface BottomUpModalProps {
   isModalOpen: boolean;
-  handleLeftButtonClick?: () => void;
-  onClose: () => void;
-  children: React.ReactNode;
   snap: number;
+  children: React.ReactNode;
+  onClose: () => void;
   disableDrag?: boolean;
-  isLeftButton?: boolean;
   text?: React.ReactNode;
+  isLeftButton?: boolean;
   isRightButton?: boolean;
+  handleLeftButtonClick?: () => void;
 }
 
 export default function BottomUpModal({
@@ -22,66 +20,58 @@ export default function BottomUpModal({
   handleLeftButtonClick,
   onClose,
   snap,
+  text,
   disableDrag = false,
   isLeftButton = false,
-  text,
   isRightButton = false,
 }: BottomUpModalProps) {
   const ref = useRef<SheetRef>();
   const snapTo = (i: number) => ref.current?.snapTo(i);
 
   return (
-    <div>
-      {isModalOpen && <ModalWrapper />}
-      <Sheet
-        ref={ref}
-        isOpen={isModalOpen}
-        onClose={onClose}
-        snapPoints={[snap, 0]}
-        initialSnap={0}
-        disableDrag={disableDrag}
-        className="fixed inset-x-0 m-auto max-w-[26.25rem] animate-slideUp"
-      >
-        <Sheet.Container className="!rounded-t-30">
-          <div className="flex w-full justify-center">
-            <div className="relative h-full w-[26.25rem]  rounded-t-30 bg-white">
-              <Sheet.Content>
-                <div className="p-25">
-                  <Sheet.Header>
-                    <div className="relative h-50 items-center justify-center">
-                      {isLeftButton && (
-                        <Image
-                          alt="close"
-                          src="/assets/arrow_back.svg"
-                          width={10}
-                          height={10}
-                          className="absolute inset-y-0  left-0 m-auto"
-                          onClick={() =>
-                            handleLeftButtonClick ? handleLeftButtonClick() : snapTo(1)
-                          }
-                        />
-                      )}
-                      <div className="flex h-full items-center justify-center">{text}</div>
-                      {isRightButton && (
-                        <Image
-                          alt="close"
-                          src="/assets/close.svg"
-                          width={30}
-                          height={30}
-                          onClick={() => snapTo(1)}
-                          className="absolute inset-y-0 right-0 m-auto"
-                        />
-                      )}
-                      <div />
-                    </div>
-                  </Sheet.Header>
-                  {children}
-                </div>
-              </Sheet.Content>
-            </div>
+    <Sheet
+      ref={ref}
+      isOpen={isModalOpen}
+      onClose={onClose}
+      snapPoints={[snap, 0]}
+      initialSnap={0}
+      disableDrag={disableDrag}
+      className="fixed inset-x-0 m-auto max-w-[28.125rem]"
+      tweenConfig={{
+        ease: 'easeInOut',
+        duration: 0.5,
+      }}
+    >
+      <Sheet.Backdrop className="fixed inset-x-0 m-auto max-w-[28.125rem] !bg-[rgba(0,0,0,0.6)]" />
+      <Sheet.Container className="relative !rounded-t-30 bg-white p-25">
+        <Sheet.Header>
+          <div className="relative h-50 items-center justify-center">
+            {isLeftButton && (
+              <Image
+                alt="close"
+                src="/assets/arrow_back.svg"
+                width={10}
+                height={10}
+                className="absolute inset-y-0  left-0 m-auto"
+                onClick={() => (handleLeftButtonClick ? handleLeftButtonClick() : snapTo(1))}
+              />
+            )}
+            <div className="flex h-full items-center justify-center">{text}</div>
+            {isRightButton && (
+              <Image
+                alt="close"
+                src="/assets/close.svg"
+                width={30}
+                height={30}
+                onClick={onClose}
+                className="absolute inset-y-0 right-0 m-auto"
+              />
+            )}
+            <div />
           </div>
-        </Sheet.Container>
-      </Sheet>
-    </div>
+        </Sheet.Header>
+        <Sheet.Content>{children}</Sheet.Content>
+      </Sheet.Container>
+    </Sheet>
   );
 }
