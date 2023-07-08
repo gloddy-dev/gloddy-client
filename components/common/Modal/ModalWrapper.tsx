a11ebb22495837ea1c452f1b03a7bf5eb5e159ba
kang <kangju2000@naver.com>
2023-07-08 22:33:01 +0900

    refactor: ModalWrapper 수정 및 StrictPropsWithChildren 타입 추가

--- components/common/Modal/ModalWrapper.tsx
+++ components/common/Modal/ModalWrapper.tsx
@@ -1,5 +1,13 @@
-export default function ModalWrapper() {
+interface ModalWrapperProps {
+  children?: React.ReactNode;
+}
+
+export default function ModalWrapper({ children }: ModalWrapperProps) {
   return (
-    <div className="overflow-y-sroll fixed left-1/2 top-0 z-10 h-full w-full max-w-[26.25rem] -translate-x-1/2 bg-black opacity-60" />
+    <div className="fixed left-1/2 top-0 z-10 h-full w-full max-w-[450px] -translate-x-1/2 bg-[rgba(0,0,0,0.4)]">
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+        {children}
+      </div>
+    </div>
   );
 }
