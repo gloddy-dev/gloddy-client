1e234e95486c39dd6086d2da389621bbbd6077a9
Kuesung Park <gueit214@naver.com>
2023-07-09 10:59:19 +0900

    refactor : 불필요한 section 제거

--- app/join/step5/page.tsx
+++ app/join/step5/page.tsx
@@ -1,4 +1,5 @@
 import TopNavigationBar from '@/components/common/NavigationBar/TopNavigationBar';
+import { Spacing } from '@/components/common/Spacing';
 import { AuthTitleTextMessage } from '@/components/TextMessage/AuthTextMessage';
 
 import InputForm from './InputForm';
@@ -8,15 +9,13 @@ export default function Step5Page() {
     <div className="relative h-full ">
       <TopNavigationBar text="회원가입" />
 
-      <section>
-        <AuthTitleTextMessage text={`사용자님의 성격을\n선택해주세요!`} />
-      </section>
+      <AuthTitleTextMessage text={`사용자님의 성격을\n선택해주세요!`} />
 
       <p className="text-14 text-gray2">3개를 선택해주세요.</p>
 
       <InputForm />
 
-      <div className="h-30" />
+      <Spacing size={30} />
     </div>
   );
 }
