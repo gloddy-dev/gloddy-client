937c59ba705549e14987dd9f1f0c0405fea14ecf
Kuesung Park <gueit214@naver.com>
2023-07-10 19:05:13 +0900

    feat : contants에 Kakao sdk url 추가

--- constants/common.ts
+++ constants/common.ts
@@ -1,1 +1,2 @@
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_APP_ENV === 'production';
+export const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false&libraries=services,clusterer`;
