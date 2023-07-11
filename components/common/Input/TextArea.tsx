341e1e26b10911123aeee2bf5db28667c9c565ca
Kuesung Park <gueit214@naver.com>
2023-07-11 13:53:45 +0900

    feat : react-hook-form 추가

--- components/common/Input/TextArea.tsx
+++ components/common/Input/TextArea.tsx
@@ -1,16 +1,19 @@
 'use client';
 
 import { TextareaHTMLAttributes } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 interface InputProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   placeholder: string;
+  register?: UseFormRegisterReturn;
 }
 
-export default function TextArea({ placeholder, ...rest }: InputProps) {
+export default function TextArea({ placeholder, register, ...rest }: InputProps) {
   return (
     <textarea
       placeholder={placeholder}
       className="font-500 h-112 w-full resize-none rounded-lg bg-gray5 px-23 py-14 text-16 text-black outline-none"
+      {...register}
       {...rest}
     />
   );
