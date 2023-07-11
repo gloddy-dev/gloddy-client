1439ff6acc4119e1e1192d92ddec780da29d6159
kang <kangju2000@naver.com>
2023-07-11 22:35:48 +0900

    feat: 게시판 페이지 레이아웃 구현

---
+++ app/grouping/[id]/board/page.tsx
@@ -0,0 +1,30 @@
+import TopNavigationBar from '../TopNavigationBar';
+import TopSection from '../TopSection';
+
+const DETAIL_DUMMY_DATA = {
+  image: '/assets/location.svg',
+  title: 'Let’s go for a walk!',
+  description: 'It’s a group that \n🏃walks around, \n🗣talks, \n🌏and learns languages.',
+  current: '2',
+  total: '4',
+  location: '동대문구 회기동',
+  time: '04.27.FRI 7PM',
+};
+
+export default function GroupingBoardPage({
+  params: { id },
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const { image, title, description, current, total, location, time } = DETAIL_DUMMY_DATA;
+
+  return (
+    <main>
+      <TopNavigationBar />
+      <TopSection id={id} title={title} thumbnailUrl={image} description={description} />
+      <div className="h-982"></div>
+    </main>
+  );
+}
