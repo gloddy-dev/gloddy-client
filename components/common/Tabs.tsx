edc22040225955ee2736850632f054063f8f94f8
kang <kangju2000@naver.com>
2023-07-11 22:32:27 +0900

    feat: Tabs 공통 컴포넌트 구현

---
+++ components/common/Tabs.tsx
@@ -0,0 +1,36 @@
+'use client';
+import clsx from 'clsx';
+import { usePathname, useRouter } from 'next/navigation';
+import type { StrictPropsWithChildren } from '@/types';
+
+export default function Tabs({ children }: StrictPropsWithChildren) {
+  return (
+    <div className="flex h-50 border-collapse border-b-[.0625rem] border-white3">{children}</div>
+  );
+}
+
+interface TabProps {
+  title: string;
+  url: string;
+}
+
+function Tab({ title, url }: TabProps) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const isActive = pathname === url;
+
+  return (
+    <div
+      className={clsx(
+        'flex flex-1 cursor-pointer items-center justify-center text-gray7',
+        isActive && 'border-b-4 border-blue3'
+      )}
+      onClick={() => router.push(url)}
+    >
+      {title}
+    </div>
+  );
+}
+
+Tabs.Tab = Tab;
