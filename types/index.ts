f1825809ed61995a0dc4945a0b6e5f2deb32e812
Kuesung Park <gueit214@naver.com>
2023-07-11 10:37:08 +0900

    refactor : Type 이동

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,23 @@ export type PageType = 'grouping' | 'meeting' | 'profile';
 export type StrictPropsWithChildren<P = unknown> = P & {
   children: React.ReactNode;
 };
+
+export interface ImageType {
+  imageFile: File | null;
+  imageBlob: string;
+}
+
+export interface TimeType {
+  fromHour: string;
+  fromMin: string;
+  fromAmPm: string;
+  toHour: string;
+  toMin: string;
+  toAmPm: string;
+}
+
+export interface BirthdayValueType {
+  year: string;
+  month: string;
+  date: string;
+}
