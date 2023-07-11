3af5d0da10bcf82325ac80840da8c9d109c07dca
Kuesung Park <gueit214@naver.com>
2023-07-11 15:53:03 +0900

    feat : 날짜 display 추가

--- app/grouping/create/InputForm.tsx
+++ app/grouping/create/InputForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 
 import Button from '@/components/common/Button';
@@ -19,16 +19,13 @@ import type { ImageType, TimeType } from '@/types';
 
 const TEXT_AREA_COUNT = 30;
 
-type ModalNameType = 'meetingDate' | 'meetingLocation' | 'meetingNumber'
-
-
+type ModalNameType = 'meetingDate' | 'meetingLocation' | 'meetingNumber';
 type ModalTabType = {
   name: ModalNameType;
   title: string;
   snap: number;
   message: string;
 };
-
 const modalTabList: ModalTabType[] = [
   {
     name: 'meetingDate',
@@ -77,8 +74,17 @@ const inputDefaultValues = {
     toAmPm: 'AM',
   },
   meetingLocation: '',
-  meetingNumber: 0,
 };
+
+function getDayName(dayIndex: number) {
+  const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  return days[dayIndex];
+}
+
+function getMonthName(monthIndex: number) {
+  const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+  return months[monthIndex];
+}
 
 export default function InputForm() {
   const imgRef = useRef<HTMLInputElement | null>(null);
@@ -88,18 +94,35 @@ export default function InputForm() {
 
   const { isModalOpen, openModal, closeModal, modalName } = useModal<ModalNameType>();
 
-  const handleNextButton = () => {
+  const handlePreviousButton = () => {
     if (modalName === 'meetingDate') openModal('meetingLocation');
     if (modalName === 'meetingLocation') openModal('meetingNumber');
-    if (modalName === 'meetingNumber') {
+  };
+
+  const handleNextButton = () => {
+    if (modalName === 'meetingNumber') openModal('meetingLocation');
+    if (modalName === 'meetingLocation') openModal('meetingDate');
+    else {
       closeModal();
       // TODO: 모임 생성 API
     }
   };
 
-  const handlePreviousButton = () => {
-    if (modalName === 'meetingLocation') openModal('meetingDate');
-    if (modalName === 'meetingNumber') openModal('meetingLocation');
+  function displayDate() {
+    const date = watch('date');
+    const year = date.getFullYear();
+    const month = getMonthName(date.getMonth());
+    const day = date.getDate();
+    const dayName = getDayName(date.getDay());
+    return `${year}. ${month}. ${day} ${dayName} ${watch('time').fromHour}:${
+      watch('time').fromMin
+    } ${watch('time').fromAmPm} ~ ${watch('time').toHour}:${watch('time').toMin} ${
+      watch('time').toAmPm
+    }`;
+  }
+
+  const handleSubmitButton = (data) => {
+    console.log(data);
   };
 
   const isAllEntered =
@@ -120,9 +143,10 @@ export default function InputForm() {
       <ImageFrame
         setImage={(value: ImageType) => setValue('image', value)}
         imgRef={imgRef}
-        imageBlob={watch('image').imageBlob}
+        imageBlob={watch('image')?.imageBlob}
         shape="square"
       />
+
       <section>
         <div className="mb-5 text-14">방제목</div>
         <Input
@@ -133,7 +157,9 @@ export default function InputForm() {
           })}
         />
       </section>
+
       <Spacing size={15} />
+
       <section>
         <div className="flex justify-between">
           <div className="mb-5 text-14">활동 소개글</div>
@@ -147,39 +173,26 @@ export default function InputForm() {
           })}
         />
       </section>
+
       <Spacing size={15} />
 
       {modalTabList.map((modalTab: ModalTabType) => (
         <InputSection
           key={modalTab.title}
           title={modalTab.title}
-          message={modalTab.message}
+          value={modalTab.name === 'meetingDate' ? displayDate() : watch(modalTab.name)}
+          placeholder={modalTab.message}
           onClick={() => openModal(modalTab.name)}
         />
       ))}
 
-      <InputSection
-        title="모임 일시"
-        message="모임 일시를 설정해주세요"
-        onClick={() => openModal('meetingDate')}
-      />
-      <Spacing size={15} />
-      <InputSection
-        title="모임 위치"
-        message="모임 위치를 설정해주세요"
-        onClick={() => openModal('meetingLocation')}
-      />
-      <Spacing size={15} />
-      <InputSection
-        title="모임 인원"
-        message="모임 인원를 설정해주세요"
-        onClick={() => openModal('meetingNumber')}
-      />
       <Button
         text="완료"
         disabled={!isAllEntered}
-        className="fixed inset-x-0 bottom-20 z-10 m-auto max-w-[23.75rem] bg-white"
+        className="fixed inset-x-0 bottom-20 z-10 m-auto max-w-380 bg-white"
+        onClick={isAllEntered ? handleSubmit(handleSubmitButton) : handleNextButton}
       />
+
       <BottomUpModal
         isModalOpen={isModalOpen}
         snap={modalTabList.find((modalTab) => modalTab.name === modalName)?.snap || 0}
