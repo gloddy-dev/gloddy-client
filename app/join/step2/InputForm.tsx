386f2b761e85ee3e3de18071744de5937d5b599a
Kuesung Park <gueit214@naver.com>
2023-07-12 10:28:59 +0900

    refactor : page servcer componepnt 일관성을 위해 이동

--- app/join/step2/InputForm.tsx
+++ app/join/step2/InputForm.tsx
@@ -10,6 +10,24 @@ type InputType = {
   school: string;
 };
 
+const DUMMY_SEARCH_RESULT_LIST = [
+  {
+    id: 1,
+    name: '경희대학교 서울캠퍼스',
+    address: '서울특별시 동대문구 경희대로 26',
+  },
+  {
+    id: 2,
+    name: '경희주유소',
+    address: '서울특별시 동대문구 경희대로 26',
+  },
+  {
+    id: 3,
+    name: '경희주유소',
+    address: '서울특별시 동대문구 경희대로 26',
+  },
+];
+
 export default function InputForm() {
   const router = useRouter();
 
@@ -36,7 +54,16 @@ export default function InputForm() {
         })}
       />
 
-      <Button text="완료" type="submit" className="absolute bottom-0 w-full" />
+      <section>
+        {DUMMY_SEARCH_RESULT_LIST.map((searchResult) => (
+          <div key={searchResult.id} className="border-b-[0.5px] border-b-gray6 p-20">
+            <div className="text-14">{searchResult.name}</div>
+            <div className="text-12 text-gray2">{searchResult.address}</div>
+          </div>
+        ))}
+      </section>
+
+      <Button text="완료" type="submit" className="absolute bottom-0" disabled={!watch('school')} />
     </form>
   );
 }
