da8e10c9e4d747939ebaa644524e7661a11dc9f6
Kuesung Park <gueit214@naver.com>
2023-07-12 16:11:04 +0900

    refactor : GroupingCreateTopNavigationBar 컴포넌트 생성

---
+++ app/grouping/create/components/GroupingCreateTopNavigationBar.server.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import { TopNavigationBar } from '@/components/common/NavigationBar';
+
+export default function GroupingCreateTopNavigationBar() {
+  const router = useRouter();
+  return (
+    <TopNavigationBar
+      leftNode={
+        <Image
+          alt="back"
+          src="/assets/arrow_back.svg"
+          width={8}
+          height={30}
+          onClick={() => {
+            router.back();
+          }}
+        />
+      }
+      text="모임 개설하기"
+    />
+  );
+}
--- app/grouping/create/components/InputForm.client.tsx
+++ app/grouping/create/components/InputForm.client.tsx
@@ -13,6 +13,7 @@ import NumberSwipePicker from '@/components/common/SwipePicker/NumberSwipePicker
 import TimeSwipePicker from '@/components/common/SwipePicker/TimeSwipePicker';
 import { useModal } from '@/hooks/useModal';
 
+import GroupingCreateTopNavigationBar from './GroupingCreateTopNavigationBar.server';
 import InputSection from './InputSection.server';
 
 import type { ImageType, TimeType } from '@/types';
@@ -138,6 +139,7 @@ export default function InputForm() {
 
   return (
     <div>
+      <GroupingCreateTopNavigationBar />
       <ImageFrame
         setImage={(value: ImageType) => setValue('image', value)}
         imgRef={imgRef}
