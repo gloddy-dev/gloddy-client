9410b4452f3c0253d56b27df07877694d7b8c2a8
Kuesung Park <gueit214@naver.com>
2023-07-12 16:47:01 +0900

    refactor : spacing 컴포넌트 확장성 있게 수정

--- components/common/Spacing.tsx
+++ components/common/Spacing.tsx
@@ -3,13 +3,19 @@ import { HTMLAttributes, memo } from 'react';
 
 interface SpacingProps extends HTMLAttributes<HTMLDivElement> {
   direction?: 'horizontal' | 'vertical';
+  className?: string;
   size: number;
 }
 
-export default memo(function Spacing({ direction = 'vertical', size, ...props }: SpacingProps) {
+export default memo(function Spacing({
+  size,
+  direction = 'vertical',
+  className,
+  ...props
+}: SpacingProps) {
   return (
     <div
-      className={clsx('flex-none', direction === 'vertical' ? `h-${size}` : `w-${size}`)}
+      className={clsx('flex-none', direction === 'vertical' ? `h-${size}` : `w-${size}`, className)}
       {...props}
     />
   );
