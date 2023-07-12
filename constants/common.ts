01c32410f96c3f2d441e5e3802fee48569fc43f4
Kuesung Park <gueit214@naver.com>
2023-07-12 10:31:45 +0900

    feat : instagram url 상수화

--- constants/common.ts
+++ constants/common.ts
@@ -1,3 +1,4 @@
 export const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_API_KEY}&autoload=false&libraries=services,clusterer`;
 export const IS_PRODUCTION = process.env.NEXT_PUBLIC_APP_ENV === 'production';
 export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
+export const INSTAGRAM_URL = 'https://www.instagram.com/gloddykorea';
