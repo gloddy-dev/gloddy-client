35357c90d97bfab533cc0bbcd48e178bf8b638ea
kang <kangju2000@naver.com>
2023-07-12 17:10:27 +0900

    refactor: Tabs 공통 컴포넌트 로직 변경

--- components/common/Tabs.tsx
+++ components/common/Tabs.tsx
@@ -1,34 +1,57 @@
 'use client';
 import clsx from 'clsx';
-import { usePathname, useRouter } from 'next/navigation';
+import { type PropsWithChildren, createContext, useContext, useState } from 'react';
 import type { StrictPropsWithChildren } from '@/types';
 
-export default function Tabs({ children }: StrictPropsWithChildren) {
+const TabsContext = createContext<{
+  activeTab: string;
+  setActiveTab: (value: string) => void;
+} | null>(null);
+
+interface TabsProps {
+  defaultActiveTab: string;
+}
+export default function Tabs({ defaultActiveTab, children }: StrictPropsWithChildren<TabsProps>) {
+  const [activeTab, setActiveTab] = useState(defaultActiveTab);
+
+  return (
+    <TabsContext.Provider value={{ activeTab, setActiveTab }}>{children}</TabsContext.Provider>
+  );
+}
+
+function List({ children }: StrictPropsWithChildren) {
   return <div className="flex h-50 border-b border-white3">{children}</div>;
 }
 
 interface TabProps {
-  title: string;
-  url: string;
+  value: string;
+  text: string;
 }
 
-function Tab({ title, url }: TabProps) {
-  const router = useRouter();
-  const pathname = usePathname();
+function Tab({ value, text }: TabProps) {
+  const { activeTab, setActiveTab } = useContext(TabsContext)!;
 
-  const isActive = pathname === url;
+  const isActive = activeTab === value;
 
   return (
     <div
       className={clsx(
-        'flex flex-1 cursor-pointer items-center justify-center text-gray7',
+        'flex flex-1 cursor-pointer items-center justify-center',
         isActive && 'border-b-4 border-blue3'
       )}
-      onClick={() => router.push(url)}
+      onClick={() => setActiveTab(value)}
     >
-      {title}
+      {text}
     </div>
   );
 }
 
+function Panel({ value, children }: PropsWithChildren<Pick<TabProps, 'value'>>) {
+  const { activeTab } = useContext(TabsContext)!;
+
+  return <div className={activeTab === value ? 'block' : 'hidden'}>{children}</div>;
+}
+
+Tabs.List = List;
 Tabs.Tab = Tab;
+Tabs.Panel = Panel;
