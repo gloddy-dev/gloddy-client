b43610d5e2ef9790d23022ad6b50ca5072bf3e41
Kuesung Park <gueit214@naver.com>
2023-07-13 15:04:15 +0900

    refactor : type import

--- app/grouping/create/components/InputSection.server.tsx
+++ app/grouping/create/components/InputSection.server.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes } from 'react';
+import type { InputHTMLAttributes } from 'react';
 
 import { Input } from '@/components/common/Input';
 import Spacing from '@/components/common/Spacing';
@@ -8,12 +8,12 @@ interface InputSectionProps extends InputHTMLAttributes<HTMLInputElement> {
   onClick: () => void;
 }
 
-export default function InputSection({ title, onClick, ...rest }: InputSectionProps) {
+export default function InputSection({ title, onClick, ...props }: InputSectionProps) {
   return (
     <section onClick={onClick}>
       <p className="text-14">{title}</p>
       <Spacing size={10} />
-      <Input readOnly {...rest} />
+      <Input readOnly {...props} />
       <Spacing size={15} />
     </section>
   );
