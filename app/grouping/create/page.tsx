21f91540411a7ba923a56a6a37fa4f98e7e50914
Kuesung Park <gueit214@naver.com>
2023-07-14 21:21:25 +0900

    refactor : 네이밍 수정

--- app/grouping/create/page.tsx
+++ app/grouping/create/page.tsx
@@ -1,10 +1,10 @@
-import GroupingCreateTopNavigationBar from './components/GroupingCreateTopNavigationBar.client';
 import InputForm from './components/InputForm.client';
+import TopNavigationBar from './components/TopNavigationBar.client';
 
 export default function CreateMeeting() {
   return (
     <div className="mx-20">
-      <GroupingCreateTopNavigationBar />
+      <TopNavigationBar />
       <InputForm />
     </div>
   );
