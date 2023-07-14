ecd2f20a2e47b583e388db8178e9a71d16e629b5
Kuesung Park <gueit214@naver.com>
2023-07-14 21:11:09 +0900

    refactor : $ 제거

--- app/grouping/create/components/inputSection/DescriptionSection.tsx
+++ app/grouping/create/components/inputSection/DescriptionSection.tsx
@@ -8,7 +8,7 @@ export default function DescriptionSection({ ...props }: Omit<InputProps, 'place
     <section>
       <div className="flex justify-between">
         <div className="mb-5 text-14">활동 소개글</div>
-        <div className="text-12 text-gray2">0/${TEXT_AREA_COUNT}</div>
+        <div className="text-12 text-gray2">0/{TEXT_AREA_COUNT}</div>
       </div>
       <TextArea {...props} placeholder="내용을 입력해주세요." />
     </section>
