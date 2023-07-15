73ed39f9a4c9cbdd44b7fcc8a4d310ea3e90b5bd
Kuesung Park <gueit214@naver.com>
2023-07-15 15:52:20 +0900

    refactor : 타입 Omit 제거

--- app/grouping/create/components/inputSection/TitleSection.server.tsx
+++ app/grouping/create/components/inputSection/TitleSection.server.tsx
@@ -1,11 +1,11 @@
 import { Input } from '@/components/common/Input';
 import { InputProps } from '@/components/common/Input/Input';
 
-export default function TitleSection({ ...props }: Omit<InputProps, 'placeholder'>) {
+export default function TitleSection({ ...props }: InputProps) {
   return (
-    <section>
+    <section {...props}>
       <div className="mb-5 text-14">방 제목</div>
-      <Input {...props} placeholder="제목을 입력해주세요" />
+      <Input placeholder="제목을 입력해주세요" {...props} />
     </section>
   );
 }
