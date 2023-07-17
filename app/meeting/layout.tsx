ba7443cfda45b37933d3e238e6d351812c60e121
Kuesung Park <gueit214@naver.com>
2023-07-17 19:27:37 +0900

    refactor : BottomNoavigationBar 추가

--- app/meeting/layout.tsx
+++ app/meeting/layout.tsx
@@ -1,9 +1,12 @@
+import { BottomNavigationBar } from '@/components/common/NavigationBar';
+import PageTransition from '@/components/common/PageTransition';
+
 export default function MeetingLayout({ children }: { children: React.ReactNode }) {
   // const bottomNavPathName = ['/grouping', '/board', '/meeting', '/profile'];
   return (
     <div>
       <div className="h-full w-full bg-white3 px-24">{children}</div>
-      {/* BottomNavigationBat */}
+      <BottomNavigationBar page="meeting" />
     </div>
   );
 }
