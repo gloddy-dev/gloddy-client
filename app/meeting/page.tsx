2bbb82df6309f2b6c5fea5edc98ba3622e22a43c
Kuesung Park <gueit214@naver.com>
2023-07-17 19:27:27 +0900

    feat : meeting 페이지 애니메이션 구현

--- app/meeting/page.tsx
+++ app/meeting/page.tsx
@@ -1,7 +1,16 @@
+'use client';
+import PageTransition from '@/components/common/PageTransition';
 import React from 'react';
 
-const Meeting = () => {
-  return <div></div>;
+type IndexPageProps = {};
+type IndexPageRef = React.ForwardedRef<HTMLDivElement>;
+
+const Meeting = (props: IndexPageProps, ref: IndexPageRef) => {
+  return (
+    <PageTransition ref={ref} isStartLeft={false}>
+      <div>Meeting</div>
+    </PageTransition>
+  );
 };
 
 export default Meeting;
