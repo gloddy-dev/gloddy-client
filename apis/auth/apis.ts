b083929007f72bdc1a31717f7a9d30dbafe26531
Kuesung Park <gueit214@naver.com>
2023-07-18 11:04:16 +0900

    feat : test id로 로그인 구현 (토큰용)

--- apis/auth/apis.ts
+++ apis/auth/apis.ts
@@ -1,1 +1,13 @@
+import publicApi from '../config/publicApi';
+
+const TEST_ID = {
+  email: 'testy54@soongsil.ac.kr',
+  password: 'qwqw5533',
+};
+
 export const getExample = () => {};
+
+export const login = async () => {
+  const data = await publicApi.post(`/api/v1/auth/login`, TEST_ID);
+  return data;
+};
