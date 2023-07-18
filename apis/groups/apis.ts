cc06f50dec5dbd7e8ae4bb17edd5eefde1c8c0cb
Kuesung Park <gueit214@naver.com>
2023-07-18 11:03:04 +0900

    feat : group api

--- apis/groups/apis.ts
+++ apis/groups/apis.ts
@@ -1,1 +1,9 @@
+import { privateApi } from '../config/privateApi.server';
+import axios from 'axios';
+
 export const getExample = () => {};
+
+export const getGroups = async () => {
+  const data = await privateApi('/api/v1/groups?size=5&page=5');
+  return data;
+};
