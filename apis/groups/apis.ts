370b8924d41372e95cb9384d4648c2e5fc121878
Kuesung Park <gueit214@naver.com>
2023-07-18 12:14:43 +0900

    feat : Group Type 추가

--- apis/groups/apis.ts
+++ apis/groups/apis.ts
@@ -2,7 +2,15 @@ import { privateApi } from '../config/privateApi.server';
 
 export const getExample = () => {};
 
+type GroupServerResponse = {
+  totalCount: number;
+  currentCount: number;
+  currentPage: number;
+  totalPage: number;
+  contents: any[];
+};
+
 export const getGroupsServer = async () => {
-  const data = await privateApi('/api/v1/groups?size=5&page=5');
+  const data = await privateApi<GroupServerResponse>(`/api/v1/groups?size=5&page=5`);
   return data;
 };
