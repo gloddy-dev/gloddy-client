dcc448df06d5150afc93f2c8ad40140b67b07d80
Kuesung Park <gueit214@naver.com>
2023-07-18 12:25:06 +0900

    refactor : import sort

--- app/grouping/create/components/inputSection/LocationSection.server.tsx
+++ app/grouping/create/components/inputSection/LocationSection.server.tsx
@@ -1,10 +1,9 @@
 import InputArea from './InputArea.server';
 import { CreateMeetingRequestType } from '../../type';
 import { BottomFixedButton } from '@/components/common/Button';
+import BottomSheet from '@/components/common/Modal/BottomSheet';
 import useModalState from '@/store/useModalStore';
 import { UseFormSetValue } from 'react-hook-form';
-
-import BottomSheet from '@/components/common/Modal/BottomSheet';
 
 interface LocationSectionProps {
   value: string;
