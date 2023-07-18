41a40c9f8aa413dbebc6264c64c34a5e93bf178d
Kuesung Park <gueit214@naver.com>
2023-07-18 11:04:36 +0900

    feat : group 페이지에서 groups 서버로부터 받아오기

--- app/grouping/page.tsx
+++ app/grouping/page.tsx
@@ -1,13 +1,16 @@
-'use client';
-
-import { BottomNavigationBar } from '@/components/common/NavigationBar';
-import { Spacing } from '@/components/common/Spacing';
-
 import FloatingBubbleSection from './components/FloatingBubbleSection.server';
 import GroupingCardList from './components/GroupingCardList.client';
 import GroupingTopNavigationBar from './components/GroupingTopNavigationBar.server';
+import { getGroupsServerServer } from '@/apis/groups';
+import { BottomNavigationBar } from '@/components/common/NavigationBar';
+import { Spacing } from '@/components/common/Spacing';
 
 export default function Grouping() {
+  const getGroupsQuery = async () => {
+    const data = await getGroupsServer();
+    return data;
+  };
+
   return (
     <div className="relative h-full">
       <GroupingTopNavigationBar />
