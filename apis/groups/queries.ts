1628b7245f10448f07ee6b404648c37cdd1d149b
Kuesung Park <gueit214@naver.com>
2023-07-18 12:14:34 +0900

    feat : useGetGroups Query 구현

--- apis/groups/queries.ts
+++ apis/groups/queries.ts
@@ -1,1 +1,7 @@
+import { getGroupsServer } from './apis';
+import { Keys } from './keys';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+
 export const useGetExample = () => {};
+
+export const useGetGroups = () => useQuery(Keys.getGroupsServer(), () => getGroupsServer());
