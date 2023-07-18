5334e3bc5c2103de74f339c92e44c0b23396e235
Kuesung Park <gueit214@naver.com>
2023-07-18 11:36:30 +0900

    refactor : server에서 호출하는 것이므로 Server 접미사

--- apis/groups/apis.ts
+++ apis/groups/apis.ts
@@ -1,9 +1,8 @@
 import { privateApi } from '../config/privateApi.server';
-import axios from 'axios';
 
 export const getExample = () => {};
 
-export const getGroups = async () => {
+export const getGroupsServer = async () => {
   const data = await privateApi('/api/v1/groups?size=5&page=5');
   return data;
 };
