ff378def1c6451c4bd5c55a84377ff9944053646
Kuesung Park <gueit214@naver.com>
2023-07-18 11:43:17 +0900

    refactor : prettier

--- app/provider.tsx
+++ app/provider.tsx
@@ -3,6 +3,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
+
 import type { StrictPropsWithChildren } from '@/types';
 
 export default function Provider({ children }: StrictPropsWithChildren) {
--- components/common/Input/Input.tsx
+++ components/common/Input/Input.tsx
@@ -1,7 +1,8 @@
 'use client';
 import clsx from 'clsx';
-import type { InputHTMLAttributes } from 'react';
 import { UseFormRegisterReturn } from 'react-hook-form';
+
+import type { InputHTMLAttributes } from 'react';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
