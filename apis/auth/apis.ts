0aeff1a8264794e5834906299ab995f2b0509008
Kuesung Park <gueit214@naver.com>
2023-07-19 11:41:41 +0900

    feat : type 선언

--- apis/auth/apis.ts
+++ apis/auth/apis.ts
@@ -1,13 +1,18 @@
 import publicApi from '../config/publicApi';
 
+export type LoginResponse = {
+  errorCode: string;
+  userId: number;
+  authority: string;
+  token: string;
+};
+
 const TEST_ID = {
   email: 'testy54@soongsil.ac.kr',
   password: 'qwqw5533',
 };
 
-export const getExample = () => {};
-
 export const login = async () => {
-  const data = await publicApi.post(`/api/v1/auth/login`, TEST_ID);
+  const data = await publicApi.post('/api/v1/auth/login', TEST_ID);
   return data;
 };
