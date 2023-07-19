90e4a2841af1226fd4af25f542d25b173c51f507
Kuesung Park <gueit214@naver.com>
2023-07-19 12:26:25 +0900

    refactor : 불필요 코드 제거

--- apis/groups/queries.ts
+++ apis/groups/queries.ts
@@ -1,8 +1,6 @@
 import { getGroups } from './apis';
 import { Keys } from './keys';
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
-
-export const useGetExample = () => {};
+import { useQuery } from '@tanstack/react-query';
 
 export const useGetGroups = (pageNum: number) =>
   useQuery(Keys.getGroups(), () => getGroups(pageNum));
