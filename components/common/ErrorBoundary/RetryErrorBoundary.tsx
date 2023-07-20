824c11f726125837af41aebe4cb131dedfa79629
Kuesung Park <gueit214@naver.com>
2023-07-20 16:24:44 +0900

    chore : pxr 오타 수정

--- components/common/ErrorBoundary/RetryErrorBoundary.tsx
+++ components/common/ErrorBoundary/RetryErrorBoundary.tsx
@@ -6,11 +6,11 @@ export default function RetryErrorBoundary({ children }: { children: React.React
   return (
     <ErrorBoundary
       fallbackRender={({ resetErrorBoundary }) => (
-        <div className="px-24pxr py-40pxr flex flex-col items-center">
+        <div className="flex flex-col items-center px-24 py-40">
           <p className="text-h5 text-black">문제가 발생했습니다</p>
-          <p className="mt-2pxr text-b3 text-grey-600">페이지를 불러오는데 실패했습니다.</p>
+          <p className="text-b3 text-grey-600 mt-2">페이지를 불러오는데 실패했습니다.</p>
           <button
-            className="mt-16pxr bg-grey-200 px-12pxr py-8pxr text-13pxr font-bold text-grey-800 rounded-lg"
+            className="bg-grey-200 font-bold text-grey-800 mt-16 rounded-lg px-12 py-8 text-13"
             onClick={() => resetErrorBoundary()}
           >
             다시 불러오기
