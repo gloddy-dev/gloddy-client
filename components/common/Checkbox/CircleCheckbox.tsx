1498a59a1e7c421b39d1989af9fd7321e3369165
kang <kangju2000@naver.com>
2023-07-20 15:53:35 +0900

    refactor: CircleCheckbox 컴포넌트에 ref 추가

--- components/common/Checkbox/CircleCheckbox.tsx
+++ components/common/Checkbox/CircleCheckbox.tsx
@@ -1,21 +1,23 @@
 import Image from 'next/image';
+import { forwardRef } from 'react';
 
 interface CircleCheckboxProps {
   text?: React.ReactNode;
   checked?: boolean;
   onClick?: () => void;
 }
-export default function CircleCheckbox({
-  text,
-  checked = false,
-  onClick,
-  ...props
-}: CircleCheckboxProps) {
+
+function CircleCheckbox(
+  { text, checked = false, onClick, ...props }: CircleCheckboxProps,
+  ref: React.Ref<HTMLDivElement>
+) {
   const checkboxImageAsset = `/assets/checkbox_circle${checked ? '_checked' : ''}.svg`;
   return (
-    <div className="flex items-center" onClick={onClick} {...props}>
+    <div ref={ref} className="flex items-center" onClick={onClick} {...props}>
       <Image alt="checkbox" src={checkboxImageAsset} width={15} height={30} className="mx-10" />
       {text}
     </div>
   );
 }
+
+export default forwardRef(CircleCheckbox);
