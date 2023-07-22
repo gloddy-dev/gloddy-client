215d98c6a4a3640b1b9962897605ee5ee37e9b0a
Kuesung Park <gueit214@naver.com>
2023-07-22 15:31:25 +0900

    refactor : ref 제거

--- components/common/Input/TextArea.tsx
+++ components/common/Input/TextArea.tsx
@@ -10,13 +10,9 @@ export interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElemen
   register?: UseFormRegisterReturn;
 }
 
-function TextArea(
-  { placeholder, className, register, ...props }: TextAreaProps,
-  ref: React.Ref<HTMLTextAreaElement>
-) {
+function TextArea({ placeholder, className, register, ...props }: TextAreaProps) {
   return (
     <textarea
-      ref={ref}
       placeholder={placeholder}
       className={cn(
         'font-500 h-112 w-full resize-none rounded-lg bg-gray5 p-10 px-23 py-14 text-16 text-black outline-none placeholder:text-gray3',
