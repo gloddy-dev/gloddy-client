2ee60814c68e76de0ca32b324afd38b80560d3c9
Kuesung Park <gueit214@naver.com>
2023-07-22 15:30:52 +0900

    Revert "refactor : {...register}로 수정"
    This reverts commit 86defe0354ee8748d027fab4568178f324ce48ea.

--- components/common/Input/Input.tsx
+++ components/common/Input/Input.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { Spacing } from '../Spacing';
 import cn from '@/utils/cn';
-import React from 'react';
 import { UseFormRegisterReturn } from 'react-hook-form';
 
 import type { InputHTMLAttributes } from 'react';
@@ -14,16 +13,19 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   register?: UseFormRegisterReturn;
 }
 
-export default function Input(
-  { label, placeholder, register, className, type = 'text', ...props }: InputProps,
-  ref: React.Ref<HTMLInputElement>
-) {
+export default function Input({
+  label,
+  placeholder,
+  register,
+  className,
+  type = 'text',
+  ...props
+}: InputProps) {
   return (
     <div className="relative flex h-52 w-full rounded-lg bg-[#f6f6f6]">
       <Spacing size={24} direction="horizontal" />
       {label && <p className="flex shrink-0 items-center text-gray2">{label}</p>}
       <input
-        ref={ref}
         placeholder={placeholder}
         className={cn(
           'flex h-full grow rounded-lg border-none bg-[#f6f6f6] pr-5 text-16 outline-none placeholder:text-gray3',
