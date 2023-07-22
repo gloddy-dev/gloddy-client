f173db0a8d378247d245aceedd1fee4374bfbb68
Kuesung Park <gueit214@naver.com>
2023-07-22 11:36:30 +0900

    fix : register 설정하지 않을 시 에러 해결

--- components/common/Input/TextArea.tsx
+++ components/common/Input/TextArea.tsx
@@ -2,14 +2,16 @@
 
 import cn from '@/utils/cn';
 import { type TextareaHTMLAttributes, forwardRef } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 export interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   placeholder: string;
   className?: string;
+  register?: UseFormRegisterReturn;
 }
 
 function TextArea(
-  { placeholder, className, ...props }: TextAreaProps,
+  { placeholder, className, register, ...props }: TextAreaProps,
   ref: React.Ref<HTMLTextAreaElement>
 ) {
   return (
@@ -20,6 +22,7 @@ function TextArea(
         'font-500 h-112 w-full resize-none rounded-lg bg-gray5 p-10 px-23 py-14 text-16 text-black outline-none placeholder:text-gray3',
         className
       )}
+      {...register}
       {...props}
     />
   );
