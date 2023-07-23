d69126950597e20fb86a63245b59ce51a239ffc0
Kuesung Park <gueit214@naver.com>
2023-07-23 12:18:19 +0900

    refactor : 각 상황에 따른 에러 핸들링

--- apis/config/interceptor.ts
+++ apis/config/interceptor.ts
@@ -11,13 +11,32 @@ export const onResponse = (response: AxiosResponse) => {
 };
 
 export const onResponseError = (error: AxiosError<ErrorType, InternalAxiosRequestConfig>) => {
-  if (error.response) {
-    const data = error.response.data;
-    const { success, statusCode, errorCode, reason } = data;
-    return Promise.reject(new ApiError(success, statusCode, errorCode, reason));
-  }
-  if (error.request) console.log(error.request);
-  else console.log('Error', error.message);
+  console.error('error', error);
+  try {
+    if (error.response) {
+      const data = error.response.data;
+      const { message, reason, status, timestamp } = data;
 
-  return Promise.reject(new Error('요청 도중 에러 발생'));
+      if (status === 401) {
+        // TODO: 토큰 재발급 로직
+      }
+      if (status === 400) {
+        return Promise.reject(message);
+      }
+      return Promise.reject(new ApiError(message, reason, status, timestamp));
+    }
+    if (error.request) {
+      // 요청이 이루어 졌으나 응답을 받지 못했습니다.
+      // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
+      // Node.js의 http.ClientRequest 인스턴스입니다.
+      console.log(error.request);
+    } else {
+      // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
+      console.log('Error', error.message);
+    }
+
+    return Promise.reject(new Error('요청 도중 에러 발생'));
+  } catch (error) {
+    return Promise.reject(error);
+  }
 };
