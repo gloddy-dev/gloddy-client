a997ee54723c7d350127a4a26631f1fa05d9c188
Kuesung Park <gueit214@naver.com>
2023-07-23 12:18:05 +0900

    feat : SMS 인증 훅 구현

--- apis/auth/mutations.ts
+++ apis/auth/mutations.ts
@@ -1,1 +1,12 @@
-export const useExmaple = () => {};
+import { postSMSVerify } from './apis';
+import { SMSVerifiyRequest } from './type';
+import { useMutation } from '@tanstack/react-query';
+
+export const useSMSVerify = () => {
+  return useMutation({
+    mutationFn: (verifyInfo: SMSVerifiyRequest) => postSMSVerify(verifyInfo),
+    onError: (error) => {
+      // TODO : 인증번호 에러에 대한 처리
+    },
+  });
+};
