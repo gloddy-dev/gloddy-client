a9b792375d8f426ee6689c4cacd575672e80688a
Kuesung Park <gueit214@naver.com>
2023-07-24 12:10:48 +0900

    feat : useTimer hook 구현

---
+++ hooks/useTimer/helpers.ts
@@ -0,0 +1,7 @@
+export function createActionType<T extends string>(type: T): { type: T };
+
+export function createActionType<T extends string, P>(type: T, payload: P): { type: T; payload: P };
+
+export function createActionType(type: string, payload?: unknown) {
+  return { type, payload };
+}
---
+++ hooks/useTimer/reducer.ts
@@ -0,0 +1,53 @@
+import { type TimerActionsType } from './actions';
+import { type State } from './type';
+
+function reducer(state: State, action: TimerActionsType): State {
+  switch (action.type) {
+    case 'advanceTime': {
+      const { timeToAdd } = action.payload;
+
+      return {
+        ...state,
+        time: state.timerType === 'DECREMENTAL' ? state.time - timeToAdd : state.time + timeToAdd,
+      };
+    }
+    case 'pause': {
+      return {
+        ...state,
+        status: 'PAUSED',
+      };
+    }
+    case 'reset': {
+      return {
+        ...state,
+        status: 'STOPPED',
+        time: action.payload.initialTime,
+      };
+    }
+    case 'set': {
+      return {
+        ...state,
+        time: action.payload.newTime,
+      };
+    }
+    case 'start': {
+      const { initialTime } = action.payload;
+
+      return {
+        ...state,
+        status: 'RUNNING',
+        time: state.status === 'STOPPED' ? initialTime : state.time,
+      };
+    }
+    case 'stop': {
+      return {
+        ...state,
+        status: 'STOPPED',
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+export default reducer;
