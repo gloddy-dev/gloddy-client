b1dbd0d38da33edcbb92858a1d81b67d4c80db33
kang <kangju2000@naver.com>
2023-07-26 00:52:28 +0900

    refactor: 형식에 맞게 파싱 후 api 요청 로직 추가

--- app/grouping/create/components/inputSection/SubmitSection.tsx
+++ app/grouping/create/components/inputSection/SubmitSection.tsx
@@ -1,18 +1,37 @@
 'use client';
 import { useCreateGroupContext } from '../CreateGroupContext';
+import { type CreateGroupRequest, usePostCreateGroup } from '@/apis/groups';
 import { BottomFixedButton } from '@/components/common/Button';
+import { format } from 'date-fns';
 
 import type { CreateGroupContextValue } from '../../type';
-import type { CreateGroupRequest } from '@/apis/groups';
+
+function timeFormat(hour: string, minute: string, ampm: string) {
+  if (ampm === 'AM') {
+    return `${hour}:${minute}`;
+  }
+
+  return `${Number(hour) + 12}:${minute}`;
+}
 
 export default function SubmitSection() {
+  const { mutate: createGroupMutate } = usePostCreateGroup();
+
   const {
     handleSubmit,
     formState: { isDirty, isValid },
   } = useCreateGroupContext();
   const onCreateGroupSubmit = (data: CreateGroupContextValue) => {
-    // TODO : 서버 api 전송
-    console.log(data);
+    const { date, time, ...rest } = data;
+
+    const createGroupRequest: CreateGroupRequest = {
+      ...rest,
+      meetDate: format(data.date, 'yyyy-MM-dd'),
+      startTime: timeFormat(data.time.fromHour, data.time.fromMin, data.time.fromAmPm),
+      endTime: timeFormat(data.time.toHour, data.time.toMin, data.time.toAmPm),
+    };
+
+    createGroupMutate(createGroupRequest);
   };
 
   return (
