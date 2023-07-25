e3675a7d8c6ada1210e6f71e16c16266d833e84b
kang <kangju2000@naver.com>
2023-07-25 16:16:59 +0900

    chore: @hookform/devtools 추가

--- app/grouping/create/components/CreateGroupContext.tsx
+++ app/grouping/create/components/CreateGroupContext.tsx
@@ -1,3 +1,4 @@
+import { DevTool } from '@hookform/devtools';
 import { createContext, useContext } from 'react';
 import { UseFormReturn, useForm } from 'react-hook-form';
 
@@ -28,6 +29,7 @@ export default function CreateGroupContextProvider({ children }: StrictPropsWith
   return (
     <CreateGroupContext.Provider value={contextValue as unknown as UseFormReturn}>
       {children}
+      <DevTool control={methods.control} />
     </CreateGroupContext.Provider>
   );
 }
