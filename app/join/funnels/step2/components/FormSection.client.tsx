e218e9e9681b944da3ce4a506af1c482649b2d11
Kuesung Park <gueit214@naver.com>
2023-07-26 11:52:20 +0900

    refactor : SubmitSection 분리

--- app/join/funnels/step2/components/FormSection.client.tsx
+++ app/join/funnels/step2/components/FormSection.client.tsx
@@ -1,26 +1,9 @@
 import EmailForm from './EmailForm.client';
-import { useJoinContext } from '../../../components/JoinContext';
-import { useFunnelContext } from '../../JoinFunnel';
-import { BottomFixedButton } from '@/components/common/Button';
 
 export default function FormSection() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-    watch,
-  } = useJoinContext();
-  const { nextStep } = useFunnelContext();
-
   return (
     <section>
       <EmailForm />
-      <BottomFixedButton
-        text="완료"
-        type="submit"
-        disabled={!watch('schoolInfo.school')}
-        onSubmit={handleSubmit(nextStep)}
-      />
     </section>
   );
 }
