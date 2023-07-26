6ad267fa1dc5a1d31546102b4ae96a1414b2ffc6
Kuesung Park <gueit214@naver.com>
2023-07-26 10:51:02 +0900

    refactor : 간격 gap조절

--- app/join/funnels/step4/components/FormSection.client.tsx
+++ app/join/funnels/step4/components/FormSection.client.tsx
@@ -1,33 +1,17 @@
 'use client';
 import BirthdayForm from './BirthdayForm.client';
-import GenderBirthdayForm from './GenderForm.client';
+import GenderForm from './GenderForm.client';
 import ImageForm from './ImageForm.client';
 import NicknameForm from './NicknameForm.client';
-import { useJoinContext } from '../../../components/JoinContext';
-import { BottomFixedButton } from '@/components/common/Button';
 import { Spacing } from '@/components/common/Spacing';
 
 export default function FormSection() {
-  const {
-    formState: { isDirty, isValid },
-  } = useJoinContext();
-
   return (
-    <section>
+    <section className="flex flex-col gap-10">
       <ImageForm />
       <NicknameForm />
-
-      <Spacing size={10} />
-
       <BirthdayForm />
-
-      <Spacing size={10} />
-
-      <GenderBirthdayForm />
-
-      <Spacing size={10} />
-
-      <BottomFixedButton text={'aaa'} disabled={true} />
+      <GenderForm />
     </section>
   );
 }
