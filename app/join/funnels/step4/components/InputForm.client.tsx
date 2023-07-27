316bb5228085b15632b0fcebb65753e63e4fcaca
Kuesung Park <gueit214@naver.com>
2023-07-27 13:08:03 +0900

    refactor : form으로 submit 옮기기

--- app/join/funnels/step4/components/InputForm.client.tsx
+++ app/join/funnels/step4/components/InputForm.client.tsx
@@ -4,15 +4,31 @@ import GenderSection from './GenderSection.client';
 import ImageSection from './ImageSection.client';
 import NicknameSection from './NicknameSection.client';
 import SubmitSection from './SubmitSection.client';
+import { useFunnelContext } from '../../JoinFunnel';
+import { useJoinContext } from '@/app/join/components/JoinContext';
 
 export default function InputForm() {
+  const { getValues, handleSubmit } = useJoinContext();
+  const { nextStep } = useFunnelContext();
+  const onSubmit = () => {
+    const isAllTyped = !!(
+      getValues('nickname') &&
+      getValues().birth.year &&
+      getValues().birth.month &&
+      getValues().birth.date &&
+      getValues().gender
+    );
+    if (!isAllTyped) return;
+    nextStep();
+  };
+
   return (
-    <section className="flex flex-col gap-10">
+    <form className="flex flex-col gap-10" onSubmit={handleSubmit(onSubmit)}>
       <ImageSection />
       <NicknameSection />
       <BirthdaySection />
       <GenderSection />
       <SubmitSection />
-    </section>
+    </form>
   );
 }
--- app/join/funnels/step4/components/SubmitSection.client.tsx
+++ app/join/funnels/step4/components/SubmitSection.client.tsx
@@ -1,27 +1,14 @@
-import { useFunnelContext } from '../../JoinFunnel';
 import { useJoinContext } from '@/app/join/components/JoinContext';
 import { BottomFixedButton } from '@/components/common/Button';
 
 export default function SubmitSection() {
-  const { watch, getValues } = useJoinContext();
-  const { nextStep } = useFunnelContext();
-  const onSubmit = () => {
-    const isAllTyped = !!(
-      getValues('nickname') &&
-      getValues().birth.year &&
-      getValues().birth.month &&
-      getValues().birth.date &&
-      getValues().gender
-    );
-    if (!isAllTyped) return;
-    nextStep();
-  };
+  const { watch } = useJoinContext();
 
   return (
     <BottomFixedButton
       text={watch('gender') ? '완료' : '다음'}
       disabled={!watch('gender')}
-      onClick={onSubmit}
+      type="submit"
     />
   );
 }
