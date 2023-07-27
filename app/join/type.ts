bd55fb4240c6b623edea31c5deec26a941c328dd
Kuesung Park <gueit214@naver.com>
2023-07-27 10:52:13 +0900

    refactor : type 상속

--- app/join/type.ts
+++ app/join/type.ts
@@ -1,18 +1,12 @@
-import { BirthdayValueType, GenderType } from '@/types';
+import { SignUpRequest } from '@/apis/auth';
 
-export interface SignUpState {
+import type { BirthdayValueType } from '@/types';
+
+export type SignUpStateType = {
   certificateNumber?: number;
   certificateEmailNumber?: number;
 
-  phoneNumber: string;
-  imageUrl?: string;
-  schoolInfo: {
-    school: string;
-    email?: string;
-    certifiedStudent: boolean; // email이 없는 경우 false
-  };
-  nickname: string;
-  birth: BirthdayValueType;
-  gender: GenderType;
   personalityIdList: number[];
-}
+
+  birth: BirthdayValueType;
+} & Omit<SignUpRequest, 'personalities' | 'birth'>;
