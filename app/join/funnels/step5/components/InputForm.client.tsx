4826758e425e0a909a145e9b9a378bc029c0238c
Kuesung Park <gueit214@naver.com>
2023-07-27 15:25:19 +0900

    refactor : 회원가입 Request에 맞게 payload 수정

--- app/join/funnels/step5/components/InputForm.client.tsx
+++ app/join/funnels/step5/components/InputForm.client.tsx
@@ -5,12 +5,21 @@ import SubmitSection from './SubmitSection.client';
 import { useSignUpMutation } from '@/apis/auth';
 import { useJoinContext } from '@/app/join/components/JoinContext';
 import { SignUpState } from '@/app/join/type';
+import { personalityList } from '@/constants/personalityList';
 
 export default function InputForm() {
   const { handleSubmit } = useJoinContext();
   const { mutate: mutateSignUp } = useSignUpMutation();
-  const onSubmit = async (data: Pick<SignUpState, 'personalityIdList'>) => {
-    // mutateSignUp(DUMMY_SIGN_UP_DATA);
+  const onSubmit = async (data: SignUpState) => {
+    const { certificateEmailNumber, certificateNumber, birth, gender, personalityIdList, ...rest } =
+      data;
+    const signUpRequest = {
+      ...rest,
+      birth: `${birth.year}-${birth.month}-${birth.date}`,
+      gender: gender === '남성' ? 'MALE' : 'FEMALE',
+      personalities: personalityIdList.map((id) => personalityList[id - 1].keywordInEnglish),
+    };
+    // mutateSignUp(signUpRequest);
     // router.push('/grouping');
   };
 
