624128c4d5d18d9fcef60cd027bd27a41b48bf59
Kuesung Park <gueit214@naver.com>
2023-07-27 20:44:17 +0900

    refactor : 선택없음 타입 추가

--- app/join/funnels/step4/components/GenderSection.client.tsx
+++ app/join/funnels/step4/components/GenderSection.client.tsx
@@ -5,18 +5,12 @@ import { Input } from '@/components/common/Input';
 import { BottomSheet } from '@/components/common/Modal';
 import GenderSwipePicker from '@/components/common/SwipePicker/GenderSwipePicker';
 import useModalStore from '@/store/useModalStore';
+import { useState } from 'react';
 
 export default function GenderSection() {
   const { modalName, openModal, closeModal } = useModalStore();
-  const { watch, setValue } = useJoinContext();
+  const { watch, setValue, getFieldState } = useJoinContext();
   const gender = watch('gender');
-
-  const isOpen = modalName === 'gender';
-
-  const handleNextButtonClick = () => {
-    if (!gender) setValue('gender', '남성');
-    closeModal();
-  };
 
   return (
     <section className="flex flex-col gap-5">
@@ -24,22 +18,30 @@ export default function GenderSection() {
       <Input
         placeholder="성별을 선택해주세요."
         onClick={() => openModal('gender')}
-        value={gender}
+        value={gender === '선택없음' ? '' : gender}
         readOnly
       />
       <BottomSheet
-        isOpen={isOpen}
+        isOpen={modalName === 'gender'}
         snap={400}
         onClose={closeModal}
         isRightButton
         text={<p className="font-500 text-18 text-gray7">성별</p>}
         disableDrag
       >
-        <GenderSwipePicker
-          genderValue={gender}
-          setGenderValue={(value) => setValue('gender', value)}
+        <div className="relative h-full">
+          <GenderSwipePicker
+            genderValue={gender}
+            setGenderValue={(value) => setValue('gender', value, { shouldTouch: true })}
+          />
+        </div>
+        <BottomFixedButton
+          text="완료"
+          onClick={() => {
+            if (gender === '선택없음') setValue('gender', '남성');
+            closeModal();
+          }}
         />
-        <BottomFixedButton text="완료" onClick={handleNextButtonClick} />
       </BottomSheet>
     </section>
   );
