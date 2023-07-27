9fe00a6d5c7cfc75c6be8b316491e555dab0427a
Kuesung Park <gueit214@naver.com>
2023-07-27 12:47:21 +0900

    refactor : SubmitSection 위치 수정

--- app/join/funnels/step3/components/EmailForm.client.tsx
+++ app/join/funnels/step3/components/EmailForm.client.tsx
@@ -1,15 +1,9 @@
 'use client';
 import EmailSection from './EmailSection.client';
 import SubmitSection from './SubmitSection.client';
-import { useJoinContext } from '../../../components/JoinContext';
-import { Spacing } from '@/components/common/Spacing';
 import { memo } from 'react';
 
 export default memo(function EmailForm() {
-  const {
-    formState: { errors },
-  } = useJoinContext();
-
   return (
     <form>
       <EmailSection />
--- app/join/funnels/step4/Step4Component.server.tsx
+++ app/join/funnels/step4/Step4Component.server.tsx
@@ -1,11 +1,9 @@
 import InputForm from './components/InputForm.client';
-import SubmitSection from './components/SubmitSection.client';
 
 export default function Step4Component() {
   return (
     <main>
       <InputForm />
-      <SubmitSection />
     </main>
   );
 }
