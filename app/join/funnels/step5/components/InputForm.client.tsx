f3aaacd32ab1578e5be2c54cd99a142a53c79dcb
Kuesung Park <gueit214@naver.com>
2023-07-27 13:10:51 +0900

    refactor : form으로 submit 옮기기 - step5

--- app/join/funnels/step5/components/InputForm.client.tsx
+++ app/join/funnels/step5/components/InputForm.client.tsx
@@ -2,10 +2,20 @@
 
 import PersonalitySection from './PersonalitySection.client';
 import SubmitSection from './SubmitSection.client';
+import { useSignUpMutation } from '@/apis/auth';
+import { useJoinContext } from '@/app/join/components/JoinContext';
+import { SignUpStateType } from '@/app/join/type';
 
 export default function InputForm() {
+  const { handleSubmit } = useJoinContext();
+  const { mutate: mutateSignUp } = useSignUpMutation();
+  const onSubmit = async (data: Pick<SignUpStateType, 'personalityIdList'>) => {
+    // mutateSignUp(DUMMY_SIGN_UP_DATA);
+    // router.push('/grouping');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <PersonalitySection />
       <SubmitSection />
     </form>
