7ceb8efbfd190778ff2970634168eeb16416426b
Kuesung Park <gueit214@naver.com>
2023-07-27 10:22:52 +0900

    refactor : type import

--- app/join/components/JoinContentTextMessage.server.tsx
+++ app/join/components/JoinContentTextMessage.server.tsx
@@ -1,4 +1,4 @@
-import { StrictPropsWithChildren } from '@/types';
+import type { StrictPropsWithChildren } from '@/types';
 
 export default function JoinContentTextMessage({ children }: StrictPropsWithChildren) {
   return <p className="text-14 text-gray2">{children}</p>;
