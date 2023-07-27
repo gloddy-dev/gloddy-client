f7a7fc3cb0425b00d4030743167ef314b2888102
Kuesung Park <gueit214@naver.com>
2023-07-27 14:54:11 +0900

    refactor : 다음 step 넘어가는 로직

--- app/join/funnels/step2/components/InputForm.client.tsx
+++ app/join/funnels/step2/components/InputForm.client.tsx
@@ -1,9 +1,18 @@
 import AgreeSection from './AgreeForm.client';
 import SchoolSection from './SchoolSection.client';
+import { useFunnelContext } from '../../JoinFunnel';
+import { useJoinContext } from '@/app/join/components/JoinContext';
+import { SignUpState } from '@/app/join/type';
 
 export default function InputForm() {
+  const { handleSubmit } = useJoinContext();
+  const { nextStep } = useFunnelContext();
+  const onSubmit = (data: Pick<SignUpState, 'schoolInfo'>) => {
+    nextStep();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <SchoolSection />
       <AgreeSection />
     </form>
