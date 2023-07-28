0d61bb7bf064f3efe52683fe3ac07302a19481f0
Kuesung Park <gueit214@naver.com>
2023-07-28 21:35:58 +0900

    feat : useDidMount 테스트 코드 작성

---
+++ hooks/common/useDidMount/index.test.ts
@@ -0,0 +1,9 @@
+import { describe } from 'node:test';
+
+import useDidMount from './useDidMount';
+
+describe('hooks/common/useDidMount/useDidMount', () => {
+  it('should defined with default', () => {
+    expect(useDidMount).toBeDefined();
+  });
+});
