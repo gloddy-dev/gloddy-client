ed75eb88a9185c64bd214ec26d387e2b1545dc9d
Kuesung Park <gueit214@naver.com>
2023-07-28 14:40:11 +0900

    fix : 토큰으로 헤더 설정

--- apis/config/interceptor.client.ts
+++ apis/config/interceptor.client.ts
@@ -3,11 +3,12 @@ import { InternalAxiosRequestConfig } from 'axios';
 
 export const onRequestClient = async (config: InternalAxiosRequestConfig) => {
   try {
-    const data = await postLogin();
-    const token = data.token;
+    const { token } = await postLogin({
+      phoneNumber: '01012345678',
+    });
 
     if (token) {
-      config.headers['X-AUTH-TOKEN'] = token;
+      config.headers['X-AUTH-TOKEN'] = token.accessToken;
       return config;
     }
     throw new Error('로그인이 필요합니다.');
