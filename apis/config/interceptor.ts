6fdfb849878d499523708bcc5da8b6562b855c9e
Kuesung Park <gueit214@naver.com>
2023-07-31 18:06:28 +0900

    refactor : 불필요 코드 제거

--- apis/config/interceptor.ts
+++ apis/config/interceptor.ts
@@ -16,19 +16,10 @@ export const onResponseError = (error: AxiosError<ErrorType, InternalAxiosReques
     if (error.response) {
       const data = error.response.data;
       const { message, reason, status, timestamp } = data;
-
-      if (status === 401) {
-        // TODO: 토큰 재발급 로직
-      }
-      if (status === 400) {
-        return Promise.reject(message);
-      }
       return Promise.reject(new ApiError(message, reason, status, timestamp));
     }
     if (error.request) {
       // 요청이 이루어 졌으나 응답을 받지 못했습니다.
-      // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
-      // Node.js의 http.ClientRequest 인스턴스입니다.
       console.log(error.request);
     } else {
       // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
