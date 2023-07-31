04e47cee38f08436e9951d2b8a7942bd51b0d087
Kuesung Park <gueit214@naver.com>
2023-07-31 11:13:55 +0900

    refactor : Layout 컴포넌트 분리

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
+import Layout from '@/app/components/Layout';
 import './globals.css';
 
-import Provider from './provider';
+import QueryProvider from './components/QueryProvider.client';
 
 import type { Metadata } from 'next';
+
+import { UserProvider } from './components/UserProvider.client';
 
 export const metadata: Metadata = {
   title: 'Gloddy',
@@ -44,14 +47,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
-      <body className="flex h-[100dvh] w-screen justify-center bg-slate-50">
-        <Provider>
-          <div className="relative h-full w-full max-w-450 overflow-y-scroll bg-white text-black">
-            {children}
-          </div>
-        </Provider>
-      </body>
-    </html>
+    <Layout>
+      <QueryProvider>
+        <UserProvider>{children}</UserProvider>
+      </QueryProvider>
+    </Layout>
   );
 }
