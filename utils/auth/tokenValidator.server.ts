e0a63b8171440ea9f94fb4d0526102dbec9f9625
Kuesung Park <gueit214@naver.com>
2023-07-31 13:16:32 +0900

    feat : token validator 구현

---
+++ utils/auth/tokenValidator.server.ts
@@ -0,0 +1,38 @@
+import { generateCookiesKeyValues } from '../auth';
+import { postReissue } from '@/apis/auth';
+import { ApiError } from '@/apis/config/customError';
+import { CookieKeyType } from '@/types';
+import { cookies } from 'next/headers';
+
+export const getAccessTokenServer = async (
+  authTokens: Partial<CookieKeyType>
+): Promise<string | null | ApiError> => {
+  try {
+    const { accessToken, refreshToken, userId } = authTokens;
+
+    if (accessToken && refreshToken) {
+      const response = await postReissue({ accessToken, refreshToken });
+      if (
+        response.token.accessToken === undefined ||
+        response.token.refreshToken === undefined ||
+        userId === undefined
+      ) {
+        return null;
+      }
+
+      const cookieStore = cookies();
+      for (const [cookieKey, cookieValue] of generateCookiesKeyValues({
+        accessToken: response.token.accessToken,
+        refreshToken: response.token.refreshToken,
+        userId,
+      })) {
+        cookieStore.set(cookieKey as string, cookieValue as string);
+      }
+      return accessToken;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    return e as ApiError;
+  }
+};
