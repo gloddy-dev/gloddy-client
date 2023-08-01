c955df472561d07d9b4fa0861da679a82ab85ecf
Kuesung Park <gueit214@naver.com>
2023-08-01 14:38:55 +0900

    refactor : GLODDY_RT 상수화

---
+++ constants/token.ts
@@ -0,0 +1,6 @@
+export const AUTH_COOKIE_KEYS: Record<string, string> = {
+  accessToken: 'gloddy_at',
+  refreshToken: 'gloddy_rt',
+  userId: 'gloddy_uid',
+} as const;
+export const GLODDY_RT = 'GLODDY_RT';
