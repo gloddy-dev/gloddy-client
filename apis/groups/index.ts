6b823987fa6dc6659135b5207d9cf3eda1053e0b
Kuesung Park <gueit214@naver.com>
2023-08-01 09:16:06 +0900

    fix : index.ts로 묶어서 export해서 발생하는 에러 해결

--- apis/groups/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './apis.client';
-export * from './apis.server';
-export * from './keys';
-export * from './mutations';
-export * from './queries';
-export * from './type';
--- apis/groups/mutations.ts
+++ apis/groups/mutations.ts
@@ -1,5 +1,5 @@
-import { postCreateGroup } from '.';
 import { useMutation } from '@tanstack/react-query';
+import { postCreateGroup } from './apis.client';
 
 export const usePostCreateGroup = () => {
   return useMutation(postCreateGroup, {});
