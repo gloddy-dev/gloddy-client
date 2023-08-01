c6d334503fffaa504be0c61f865c19fd8842db2d
Kuesung Park <gueit214@naver.com>
2023-08-01 12:39:04 +0900

    refactor : dummy data 한 파일에서 export

--- app/grouping/components/GroupingCardList.client.tsx
+++ app/grouping/components/GroupingCardList.client.tsx
@@ -2,60 +2,12 @@
 import GroupingCard from './GroupingCard.client';
 import { useGetGroups } from '@/apis/groups/queries.client';
 
-import type { Grouping } from '@/apis/groups/type';
-
-const DUMMY_GROUPING_DATA: Grouping[] = [
-  {
-    groupId: 1,
-    imageUrl: '/assets/location.svg',
-    title: 'Let’s go for a walk!',
-    content: 'It’s a group that walks around, talks, and learns languages.',
-    memberCount: 2,
-    maxUser: 2,
-    place: '동대문구 회기동',
-    meetDate: '04.27.FRI 7PM',
-    startTime: '13:00',
-    endTime: '15:00',
-    place_latitude: 37.579,
-    place_longitude: 127.056,
-  },
-  {
-    groupId: 2,
-    imageUrl: '/assets/location.svg',
-    title: 'Let’s go for a walk!',
-    content: 'It’s a group that walks around, talks, and learns languages.',
-    memberCount: 2,
-    maxUser: 3,
-    place: '동대문구 회기동',
-    meetDate: '04.27.FRI 7PM',
-    startTime: '13:00',
-    endTime: '15:00',
-    place_latitude: 37.579,
-    place_longitude: 127.056,
-  },
-  {
-    groupId: 3,
-    imageUrl: '/assets/location.svg',
-    title: 'Let’s go for a walk!',
-    content: 'It’s a group that walks around, talks, and learns languages.',
-    memberCount: 0,
-    maxUser: 5,
-    place: '동대문구 회기동',
-    meetDate: '04.27.FRI 7PM',
-    startTime: '13:00',
-    endTime: '15:00',
-    place_latitude: 37.579,
-    place_longitude: 127.056,
-  },
-];
-
 export default function GroupingCardList() {
   const { data } = useGetGroups(0);
-  console.log(data);
 
   return (
     <section className="mx-20 flex flex-col gap-8">
-      {DUMMY_GROUPING_DATA.map((groupingData) => (
+      {data?.contents.map((groupingData) => (
         <GroupingCard groupingData={groupingData} key={groupingData.groupId} />
       ))}
     </section>
--- app/join/funnels/step5/components/InputForm.client.tsx
+++ app/join/funnels/step5/components/InputForm.client.tsx
@@ -9,20 +9,7 @@ import { personalityList } from '@/constants/personalityList';
 import { useUser } from '@/hooks/useUser';
 import { useRouter } from 'next/navigation';
 
-const DUMMY_SIGN_UP_DATA = {
-  phoneNumber: '010-5728-9353',
-  imageUrl:
-    'https://gloddy.s3.ap-northeast-2.amazonaws.com/file/87d8b6c4-fcda-4588-8334-b3ca96e635a0.png',
-  schoolInfo: {
-    school: '가천대학교',
-    email: 'gueit214@gachon.ac.kr',
-    certifiedStudent: true,
-  },
-  nickname: 'string',
-  birth: '2023-07-22',
-  gender: 'MAIL',
-  personalities: ['OUTGOING'],
-};
+
 
 export default function InputForm() {
   const { handleSubmit, getValues } = useJoinContext();
