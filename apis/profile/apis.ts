ecb3c82340fbd5668dd7431fbf8c9e3727d8fb96
Kuesung Park <gueit214@naver.com>
2023-08-03 14:24:44 +0900

    refactor : get 추가

--- apis/profile/apis.ts
+++ apis/profile/apis.ts
@@ -1,4 +1,4 @@
 import { ProfileResponse } from './type';
 import privateApi from '../config/privateApi';
 
-export const getProfile = () => privateApi<ProfileResponse>('/me/page');
+export const getProfile = () => privateApi.get<ProfileResponse>('/me/page');
