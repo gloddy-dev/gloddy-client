b1810d11e630e6074b1c7334b1ffcb438d12e8f4
Kuesung Park <gueit214@naver.com>
2023-08-03 14:17:56 +0900

    feat : api 연결

--- app/profile/components/ProfileSection.client.tsx
+++ app/profile/components/ProfileSection.client.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { ProfileResponse } from '@/apis/profile';
 import ImageFrame from '@/components/common/ImageFrame';
+import { Spacing } from '@/components/common/Spacing';
 import DivisionBar from '@/components/common/Spacing/DivisionBar';
 import { useState } from 'react';
 
@@ -9,22 +10,14 @@ interface ProfileSectionProps {
 }
 export default function ProfileSection({ profileData }: ProfileSectionProps) {
   console.log(profileData);
-  const {
-    age,
-    gender,
-    imageUrl,
-    introduce,
-    name,
-    personlaities,
-    praiseCount,
-    reviewCount,
-    school,
-  } = profileData;
+  const { age, gender, imageUrl, name, praiseCount, reviewCount, school } = profileData;
 
   return (
     <section className="h-500 rounded-b-35 bg-white px-40">
+      <Spacing size={30} />
       <article className="flex flex-col items-center">
         <ImageFrame canChange={false} />
+        {/* TODO : 서버 측 이미지 에러로 defaultImageUrl 빼둔 상태 */}
         <p>
           <span className="text-20 font-700 leading-40">{name}</span>
           <span className="text-14 font-400 leading-40 text-gray2">님</span>
@@ -36,7 +29,7 @@ export default function ProfileSection({ profileData }: ProfileSectionProps) {
       <DivisionBar className="my-20" />
       <article className="flex flex-col">
         <p className="text-12 font-700">신뢰도 지표</p>
-        {/* 신뢰도 API 나오면 구현 예정 */}
+        {/* TODO : 신뢰도 API 나오면 구현 예정 */}
       </article>
       <DivisionBar className="my-20" />
       <article className="flex h-40">
@@ -47,12 +40,12 @@ export default function ProfileSection({ profileData }: ProfileSectionProps) {
         <DivisionBar direction="vertical" />
         <div className="flex flex-grow flex-col items-center">
           <p className="text-12">받은 칭찬</p>
-          <p className="text-14 font-700">111개</p>
+          <p className="text-14 font-700 text-blue">{praiseCount}개</p>
         </div>
         <DivisionBar direction="vertical" />
         <div className="flex flex-grow flex-col items-center">
           <p className="text-12">모임 후기</p>
-          <p className="text-14 font-700">111개</p>
+          <p className="text-14 font-700 text-blue">{reviewCount}개</p>
         </div>
       </article>
     </section>
