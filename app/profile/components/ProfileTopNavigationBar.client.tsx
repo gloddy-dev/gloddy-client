312d07953266286c7a05fdd7e3026cba1cae36cf
Kuesung Park <gueit214@naver.com>
2023-08-03 12:48:34 +0900

    refactor : isSpacing 추가

--- app/profile/components/ProfileTopNavigationBar.client.tsx
+++ app/profile/components/ProfileTopNavigationBar.client.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 export default function ProfileTopNavigationBar() {
   return (
     <TopNavigationBar
+      isSpacing={false}
       rightNode={<Image src="/assets/setting_black.svg" width={24} height={24} alt="setting" />}
     />
   );
