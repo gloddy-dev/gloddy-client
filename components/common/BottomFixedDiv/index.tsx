de90cbd34fa75af5a36f8629311710c05395f6b7
kang <kangju2000@naver.com>
2023-08-06 17:58:44 +0900

    feat: 게시글 상세 페이지 구현

--- components/common/BottomFixedDiv/index.tsx
+++ components/common/BottomFixedDiv/index.tsx
@@ -1,9 +1,17 @@
+import clsx from 'clsx';
+
 import type { StrictPropsWithChildren } from '@/types';
 
-export default function BottomFixedDiv({ children }: StrictPropsWithChildren) {
+export default function BottomFixedDiv({
+  children,
+  className = '',
+  ...props
+}: StrictPropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) {
   return (
     <div className="fixed inset-x-0 bottom-0 m-auto max-w-450">
-      <div className="px-20 pb-20">{children}</div>
+      <div className={clsx('px-20 pb-20', className)} {...props}>
+        {children}
+      </div>
     </div>
   );
 }
