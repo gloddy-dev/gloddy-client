a0e82e6d1cd8eca9df59eb69c0b10cbeda8d0ca9
kang <kangju2000@naver.com>
2023-08-06 16:05:17 +0900

    refactor: [id] -> [groupId]로 변경

---
+++ app/grouping/[groupId]/components/board/ArticleList.client.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { useGetArticles } from '@/apis/groups/queries';
+import ArticleItem from '@/app/grouping/components/ArticleItem.client';
+import { DUMMY_ARTICLES_DATA } from '@/constants/dummyData';
+import { useParams } from 'next/navigation';
+
+export default function ArticleList() {
+  const { groupId } = useParams() as { groupId: string };
+  const id = Number(groupId);
+
+  // const { data: articlesData } = useGetArticles(groupId);
+
+  // if (!articlesData.length) return <p>게시글이 없습니다.</p>;
+
+  return (
+    <div className="flex flex-col gap-15">
+      {DUMMY_ARTICLES_DATA.map((article) => (
+        <ArticleItem key={article.articleId} article={article} />
+      ))}
+    </div>
+  );
+}
