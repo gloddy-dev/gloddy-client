81dd71b7d8a93eb026ec3461a069cb7448310442
Kuesung Park <gueit214@naver.com>
2023-08-06 21:20:40 +0900

    refactor : privateFetch 중복 코드 제거

--- apis/config/privateFetch.ts
+++ apis/config/privateFetch.ts
@@ -12,30 +12,6 @@ class PrivateFetch extends PublicFetch {
       },
     });
   }
-  async get<T>(route: string, requestInit?: RequestInit) {
-    return this.common<T>(route, {
-      method: 'GET',
-      ...(requestInit ?? {}),
-    });
-  }
-  async post<T>(route: string, requestInit?: RequestInit) {
-    return this.common<T>(route, {
-      method: 'POST',
-      ...(requestInit ?? {}),
-    });
-  }
-  async put<T>(route: string, requestInit?: RequestInit) {
-    return this.common<T>(route, {
-      method: 'PUT',
-      ...(requestInit ?? {}),
-    });
-  }
-  async delete<T>(route: string, requestInit?: RequestInit) {
-    return this.common<T>(route, {
-      method: 'DELETE',
-      ...(requestInit ?? {}),
-    });
-  }
 }
 const privateFetch = new PrivateFetch();
 
