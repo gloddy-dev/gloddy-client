c1c278931722284e473bbf9575d00c5b53791b8b
Kuesung Park <gueit214@naver.com>
2023-08-06 18:38:03 +0900

    refactor : if-else문 제거하여 보다 직관적으로 수정

--- middleware.ts
+++ middleware.ts
@@ -6,24 +6,22 @@ const middleware = async (request: NextRequest) => {
   const accessToken = request.cookies.get(AUTH_KEYS.accessToken)?.value;
   const refreshToken = request.cookies.get(AUTH_KEYS.refreshToken)?.value;
 
-  if (accessToken && refreshToken) {
-    try {
-      const {
-        token: { accessToken: reIssuedAccessToken, refreshToken: reIssuedRefreshToken },
-      } = await postReissue(
-        { accessToken, refreshToken },
-        { headers: { 'X-AUTH-TOKEN': accessToken } }
-      );
-      const response = NextResponse.next();
-      response.cookies.set(AUTH_KEYS.accessToken, reIssuedAccessToken);
-      response.cookies.set(AUTH_KEYS.refreshToken, reIssuedRefreshToken);
-      return response;
-    } catch (e) {
-      console.log(e);
-      return NextResponse.redirect(new URL('/join', request.nextUrl.origin));
-    }
-  } else {
+  if (!accessToken || !refreshToken)
     return NextResponse.redirect(new URL('join', request.nextUrl.origin));
+  try {
+    const {
+      token: { accessToken: reIssuedAccessToken, refreshToken: reIssuedRefreshToken },
+    } = await postReissue(
+      { accessToken, refreshToken },
+      { headers: { 'X-AUTH-TOKEN': accessToken } }
+    );
+    const response = NextResponse.next();
+    response.cookies.set(AUTH_KEYS.accessToken, reIssuedAccessToken);
+    response.cookies.set(AUTH_KEYS.refreshToken, reIssuedRefreshToken);
+    return response;
+  } catch (e) {
+    console.log(e);
+    return NextResponse.redirect(new URL('/join', request.nextUrl.origin));
   }
 };
 
