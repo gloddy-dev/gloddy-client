6a905c56d28ef6b681a2d16c2438e3c802fe5aa5
Kuesung Park <gueit214@naver.com>
2023-08-07 12:01:56 +0900

    feat : mate 페이지 구현

---
+++ app/profile/mates/components/ProfileMatesDetail.client.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { formatRelativeDate } from '../util';
+import { MatesResponse, useGetMates } from '@/apis/profile';
+import { Spacing } from '@/components/common/Spacing';
+import { DUMMY_PROFILE_MATES_DATA } from '@/constants/dummyData';
+import Image from 'next/image';
+
+export default function ProfileMatesDetail() {
+  const {
+    data: { mates: matesData },
+  } = useGetMates();
+
+  return (
+    <main className="px-20">
+      {DUMMY_PROFILE_MATES_DATA.map((mateData) => (
+        <ProfileMate key={mateData.createdAt} mateData={mateData} />
+      ))}
+    </main>
+  );
+}
+
+interface ProfileMateProps {
+  mateData: MatesResponse['mates'][0];
+}
+
+function ProfileMate({ mateData }: ProfileMateProps) {
+  return (
+    <div className="mt-14 rounded-8 bg-gray6 p-16" key={mateData.createdAt}>
+      <section className="relative flex">
+        <Image alt="profile" src={mateData.mateImageUrl} width={30} height={30} />
+        <Spacing size={16} direction="horizontal" />
+        <div>
+          <p className="text-16 font-700">{mateData.mateName}</p>
+          <p className="text-10 text-gray2">
+            {mateData.school} | {formatRelativeDate(mateData.createdAt)}
+          </p>
+        </div>
+        <Image
+          alt="more"
+          src="/assets/more.svg"
+          width={3}
+          height={10}
+          className="absolute right-0"
+        />
+      </section>
+      <Spacing size={10} />
+      <section className="rounded-8 bg-white p-16 text-10 leading-20">
+        {mateData.selectionReason}
+      </section>
+    </div>
+  );
+}
