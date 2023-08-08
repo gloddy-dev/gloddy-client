1f5ee0c473c5a1b0403673e9a9761404c9f1f67f
Kuesung Park <gueit214@naver.com>
2023-08-08 12:40:43 +0900

    fix : priority 속성 부여 (에러 해결)

--- app/grouping/components/FloatingBubbleSection.server.tsx
+++ app/grouping/components/FloatingBubbleSection.server.tsx
@@ -6,7 +6,7 @@ export default function FloatingBubbleSection() {
   return (
     <section className="fixed inset-x-0 bottom-80 mx-auto flex max-w-450 justify-end">
       <Link href="/grouping/create" className="relative h-50 w-50">
-        <Image src="/assets/plus.svg" alt="add_icon" fill />
+        <Image src="/assets/plus.svg" alt="add_icon" fill priority />
       </Link>
       <Spacing size={20} direction="horizontal" />
     </section>
