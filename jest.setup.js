b54bb8adfb848b7b0b0d0fd051e90bc40f219f32
Kuesung Park <gueit214@naver.com>
2023-08-09 12:06:37 +0900

    refactor : 불필요한 설정 코드 제거

--- jest.setup.js
+++ jest.setup.js
@@ -1,16 +1,3 @@
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-plugin-context/setup';
-
-jest.mock('next/router', () => require('next-router-mock'));
-
-window.matchMedia = (query) => ({
-  matches: false,
-  media: query,
-  onchange: null,
-  addListener: jest.fn(), // deprecated
-  removeListener: jest.fn(), // deprecated
-  addEventListener: jest.fn(),
-  removeEventListener: jest.fn(),
-  dispatchEvent: jest.fn(),
-});
