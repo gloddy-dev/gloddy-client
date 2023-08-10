b801585fc6d6bf66dc4da2e77fc38f272c80b96c
Kuesung Park <gueit214@naver.com>
2023-08-10 18:47:29 +0900

    add : typography 추가

---
+++ style/theme/fontSizes.ts
@@ -0,0 +1,82 @@
+export const fontSizes = {
+  h1: [
+    '3rem',
+    {
+      lineHeight: '4.25rem',
+      fontWeight: 'bold',
+      letterSpacing: '-0.025rem',
+    },
+  ],
+  h2: [
+    '2rem',
+    {
+      lineHeight: '3rem',
+      fontWeight: 'bold',
+      letterSpacing: '-0.0187rem',
+    },
+  ],
+  h3: [
+    '1.5rem',
+    {
+      lineHeight: '2.25rem',
+      fontWeight: 'bold',
+      letterSpacing: '-0.0125rem',
+    },
+  ],
+  h4: [
+    '1.25rem',
+    {
+      lineHeight: '1.875rem',
+      fontWeight: 'bold',
+      letterSpacing: '-0.0063rem',
+    },
+  ],
+  'subtitle-1': [
+    '1.125rem',
+    {
+      lineHeight: '1.625rem',
+      fontWeight: '500',
+      letterSpacing: '-0.0063rem',
+    },
+  ],
+  'subtitle-2': [
+    '1rem',
+    {
+      lineHeight: '1.5rem',
+      fontWeight: '500',
+      letterSpacing: '-0.0063rem',
+    },
+  ],
+  'subtitle-3': [
+    '0.875rem',
+    {
+      lineHeight: '1.375rem',
+      fontWeight: '500',
+      letterSpacing: '0rem',
+    },
+  ],
+  'paragraph-1': [
+    '1rem',
+    {
+      lineHeight: '1.5rem',
+      fontWeight: '400',
+      letterSpacing: '-0.0063rem',
+    },
+  ],
+  'paragraph-2': [
+    '0.875rem',
+    {
+      lineHeight: '1.375rem',
+      fontWeight: '400',
+      letterSpacing: '-0.0063rem',
+    },
+  ],
+  caption: [
+    '0.75rem',
+    {
+      lineHeight: '1.125rem',
+      fontWeight: '500',
+      letterSpacing: '0.0125rem',
+    },
+  ],
+};
