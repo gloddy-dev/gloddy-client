b32ebf89c38a09c6104e27f966bcef9c131cae01
kang <kangju2000@naver.com>
2023-08-11 17:16:00 +0900

    refactor: button 컴포넌트에 outline-warning color 추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import cn from '@/utils/cn';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'small' | 'medium';
-  color?: 'solid-primary' | 'solid-default' | 'solid-warning';
+  color?: 'solid-primary' | 'solid-default' | 'outline-warning' | 'solid-warning';
   children: React.ReactNode;
 }
 
@@ -20,9 +20,12 @@ export default function Button({
         'flex items-center justify-center rounded-8 px-24 py-16 text-subtitle-2',
         {
           'h-56': size === 'medium',
+          'h-48': size === 'small',
           'bg-primary text-sign-white disabled:bg-primary-light': color === 'solid-primary',
           'bg-button text-sign-secondary disabled:bg-sub disabled:text-sign-caption':
             color === 'solid-default',
+          'border border-warning bg-warning-color text-warning disabled:border-warning-light disabled:bg-white disabled:text-warning-light':
+            color === 'outline-warning',
           'bg-warning text-sign-white disabled:bg-sub disabled:text-sign-caption':
             color === 'solid-warning',
         },
