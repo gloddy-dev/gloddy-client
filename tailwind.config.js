baa681f414dc106e9082402663241d77cbd36d65
kang <kangju2000@naver.com>
2023-08-11 13:18:37 +0900

    fix: tailwind 설정 범위 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const { colors, pxToRemTailwind, animations, fontSizes } = require('./src/style/
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       ...pxToRemTailwind,
