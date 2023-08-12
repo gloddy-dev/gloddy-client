de3886eadacd519bf45884c2dbe559e9ffac1b3e
Kuesung Park <gueit214@naver.com>
2023-08-12 14:51:16 +0900

    refactor : isLeftError, isRightError 구분

--- src/app/join/funnels/step4/components/NicknameSection.client.tsx
+++ src/app/join/funnels/step4/components/NicknameSection.client.tsx
@@ -2,28 +2,43 @@
 import { useJoinContext } from '../../../components/JoinContext';
 import { Input } from '@/components/common/Input';
 import { Spacing } from '@/components/common/Spacing';
+import { TextField } from '@/components/TextField';
+import TextFieldInput from '@/components/TextField/TextFieldInput';
 
 export default function NicknameSection() {
-  const {
-    register,
-    formState: { errors },
-  } = useJoinContext();
+  const hookForm = useJoinContext();
+  const { register, watch, formState } = hookForm;
+  const leftCaption =
+    formState.errors.nickname?.message ?? '*최소 3글자 이상 15자 이하로 작성해주세요.';
 
   return (
     <section>
       <p className="text-14">닉네임</p>
       <Spacing size={5} />
-      <Input
+      <TextFieldInput
+        placeholder="닉네임을 입력해주세요."
+        hookForm={hookForm}
+        register={register('nickname', {
+          required: true,
+          pattern: {
+            value: /^[a-zA-Z0-9ㄱ-ㅎ가-힣]{3,15}$/,
+            message: '닉네임은 3~20자의 한글, 영문, 숫자만 사용 가능합니다.',
+          },
+        })}
+        leftCaption="*최소 3글자 이상 15자 이하로 작성해주세요."
+        maxCount={15}
+      />
+      {/* <TextField
         placeholder="닉네임을 입력해주세요."
         register={register('nickname', {
           required: true,
           pattern: {
-            value: /^[a-zA-Z0-9ㄱ-ㅎ가-힣]{3,20}$/,
+            value: /^[a-zA-Z0-9ㄱ-ㅎ가-힣]{3,15}$/,
             message: '닉네임은 3~20자의 한글, 영문, 숫자만 사용 가능합니다.',
           },
         })}
-        errorMessage={errors.nickname?.message}
-      />
+        leftCaption={leftCaption}
+      /> */}
     </section>
   );
 }
