b050923746e96709150885bc2554657115397721
Kuesung Park <gueit214@naver.com>
2023-08-13 16:16:11 +0900

    feat : children, props 그대로 넘겨주기

--- src/components/Layout/Flex.tsx
+++ src/components/Layout/Flex.tsx
@@ -8,45 +8,58 @@ interface FlexProps {
   justify?: 'center' | 'start' | 'end' | 'between' | 'around' | 'evenly' | 'stretch';
   align?: 'center' | 'start' | 'end' | 'between' | 'around' | 'evenly' | 'stretch';
   wrap?: 'wrap' | 'nowrap' | 'wrap-reverse';
+  className?: string;
 }
 
 export default forwardRef(function Flex(
-  { children, direction, justify, align, wrap }: StrictPropsWithChildren<FlexProps>,
+  {
+    children,
+    direction,
+    justify,
+    align,
+    wrap,
+    className,
+    ...props
+  }: StrictPropsWithChildren<FlexProps>,
   ref: Ref<HTMLDivElement>
 ) {
   return (
     <div
       ref={ref}
-      className={cn(
-        'flex',
-        {
-          'justify-center': justify === 'center',
-          'justify-start': justify === 'start',
-          'justify-end': justify === 'end',
-          'justify-between': justify === 'between',
-          'justify-around': justify === 'around',
-          'justify-evenly': justify === 'evenly',
-          'justify-stretch': justify === 'stretch',
-        },
-        {
-          'items-center': align === 'center',
-          'items-start': align === 'start',
-          'items-end': align === 'end',
-          'items-between': align === 'between',
-          'items-around': align === 'around',
-          'items-evenly': align === 'evenly',
-          'items-stretch': align === 'stretch',
-        },
-        {
-          'flex-row': direction === 'row',
-          'flex-column': direction === 'column',
-        },
-        {
-          'flex-wrap': wrap === 'wrap',
-          'flex-nowrap': wrap === 'nowrap',
-          'flex-wrap-reverse': wrap === 'wrap-reverse',
-        }
-      )}
+      className={
+        (cn(
+          'flex',
+          {
+            'justify-center': justify === 'center',
+            'justify-start': justify === 'start',
+            'justify-end': justify === 'end',
+            'justify-between': justify === 'between',
+            'justify-around': justify === 'around',
+            'justify-evenly': justify === 'evenly',
+            'justify-stretch': justify === 'stretch',
+          },
+          {
+            'items-center': align === 'center',
+            'items-start': align === 'start',
+            'items-end': align === 'end',
+            'items-between': align === 'between',
+            'items-around': align === 'around',
+            'items-evenly': align === 'evenly',
+            'items-stretch': align === 'stretch',
+          },
+          {
+            'flex-row': direction === 'row',
+            'flex-column': direction === 'column',
+          },
+          {
+            'flex-wrap': wrap === 'wrap',
+            'flex-nowrap': wrap === 'nowrap',
+            'flex-wrap-reverse': wrap === 'wrap-reverse',
+          }
+        ),
+        className)
+      }
+      {...props}
     >
       {children}
     </div>
