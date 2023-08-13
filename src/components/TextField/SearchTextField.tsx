f0f7227e78c22543a7242dc57528b1b0f0665429
Kuesung Park <gueit214@naver.com>
2023-08-13 16:02:35 +0900

    feat : props 넘겨주기

--- src/components/TextField/SearchTextField.tsx
+++ src/components/TextField/SearchTextField.tsx
@@ -1,10 +1,11 @@
-import TextField from './TextField';
+import TextField, { TextFieldProps } from './TextField';
 import Image from 'next/image';
 
-export default function SearchTextField() {
+export default function SearchTextField({ ...props }: TextFieldProps) {
   return (
     <TextField
       leftInputIcon={<Image src="/icons/24/search.svg" width={24} height={24} alt="search" />}
+      {...props}
     />
   );
 }
