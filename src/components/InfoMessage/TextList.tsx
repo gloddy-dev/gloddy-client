82824caae4a2a95cba0d5d6db7a175b82653cf85
Kuesung Park <gueit214@naver.com>
2023-08-13 12:08:27 +0900

    feat : variant별 분기처리

--- src/components/InfoMessage/TextList.tsx
+++ src/components/InfoMessage/TextList.tsx
@@ -2,18 +2,38 @@ import { Spacing } from '../common/Spacing';
 import { StrictPropsWithChildren } from '@/types';
 import Image from 'next/image';
 
+type VariantKeys = 'info' | 'caption' | 'subtitle' | 'grade';
+
 interface TextListProps {
   /**
    * 텍스트의 유형을 설정합니다. (default: info)
    */
-  variant?: 'info' | 'info-icon' | 'caption' | 'subtitle' | 'grade';
+  variant?: VariantKeys;
 }
 
-export default function TextList({ children }: StrictPropsWithChildren<TextListProps>) {
+type VariantPrefix = Record<Partial<VariantKeys>, { prefix?: React.ReactNode; margin?: number }>;
+
+const variantPrefix: VariantPrefix = {
+  info: {
+    prefix: <Image src="/icons/4/dot.svg" width={4} height={4} alt="dot" />,
+    margin: 8,
+  },
+  caption: {
+    prefix: <Image src="/icons/16/info.svg" width={16} height={16} alt="info" />,
+    margin: 4,
+  },
+  subtitle: {},
+  grade: {},
+};
+
+export default function TextList({
+  children,
+  variant = 'info',
+}: StrictPropsWithChildren<TextListProps>) {
   return (
     <div className="flex">
-      <Image src="/icons/16/info.svg" width={20} height={20} alt="info" />
-      <Spacing size={4} direction="horizontal" />
+      {variantPrefix[variant].prefix}
+      <Spacing size={variantPrefix[variant].margin || 0} direction="horizontal" />
       <p className="text-caption">{children}</p>
     </div>
   );
