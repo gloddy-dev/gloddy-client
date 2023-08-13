c689f673055db63380fa38680c1ef5b24514d2e0
Kuesung Park <gueit214@naver.com>
2023-08-13 18:40:44 +0900

    refactor : isFocus를 컴포넌트 바깥에서 컨트롤

--- src/components/TextField/TextFieldController.tsx
+++ src/components/TextField/TextFieldController.tsx
@@ -1,5 +1,8 @@
 import TextField, { type TextFieldProps } from './TextField';
+import { useOnClickInside } from '@/hooks/useOnClickInside';
+import { useOnClickOutside } from '@/hooks/useOnClickOutside';
 import Image from 'next/image';
+import { forwardRef, useRef, useState } from 'react';
 
 import type { UseFormRegisterReturn, UseFormReturn } from 'react-hook-form';
 
@@ -29,15 +32,29 @@ export default function TextFieldController({
   timer,
   ...TextFieldProps
 }: TextFieldControllerProps) {
+  const textFieldRef = useRef<HTMLDivElement>(null);
+
+  const [isFocus, setIsFocus] = useState(false);
+  const [isUserTouchOutsideOnce, setIsUserTouchOutsideOnce] = useState(false);
+
   const { formState, watch, setValue } = hookForm;
   const inputName = register.name;
   const errorMessage = formState.errors[inputName]?.message;
   const isRightError = maxCount ? watch(inputName).length > maxCount : false;
-  const isLeftError = !!errorMessage || isRightError;
+  const isLeftError = isUserTouchOutsideOnce && (!!errorMessage || isRightError);
 
   const isError = isRightError || isLeftError;
 
   const rightInputIconName = isError ? 'warning' : watch(inputName).length > 0 ? 'backspace' : '';
+
+  useOnClickOutside(textFieldRef, () => {
+    setIsUserTouchOutsideOnce(true);
+    setIsFocus(false);
+  });
+
+  useOnClickInside(textFieldRef, () => {
+    setIsFocus(true);
+  });
 
   return (
     <TextField
@@ -56,10 +73,12 @@ export default function TextFieldController({
           />
         )
       }
+      isFocus={isFocus}
       isLeftError={isLeftError}
       isRightError={isRightError}
+      register={register}
+      ref={textFieldRef}
       {...TextFieldProps}
-      {...register}
     />
   );
 }
---
+++ src/hooks/useOnClickInside.ts
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+export function useOnClickInside<T extends HTMLElement = HTMLElement>(
+  ref: React.RefObject<T>,
+  handler: (event: MouseEvent | TouchEvent) => void
+) {
+  useEffect(() => {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      event.stopPropagation();
+
+      if (ref.current && ref.current.contains(event.target as Node)) {
+        handler(event);
+      }
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+}
