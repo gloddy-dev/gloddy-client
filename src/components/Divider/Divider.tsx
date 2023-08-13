e8a2b9692ea04c7b52b12868856b056ecca7095a
kang <kangju2000@naver.com>
2023-08-13 18:17:34 +0900

    refactor: Divider 컴포넌트에 as prop 추가

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,7 +1,11 @@
 import cn from '@/utils/cn';
 import { memo } from 'react';
 
-interface DividerProps {
+interface DividerProps<T extends React.ElementType> {
+  /**
+   * HTML 요소를 지정합니다. (default: div)
+   */
+  as?: T;
   /**
    * 길이 조절 등 추가 스타일을 적용하고 싶을 때 사용합니다.
    */
@@ -20,15 +24,18 @@ interface DividerProps {
   flexItem?: boolean;
 }
 
-export default memo(function Divider({
+export default memo(function Divider<T extends React.ElementType>({
+  as,
   className,
   thickness = 'thin',
   direction = 'horizontal',
   flexItem = false,
   ...props
-}: DividerProps) {
+}: DividerProps<T> & React.ComponentPropsWithoutRef<T>) {
+  const Element = as ?? 'div';
+
   return (
-    <div
+    <Element
       className={cn(
         'h-auto w-auto flex-none bg-divider',
         {
