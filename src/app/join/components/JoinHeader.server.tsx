b67b534b364e4ec9e8b155c9f5103c222bc93482
Kuesung Park <gueit214@naver.com>
2023-08-13 10:39:14 +0900

    feat : Header 구현

---
+++ src/app/join/components/JoinHeader.server.tsx
@@ -0,0 +1,26 @@
+'use client';
+import Header from '@/components/Header/Header';
+import Image from 'next/image';
+
+interface JoinHeaderProps {
+  onPrevClick?: () => void;
+}
+
+export default function JoinHeader({ onPrevClick }: JoinHeaderProps) {
+  return (
+    <Header
+      leftNode={
+        <div className="flex items-center">
+          <Image
+            alt="back"
+            src="/icons/24/arrow_back.svg"
+            width={24}
+            height={24}
+            onClick={onPrevClick}
+          />
+          <p>회원가입</p>
+        </div>
+      }
+    />
+  );
+}
