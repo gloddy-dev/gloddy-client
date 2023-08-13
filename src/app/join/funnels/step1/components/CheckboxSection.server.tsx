c58f9125c26da4c88fd8da2e9e747dc221dee61f
Kuesung Park <gueit214@naver.com>
2023-08-13 09:48:29 +0900

    refactor : 제거

--- src/app/join/funnels/step1/components/CheckboxSection.server.tsx
+++ src/app/join/funnels/step1/components/CheckboxSection.server.tsx
@@ -1,5 +1,4 @@
 import { CircleCheckbox } from '@/components/common/Checkbox';
-import { InfoMessage } from '@/components/InfoMessage';
 
 export default function CheckboxSection() {
   return (
@@ -13,7 +12,6 @@ export default function CheckboxSection() {
         text={<span className=" text-[0.875rem]">휴대폰 번호는 어디에도 공개되지 않습니다.</span>}
         checked
       />
-      <InfoMessage>휴대폰 번호는 안전하게 보관됩니다.</InfoMessage>
     </section>
   );
 }
