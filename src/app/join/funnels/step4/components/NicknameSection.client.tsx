f5a5ec4e517417f202825ab75c98e1cad4c7f73c
Kuesung Park <gueit214@naver.com>
2023-08-14 11:05:52 +0900

    refactor : isSpacing에 따라 자리 차지 여부 판단

--- src/app/join/funnels/step4/components/NicknameSection.client.tsx
+++ src/app/join/funnels/step4/components/NicknameSection.client.tsx
@@ -18,10 +18,10 @@ export default function NicknameSection() {
           required: true,
           pattern: {
             value: /^[a-zA-Z0-9ㄱ-ㅎ가-힣]{3,15}$/,
-            message: '닉네임은 3~20자의 한글, 영문, 숫자만 사용 가능합니다.',
+            message: '* 최소 3글자 이상 15자 이하로 작성해주세요.',
           },
         })}
-        leftCaption="*최소 3글자 이상 15자 이하로 작성해주세요."
+        leftCaption="* 최소 3글자 이상 15자 이하로 작성해주세요."
         maxCount={15}
       />
     </section>
