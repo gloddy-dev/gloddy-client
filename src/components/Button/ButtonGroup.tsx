810083f340de933b252c535edc91a6b6423c0f65
kang <kangju2000@naver.com>
2023-08-14 18:22:42 +0900

    refactor: ButtonGroup 에러처리

--- src/components/Button/ButtonGroup.tsx
+++ src/components/Button/ButtonGroup.tsx
@@ -5,6 +5,37 @@ import { Children, type ReactElement, cloneElement, isValidElement } from 'react
 
 import type { StrictPropsWithChildren } from '@/types';
 
+const renderElements = (
+  elements: ReactElement[],
+  props: Array<React.ComponentProps<typeof Button>>
+) => {
+  if (elements.length === 1) {
+    return elements[0];
+  }
+
+  return (
+    <div className="flex gap-8">
+      {elements.map((element, index) => {
+        return cloneElement(element, {
+          className: cn(
+            {
+              'flex-shrink-0 w-auto': index === 0,
+            },
+            props[index].className
+          ),
+          variant: cn(
+            {
+              'solid-default': index === 0,
+              'solid-primary': index !== 0,
+            },
+            props[index].variant
+          ),
+        });
+      })}
+    </div>
+  );
+};
+
 interface ButtonGroupProps {
   /**
    * 버튼 그룹의 위치를 설정합니다. (default: bottom)
@@ -31,40 +62,20 @@ export default function ButtonGroup({
       ).name === 'Button'
   ) as ReactElement[];
 
+  if (validChildren.length === 0) {
+    throw new Error('ButtonGroup 컴포넌트는 Button 컴포넌트를 포함해야 합니다.');
+  }
+
+  if (validChildren.length > 2) {
+    throw new Error('ButtonGroup 컴포넌트는 2개 이하의 Button 컴포넌트를 포함해야 합니다.');
+  }
+
   const props = validChildren.map((child) => child.props as React.ComponentProps<typeof Button>);
 
   const buttonHeight = {
     small: 48,
     medium: 56,
   }[props[0].size ?? 'medium'];
-
-  const renderElements = (elements: ReactElement[]) => {
-    if (elements.length === 1) {
-      return elements[0];
-    }
-
-    return (
-      <div className="flex gap-8">
-        {elements.map((element, index) => {
-          return cloneElement(element, {
-            className: cn(
-              {
-                'flex-shrink-0 w-auto': index === 0,
-              },
-              props[index].className
-            ),
-            variant: cn(
-              {
-                'solid-default': index === 0,
-                'solid-primary': index !== 0,
-              },
-              props[index].variant
-            ),
-          });
-        })}
-      </div>
-    );
-  };
 
   return (
     <>
@@ -74,7 +85,7 @@ export default function ButtonGroup({
           'fixed inset-x-0 bottom-0 z-50 max-w-450': position === 'bottom',
         })}
       >
-        {renderElements(validChildren)}
+        {renderElements(validChildren, props)}
       </div>
     </>
   );
