0acaddd9f6d3831b01dd98604f0d5ba9c63768b5
Kuesung Park <gueit214@naver.com>
2023-08-14 13:04:00 +0900

    refactor : 수정된 디자인에 맞게 Popup구현

--- src/components/Modal/Popup.client.tsx
+++ src/components/Modal/Popup.client.tsx
@@ -1,22 +1,64 @@
 import ModalWrapper from './ModalWrapper';
-
-import type { PropsWithChildren } from 'react';
+import { Button } from '../Button';
+import { Spacing } from '../common/Spacing';
+import { StrictPropsWithChildren } from '@/types';
 
 interface PopupProps {
+  children?: React.ReactNode;
   isOpen: boolean;
-  onClickOk?: () => void;
-  onClickCancel?: () => void;
+  onOkClick?: () => void;
+  okText?: string;
+  onCancelClick?: () => void;
+  cancelText?: string;
+  variant?: 'warning' | 'success';
 }
 
+const variantMap = {
+  warning: {
+    ok: 'solid-warning',
+    cancel: 'outline-warning',
+  },
+  success: {
+    ok: 'solid-primary',
+    cancel: 'solid-default',
+  },
+} as const;
+
 export default function Popup({
-  isOpen,
-  onClickOk,
-  onClickCancel,
   children,
-}: PropsWithChildren<PopupProps>) {
+  isOpen,
+  onOkClick,
+  okText = '네',
+  onCancelClick,
+  cancelText = '아니요',
+  variant = 'warning',
+}: StrictPropsWithChildren<PopupProps>) {
   return (
-    <ModalWrapper isOpen={isOpen} onClose={onClickCancel}>
-      {children}
+    <ModalWrapper isOpen={isOpen} onClose={onCancelClick}>
+      <div className="flex w-300 flex-col items-center rounded-16 bg-white px-16 py-12">
+        {children}
+        {onCancelClick && (
+          <div className="w-full">
+            <Button
+              variant={variantMap[variant].ok}
+              className="w-full"
+              size="small"
+              onClick={onOkClick}
+            >
+              {okText}
+            </Button>
+            <Spacing size={8} />
+            <Button
+              variant={variantMap[variant].cancel}
+              className="w-full"
+              size="small"
+              onClick={onCancelClick}
+            >
+              {cancelText}
+            </Button>
+          </div>
+        )}
+      </div>
     </ModalWrapper>
   );
 }
