ddd4ee3de6b144a324c2112a0408f5df35dc2c75
kang <kangju2000@naver.com>
2023-08-14 15:30:28 +0900

    refactor: memoization 추가

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,23 +1,33 @@
+import { StrictPropsWithChildren } from '@/types';
 import cn from '@/utils/cn';
+import { memo } from 'react';
 
-interface TagProps {
+interface TagProps<T> {
+  id: T;
+  isSelected: boolean;
   variant?: 'solid' | 'outline';
-  isSelected?: boolean;
-  children: React.ReactNode;
+  onSelected?: (id: T) => void;
 }
 
-export default function Tag({ children, variant = 'solid', isSelected = false }: TagProps) {
+export default memo(function Tag<T>({
+  id,
+  isSelected,
+  children,
+  onSelected,
+  variant = 'outline',
+}: StrictPropsWithChildren<TagProps<T>>) {
   return (
     <button
       // TODO: solid 디자인 추가 시 적용
-      className={cn('rounded-80 px-16 py-8', {
+      className={cn('h-40 rounded-full px-16 py-8 text-subtitle-2', {
         'border border-border-default bg-white text-sign-tertiary':
-          variant === 'solid' && !isSelected,
+          variant === 'outline' && !isSelected,
         'border border-primary-dark bg-brand-color text-primary-dark':
           variant === 'outline' && isSelected,
       })}
+      onClick={() => onSelected?.(id)}
     >
       {children}
     </button>
   );
-}
+}) as <T>(props: StrictPropsWithChildren<TagProps<T>>) => JSX.Element;
