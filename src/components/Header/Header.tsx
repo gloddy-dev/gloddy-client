5e0ff714927c25ce635b54eed81ced72c086407f
Kuesung Park <gueit214@naver.com>
2023-08-14 19:40:05 +0900

    refactor : text props 받기

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,38 @@
 import { Spacing } from '../common/Spacing';
+import cn from '@/utils/cn';
+import { HtmlHTMLAttributes } from 'react';
 
-interface HeaderProps {
+interface HeaderProps extends HtmlHTMLAttributes<HTMLHeadElement> {
   leftNode?: React.ReactNode;
   rightNode?: React.ReactNode;
   isSpacing?: boolean;
   text?: string;
+  className?: string;
 }
 
-export default function Header({ leftNode, rightNode, isSpacing = true, text }: HeaderProps) {
+export default function Header({
+  leftNode,
+  rightNode,
+  isSpacing = true,
+  text,
+  className,
+  ...props
+}: HeaderProps) {
   return (
     <>
-      <div className="fixed inset-x-0 z-50 mx-auto max-w-450 bg-transparent px-20">
-        <header className="flex h-48 items-center justify-between text-subtitle-1">
+      <header
+        className={cn(
+          'fixed inset-x-0 z-50 mx-auto flex h-48 max-w-450 items-center justify-between bg-white text-subtitle-1 text-sign-primary',
+          className
+        )}
+        {...props}
+      >
+        <div className="flex items-center">
           {leftNode ? leftNode : <div />}
-          {rightNode ? rightNode : <div />}
-        </header>
-      </div>
-
+          {text && <div>{text}</div>}
+        </div>
+        {rightNode ? rightNode : <div />}
+      </header>
       {isSpacing && <Spacing size={48} />}
     </>
   );
