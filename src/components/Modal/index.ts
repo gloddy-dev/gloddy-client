a6f74539eb33635dccd50654b4d30d619e59f0d7
Kuesung Park <gueit214@naver.com>
2023-08-15 14:38:19 +0900

    refactor : server > client로 수정

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,3 +1,3 @@
-export { default as ModalWrapper } from './ModalWrapper.server';
+export { default as ModalWrapper } from './ModalWrapper.client';
 export { default as BottomSheet } from './BottomSheet';
 export { default as Modal } from './Modal.client';
