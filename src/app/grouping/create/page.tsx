19b6a3b142248185855434b20c2cf10bbe73c8d3
Kuesung Park <gueit214@naver.com>
2023-08-15 10:14:38 +0900

    feat : GroupingPopup 구현

--- src/app/grouping/create/page.tsx
+++ src/app/grouping/create/page.tsx
@@ -1,10 +1,12 @@
 import CreateGroupContextProvider from './components/CreateGroupContext';
 import TopNavigationBar from './components/CreateTopNavigationBar';
+import GroupingPopup from './components/GroupingPopup.client';
 import InputForm from './components/InputForm.client';
 
 export default function CreateGroup() {
   return (
     <div className="mx-20">
+      {/* <GroupingPopup /> */}
       <TopNavigationBar />
       <CreateGroupContextProvider>
         <InputForm />
