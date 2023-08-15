84955052e8696436454cc55c115eccae23e19d5e
Kuesung Park <gueit214@naver.com>
2023-08-15 11:32:36 +0900

    refactor : info-no-icon variant 추가

--- src/app/join/funnels/step3/components/NoticeSection.tsx
+++ src/app/join/funnels/step3/components/NoticeSection.tsx
@@ -21,9 +21,8 @@ export default function NoticeSection() {
       <Spacing size={8} />
       <TextList variant="info">신뢰있는 모임을 위해 재학생 인증을 꼭 진행해주세요</TextList>
       <Spacing size={8} />
-      <TextList variant="info">
-        재학생 이메일 발급
-        <br />
+      <TextList variant="info">재학생 이메일 발급</TextList>
+      <TextList variant="info-no-icon">
         <Link href={INSTAGRAM_URL}>{INSTAGRAM_URL}</Link>
       </TextList>
     </section>
