0523146f2424f9d9b06b9290696d3af9e700644f
Kuesung Park <gueit214@naver.com>
2023-08-15 13:19:10 +0900

    refactor : CheckboxSection > NoticeSection 으로 수정

--- src/app/join/funnels/step1/components/FormSection.client.tsx
+++ src/app/join/funnels/step1/components/FormSection.client.tsx
@@ -2,18 +2,16 @@
 
 import NumberForm from './NumberForm.client';
 import NumberVerifyForm from './NumberVerifyForm.client';
-import { Spacing } from '@/components/common/Spacing';
 import { useState } from 'react';
 
 import type { StatusType } from '../type';
 
 export default function InputForm() {
-  const [inputStatus, setInputStatus] = useState<StatusType>('notReadyForSend');
+  const [inputStatus, setInputStatus] = useState<StatusType>('afterSend');
 
   return (
     <section>
       <NumberForm inputStatus={inputStatus} setInputStatus={setInputStatus} />
-      <Spacing size={18} />
       {inputStatus === 'afterSend' && <NumberVerifyForm />}
     </section>
   );
---
+++ src/app/join/funnels/step1/components/NoticeSection.server.tsx
@@ -0,0 +1,12 @@
+import { Spacing } from '@/components/common/Spacing';
+import TextList from '@/components/InfoMessage/TextList';
+
+export default function NoticeSection() {
+  return (
+    <section>
+      <TextList variant="caption">휴대폰 번호는 안전하게 보관됩니다.</TextList>
+      <Spacing size={8} />
+      <TextList variant="caption">휴대폰 번호는 어디에도 공개되지 않습니다.</TextList>
+    </section>
+  );
+}
