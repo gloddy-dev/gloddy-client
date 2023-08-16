238538e9557d16027ec0b4726d91d0dcaf8a1f0c
Kuesung Park <gueit214@naver.com>
2023-08-16 12:01:20 +0900

    refactor : 기존 Modal 제거

--- src/components/common/Modal/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ModalWrapper } from './ModalWrapper';
-export { default as BottomSheet } from './BottomSheet.client';
-export { default as ConfirmModal } from './ConfirmModal.client';
