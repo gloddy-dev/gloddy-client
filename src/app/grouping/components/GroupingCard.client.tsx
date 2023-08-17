2e7ebf26b182f4001a1d37bc81fdc5c95737e9ed
Kuesung Park <gueit214@naver.com>
2023-08-17 13:43:59 +0900

    refactor : Grouping Card 수정

--- src/app/grouping/components/GroupingCard.client.tsx
+++ src/app/grouping/components/GroupingCard.client.tsx
@@ -2,7 +2,6 @@
 import { Spacing } from '@/components/common/Spacing';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import React from 'react';
 
 import type { Grouping } from '@/apis/groups/type';
 
@@ -11,37 +10,36 @@ interface GroupingCardProps {
 }
 
 export default function GroupingCard({ groupingData }: GroupingCardProps) {
-  const { title, content, memberCount, maxUser, meetDate, place } = groupingData;
+  const { title, content, imageUrl, memberCount, maxUser, meetDate, place } = groupingData;
   const router = useRouter();
 
   return (
     <div
-      className="flex h-126 w-full cursor-pointer flex-col justify-between rounded-8 bg-white px-20 py-16 pl-14"
+      className="flex h-128 cursor-pointer bg-white py-16"
       onClick={() => router.push(`/grouping/${groupingData.groupId}`)}
     >
-      <div className="flex w-full flex-row">
-        <div className="h-60 w-60 rounded-8 bg-white3"></div>
-        <Spacing size={13} direction="horizontal" />
-        <div className="flex w-206 flex-col">
-          <div className="font-700 text-14">{title}</div>
-          <div className="font-400 text-12">{content}</div>
-        </div>
-      </div>
-      <div className="flex flex-row">
-        <div className="flex items-center">
-          <Image src="/assets/avatar.svg" alt="avatar" width={10} height={12} />
-          <Spacing size={7} direction="horizontal" />
-          <div className="font-700 text-12">{`${memberCount}명`}</div>
-          <div className="font-400 text-12">{`/ ${maxUser}명`}</div>
-        </div>
-        <Spacing size={8} direction="horizontal" />
-        <div className="flex items-center">
-          <Image src="/assets/location.svg" alt="location" width={10} height={12} />
-          <Spacing size={5} direction="horizontal" />
-          <div className="font-400 w-110 truncate text-12">{place}</div>
-        </div>
-        <Spacing size={6} direction="horizontal" />
-        <div className="font-400 text-12 text-blue3">{meetDate}</div>
+      <section className="h-96 w-96">
+        {imageUrl ? (
+          <Image fill src={imageUrl} alt="group" className="rounded-8" />
+        ) : (
+          <div className="h-full rounded-8 bg-white3" />
+        )}
+      </section>
+
+      <Spacing size={12} direction="horizontal" />
+
+      <div>
+        <p className="text-subtitle-1">{title}</p>
+        <p className="text-paragraph-2">{content}</p>
+        <Spacing size={8} />
+        <p className="flex text-caption text-sign-tertiary">
+          <Image src="/icons/16/location.svg" width={16} height={16} alt="location" />
+          {place}
+        </p>
+        <p className="flex text-caption text-sign-tertiary">
+          <Image src="/icons/16/date_range.svg" width={16} height={16} alt="location" />
+          {meetDate}
+        </p>
       </div>
     </div>
   );
--- src/app/grouping/page.tsx
+++ src/app/grouping/page.tsx
@@ -2,33 +2,25 @@ import FloatingBubbleSection from './components/FloatingBubbleSection.server';
 import GroupingCardList from './components/GroupingCardList.client';
 import { getGroups } from '@/apis/groups/apis';
 import { Keys } from '@/apis/groups/keys';
-import { RetryErrorBoundary } from '@/components/common/ErrorBoundary';
+import { RejectedFallback } from '@/components/common/ErrorBoundary';
 import { BottomNavigationBar } from '@/components/common/NavigationBar';
 import { HydrationProvider } from '@/components/common/Provider/HydrationProvider';
 import { Spacing } from '@/components/common/Spacing';
 import { Header } from '@/components/Header';
-import { Suspense } from 'react';
-
-const GroupingComponent = () => {
-  return (
-    <RetryErrorBoundary>
-      <Suspense fallback={<div>로딩 중...</div>}>
-        <HydrationProvider queryKey={Keys.getGroups()} queryFn={() => getGroups(0)} isInfiniteQuery>
-          <GroupingCardList />
-        </HydrationProvider>
-      </Suspense>
-    </RetryErrorBoundary>
-  );
-};
+import { QueryAsyncBoundary } from '@suspensive/react-query';
 
 export default function Grouping() {
   return (
-    <main className="h-full bg-white2">
+    <>
       <Header leftNode={<p className="font-700 text-black2">그루핑</p>} />
       <Spacing size={18} />
-      <GroupingComponent />
+      <QueryAsyncBoundary rejectedFallback={RejectedFallback} pendingFallback={null}>
+        <HydrationProvider queryKey={Keys.getGroups()} queryFn={() => getGroups(0)} isInfiniteQuery>
+          <GroupingCardList />
+        </HydrationProvider>
+      </QueryAsyncBoundary>
       <FloatingBubbleSection />
       <BottomNavigationBar page="grouping" />
-    </main>
+    </>
   );
 }
