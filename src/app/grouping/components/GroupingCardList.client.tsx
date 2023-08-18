89a4bfcde58abbcb06b4cf6bc70a3cbc8db2ef9c
Kuesung Park <gueit214@naver.com>
2023-08-18 11:45:07 +0900

    Revert "refactor : GroupingCard onClick props로 받도록 수정"
    This reverts commit b8aa2bb02f6b4f104d0a70738021c56b2ef1972f.

--- src/app/grouping/components/GroupingCardList.client.tsx
+++ src/app/grouping/components/GroupingCardList.client.tsx
@@ -1,20 +1,14 @@
 'use client';
 import GroupingCard from './GroupingCard.client';
 import { useGetGroups } from '@/apis/groups';
-import { useRouter } from 'next/navigation';
 
 export default function GroupingCardList() {
-  const router = useRouter();
   const { data } = useGetGroups();
 
   return (
     <section className="mx-20 flex flex-col gap-8">
       {data.map((groupingData) => (
-        <GroupingCard
-          groupingData={groupingData}
-          key={groupingData.groupId}
-          onClick={() => router.push(`/grouping/${groupingData.groupId}`)}
-        />
+        <GroupingCard groupingData={groupingData} key={groupingData.groupId} />
       ))}
     </section>
   );
