98ee4e37824c34f065f17bc3e812fb8f1c4048b4
Kuesung Park <gueit214@naver.com>
2023-08-18 11:38:23 +0900

    feat : Dummy데이터로 나의 모임 페이지 구현

--- src/app/meeting/components/ParticipatingContent.client.tsx
+++ src/app/meeting/components/ParticipatingContent.client.tsx
@@ -1,4 +1,29 @@
+'use client';
+import GroupingCard from '@/app/grouping/components/GroupingCard.client';
+import { DivisionBar, DivisionSpacing, Spacing } from '@/components/common/Spacing';
+import { Divider } from '@/components/Divider';
+import { DUMMY_GROUPING_DATA } from '@/constants/dummyData';
+
 interface ParticipatingContentProps {}
 export default function ParticipatingContent() {
-  return <div></div>;
+  return (
+    <div className="px-20">
+      <Spacing size={20} />
+      <p>
+        <Spacing size={4} direction="horizontal" />
+        <span className="text-subtitle-3 text-sign-secondary">멤버로 참여중인 모임</span>
+      </p>
+      <GroupingCard groupingData={DUMMY_GROUPING_DATA[0]} />
+      <GroupingCard groupingData={DUMMY_GROUPING_DATA[0]} />
+      <Spacing size={8} />
+      <DivisionSpacing size={5} />
+      <Spacing size={20} />
+      <p>
+        <Spacing size={4} direction="horizontal" />
+        <span className="text-subtitle-3 text-sign-secondary">멤버로 참여중인 모임</span>
+      </p>
+      <GroupingCard groupingData={DUMMY_GROUPING_DATA[0]} />
+      <GroupingCard groupingData={DUMMY_GROUPING_DATA[0]} />
+    </div>
+  );
 }
