60b4007c8aa6eb92df60c61aa50fac78afaf7750
Kuesung Park <gueit214@naver.com>
2023-08-18 15:21:24 +0900

    refactor : location 아이콘 옆 여백 추가

--- src/components/Card/GroupingCard.client.tsx
+++ src/components/Card/GroupingCard.client.tsx
@@ -38,10 +38,11 @@ export default function GroupingCard({
           <p className="w-250 truncate text-subtitle-1">{title}</p>
           <p className="w-250 truncate text-paragraph-2 text-sign-secondary">{content}</p>
           <Spacing size={8} />
-          <p className="flex text-caption text-sign-tertiary">
+          <div className="flex text-caption text-sign-tertiary">
             <Image src="/icons/16/location.svg" width={16} height={16} alt="location" />
+            <Spacing size={4} direction="horizontal" />
             {place}
-          </p>
+          </div>
           <Spacing size={4} />
           <p className="flex text-caption text-sign-tertiary">
             <Image src="/icons/16/date_range.svg" width={16} height={16} alt="location" />
