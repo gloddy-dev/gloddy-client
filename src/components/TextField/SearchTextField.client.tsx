faf3d5c04d57c32df49bf7cac3d8e489e7f84f7b
Kuesung Park <gueit214@naver.com>
2023-08-18 20:26:38 +0900

    refactor : SearchTextField 타입 선언

--- src/components/TextField/SearchTextField.client.tsx
+++ src/components/TextField/SearchTextField.client.tsx
@@ -3,15 +3,19 @@ import Image from 'next/image';
 import { forwardRef } from 'react';
 
 export default forwardRef(function SearchTextField<T extends React.ElementType>(
-  { ...props }: TextFieldProps<T>,
+  { ...props }: TextFieldProps<T> & React.ComponentPropsWithoutRef<T>,
   ref: React.ForwardedRef<HTMLLabelElement>
 ) {
   return (
-    <div></div>
-    // <TextField
-    //   ref={ref}
-    //   leftIcon={<Image src="/icons/24/search.svg" width={24} height={24} alt="search" />}
-    //   {...props}
-    // />
+    <div>
+      <TextField
+        ref={ref}
+        leftIcon={<Image src="/icons/24/search.svg" width={24} height={24} alt="search" />}
+        {...props}
+      />
+    </div>
   );
-});
+}) as <T extends React.ElementType = 'input'>(
+  props: TextFieldProps<T> &
+    React.ComponentPropsWithoutRef<T> & { ref?: React.ForwardedRef<HTMLLabelElement> }
+) => React.ReactElement;
