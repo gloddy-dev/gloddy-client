c98ae6f4b26466bb5ca1b0bd39de97688e0307dc
Kuesung Park <gueit214@naver.com>
2023-08-18 13:21:55 +0900

    refactor : items map으로 뿌려주기

--- src/app/meeting/components/ParticipatingContent.client.tsx
+++ src/app/meeting/components/ParticipatingContent.client.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { GroupingCard } from '@/components/Card';
 import { DivisionSpacing, Spacing } from '@/components/common/Spacing';
+import { Divider } from '@/components/Divider';
 import { DUMMY_GROUPING_DATA } from '@/constants/dummyData';
 
 interface ParticipatingContentProps {}
@@ -12,17 +13,20 @@ export default function ParticipatingContent() {
         <Spacing size={4} direction="horizontal" />
         <span className="text-subtitle-3 text-sign-secondary">멤버로 참여중인 모임</span>
       </div>
-      <GroupingCard groupingData={DUMMY_GROUPING_DATA[0]} />
-      <GroupingCard groupingData={DUMMY_GROUPING_DATA[1]} />
+      {DUMMY_GROUPING_DATA.map((groupingData) => (
+        <GroupingCard groupingData={groupingData} key={groupingData.groupId} />
+      ))}
+
       <Spacing size={8} />
-      <DivisionSpacing size={5} />
+      <Divider thickness="thick" />
       <Spacing size={20} />
       <div>
         <Spacing size={4} direction="horizontal" />
         <span className="text-subtitle-3 text-sign-secondary">멤버로 참여중인 모임</span>
       </div>
-      <GroupingCard groupingData={DUMMY_GROUPING_DATA[2]} />
-      <GroupingCard groupingData={DUMMY_GROUPING_DATA[1]} />
+      {DUMMY_GROUPING_DATA.map((groupingData) => (
+        <GroupingCard groupingData={groupingData} key={groupingData.groupId} />
+      ))}
     </div>
   );
 }
