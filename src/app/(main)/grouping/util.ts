3c0bd181108d92ac2a1b4b05df9a23aeeb7b7011
Kuesung Park <gueit214@naver.com>
2023-08-19 19:35:21 +0900

    refactor : 날짜 format 수정

--- src/app/(main)/grouping/util.ts
+++ src/app/(main)/grouping/util.ts
@@ -1,6 +1,6 @@
 export function formatDate(input: string): string {
-  const [datePart, timePart] = input.split(' ');
-  const [month, day, weekday] = datePart.split('.');
-  const [time, ampm] = timePart.split(/(AM|PM)/);
-  return `${month}.${day} ${weekday} ${time}${ampm}`;
+  const day = input.split(' ')[1];
+  const month = input.split(' ')[0].substr(5, 2);
+  const date = input.split(' ')[0].substr(8, 2);
+  return `${month}.${date} ${day}`;
 }
