f657761cf76db0e86841dbe9e5a35d545b2a7cd1
Kuesung Park <gueit214@naver.com>
2023-08-19 21:09:26 +0900

    feat : 자기소개 section 구현

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,5 +1,6 @@
-import ProfileDetail from './components/ProfileDetail.client';
+import IntroduceSection from './components/IntroduceSection.client';
 import ProfileHeader from './components/ProfileHeader.client';
+import ProfileSection from './components/ProfileSection.client';
 import { Keys, getProfile } from '@/apis/profile';
 import { BottomNavigationBar } from '@/components/common/NavigationBar';
 import { HydrationProvider } from '@/components/common/Provider/HydrationProvider';
@@ -7,14 +8,15 @@ import { QueryAsyncBoundary } from '@suspensive/react-query';
 
 export default function Profile() {
   return (
-    <>
+    <div className="h-full bg-sub">
       <ProfileHeader />
       <QueryAsyncBoundary rejectedFallback={<div>에러</div>} pendingFallback={null}>
         <HydrationProvider queryFn={getProfile} queryKey={Keys.getProfile()}>
-          <ProfileDetail />
+          <ProfileSection />
+          <IntroduceSection />
         </HydrationProvider>
       </QueryAsyncBoundary>
       <BottomNavigationBar page="profile" />
-    </>
+    </div>
   );
 }
