7e9176b89a27c26f97a9eab349aaff0446e10160
Kuesung Park <gueit214@naver.com>
2023-08-19 12:01:45 +0900

    feat : Segment컴포넌트로 교체

--- src/app/(sub)/join/funnels/step4/components/GenderSection.client.tsx
+++ src/app/(sub)/join/funnels/step4/components/GenderSection.client.tsx
@@ -1,47 +1,17 @@
 'use client';
 import { useJoinContext } from '../../../components/JoinContext.client';
-import { BottomFixedButton } from '@/components/common/Button';
-import GenderSwipePicker from '@/components/common/SwipePicker/GenderSwipePicker';
-import { BottomSheet } from '@/components/Modal';
-import { TextField } from '@/components/TextField';
-import useModalStore from '@/store/useModalStore';
+import { SegmentGroup } from '@/components/SegmentGroup';
 
 export default function GenderSection() {
-  const { modalName, openModal, closeModal } = useModalStore();
   const { watch, setValue } = useJoinContext();
-  const gender = watch('gender');
 
   return (
     <section className="flex flex-col gap-5">
       <p className="text-14">성별</p>
-      <TextField
-        placeholder="성별을 선택해주세요."
-        onClick={() => openModal('gender')}
-        value={gender ?? ''}
-        readOnly
-      />
-      <BottomSheet
-        isOpen={modalName === 'gender'}
-        snap={400}
-        onClose={closeModal}
-        isRightButton
-        text={<p className="font-500 text-18 text-gray7">성별</p>}
-        disableDrag
-      >
-        <div className="relative h-full">
-          <GenderSwipePicker
-            genderValue={gender}
-            setGenderValue={(value) => setValue('gender', value)}
-          />
-        </div>
-        <BottomFixedButton
-          text="완료"
-          onClick={() => {
-            if (!gender) setValue('gender', '남성');
-            closeModal();
-          }}
-        />
-      </BottomSheet>
+      <SegmentGroup selectedValue={watch('gender')} onChange={(value) => setValue('gender', value)}>
+        <SegmentGroup.Segment value={'MALE'} label="남성" />
+        <SegmentGroup.Segment value={'FEMALE'} label="여성" />
+      </SegmentGroup>
     </section>
   );
 }
