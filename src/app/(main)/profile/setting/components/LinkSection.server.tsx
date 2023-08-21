174c2a347a6ae679d2c4c7b62edb53e3023431a8
Kuesung Park <gueit214@naver.com>
2023-08-21 09:48:15 +0900

    feat : Link Section 구현

--- src/app/(main)/profile/setting/components/LinkSection.server.tsx
+++ src/app/(main)/profile/setting/components/LinkSection.server.tsx
@@ -1,4 +1,26 @@
-interface LinkSectionProps {}
+import { Divider } from '@/components/Divider';
+import Link from 'next/link';
+
 export default function LinkSection() {
-  return <div></div>;
+  return (
+    <section className="text-subtitle-2">
+      <div className="flex px-20 py-12">
+        <span>버전</span>
+        <span className="ml-auto text-caption text-sign-caption">1.0.0v</span>
+      </div>
+      <div className="px-20 py-12">
+        <span>FAQ</span>
+      </div>
+      <div className="px-20 py-12">
+        <Link href="/profile/setting/service">서비스 이용약관</Link>
+      </div>
+      <div className="px-20 py-12">
+        <Link href="/profile/setting/information">개인정보 처리방침</Link>
+      </div>
+      <Divider thickness="thick" />
+      <div className="px-20 py-12 ">
+        <Link href="/profile/setting/quit">계정 삭제하기</Link>
+      </div>
+    </section>
+  );
 }
--- src/app/(main)/profile/setting/components/ProfileSection.client.tsx
+++ src/app/(main)/profile/setting/components/ProfileSection.client.tsx
@@ -9,16 +9,16 @@ import Link from 'next/link';
 export default function ProfileSection() {
   const { data: profileData } = useGetProfile();
   const { imageUrl, name } = profileData;
-  console.log(imageUrl);
+
   return (
     <section className="px-20 py-8">
       <Flex align="center" className="gap-12 rounded-8 bg-sub px-16 py-12">
         <Avatar imageUrl={imageUrl} />
         <div>
           <p className="text-paragraph-1">{name}</p>
-          <p className="text-caption text-sign-tertiary"> GLODDY</p>
+          <p className="text-caption text-sign-tertiary">GLODDY</p>
         </div>
-        <Link href="/profile/edit" className="ml-auto">
+        <Link href="/profile/setting/edit" className="ml-auto">
           <Image src="/icons/24/settings.svg" width={24} height={24} alt="setting" />
         </Link>
       </Flex>
