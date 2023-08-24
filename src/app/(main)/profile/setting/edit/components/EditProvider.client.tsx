0df6041108ef52520dbc349a4121ed2854177b91
Kuesung Park <gueit214@naver.com>
2023-08-24 11:16:01 +0900

    feat : EditProvider 구현

---
+++ src/app/(main)/profile/setting/edit/components/EditProvider.client.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { FormProvider, useForm, useFormContext } from 'react-hook-form';
+
+import type { ProfileEditState } from '../type';
+import type { StrictPropsWithChildren } from '@/types';
+
+const formDefaultValue: ProfileEditState = {
+  name: '',
+  birth: {
+    year: '',
+    month: '',
+    date: '',
+  },
+  gender: 'MAIL',
+  imageUrl: '',
+  introduce: '',
+  personalities: [],
+};
+
+export default function EditProvider({ children }: StrictPropsWithChildren) {
+  const hookForm = useForm<ProfileEditState>({ defaultValues: formDefaultValue, mode: 'onBlur' });
+
+  return <FormProvider {...hookForm}>{children}</FormProvider>;
+}
+
+export function useEditContext() {
+  return useFormContext<ProfileEditState>();
+}
