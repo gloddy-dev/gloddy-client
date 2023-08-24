de5cf6144beadfba7bbf224fd8ba29a17eaab97a
Kuesung Park <gueit214@naver.com>
2023-08-24 10:49:47 +0900

    feat : 프로필 수정 API 구현

--- src/apis/profile/apis.ts
+++ src/apis/profile/apis.ts
@@ -1,9 +1,12 @@
 import privateApi from '../config/privateApi';
 
-import type { MatesResponse, PraisesResponse, ProfileResponse } from './type';
+import type { MatesResponse, PraisesResponse, ProfileRequest, ProfileResponse } from './type';
 
 export const getProfile = () => privateApi.get<ProfileResponse>('/me/page');
 
 export const getPraises = () => privateApi.get<PraisesResponse>('/me/praises');
+
+export const patchProfile = (profileData: ProfileRequest) =>
+  privateApi.patch('/me/info', profileData);
 
 export const getMates = () => privateApi.get<MatesResponse>('/me/mates');
