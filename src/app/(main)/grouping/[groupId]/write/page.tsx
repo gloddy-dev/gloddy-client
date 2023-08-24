3f6b04b329bf4cb39ecef28642e82bdd8fd596e1
kang <kangju2000@naver.com>
2023-08-24 17:42:25 +0900

    refactor: WriteContext 제거

--- src/app/(main)/grouping/[groupId]/write/page.tsx
+++ src/app/(main)/grouping/[groupId]/write/page.tsx
@@ -1,12 +1,11 @@
 import InputForm from './components/InputForm';
 import WriteHeader from './components/WriteHeader';
-import { WriteContextProvider } from './WriteContext';
 
 export default function WritePage() {
   return (
-    <WriteContextProvider>
+    <>
       <WriteHeader />
       <InputForm />
-    </WriteContextProvider>
+    </>
   );
 }
