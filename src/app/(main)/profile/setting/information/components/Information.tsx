ee8d713ea7c45b064f70af761d63f4ae9e5619d8
Kuesung Park <gueit214@naver.com>
2023-08-28 19:24:28 +0900

    feat : 개인정보 정보 페이지 구현

---
+++ src/app/(main)/profile/setting/information/components/Information.tsx
@@ -0,0 +1,5 @@
+import { settingInformation } from '@/constants/settingInformation';
+
+export default function Information() {
+  return <div className="p-20 text-paragraph-2 text-sign-tertiary ">{settingInformation}</div>;
+}
---
+++ src/app/(main)/profile/setting/service/components/ServiceHeader.tsx
@@ -0,0 +1,19 @@
+import { IconButton } from '@/components/Button';
+import { Header } from '@/components/Header';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function ServiceHeader() {
+  return (
+    <Header>
+      <Header.Left className="px-4">
+        <Link href="/profile/setting">
+          <IconButton size="large">
+            <Image src="/icons/24/arrow_back.svg" width={24} height={24} alt="back" />
+          </IconButton>
+        </Link>
+        <p className="text-subtitle-1">개인정보 처리 방침</p>
+      </Header.Left>
+    </Header>
+  );
+}
