78e80046aa2c749ee67d979e20323a7f4ff56f4b
Kuesung Park <gueit214@naver.com>
2023-08-28 12:31:39 +0900

    feat : birth 데이터 포맷팅

--- src/apis/profile/queries.ts
+++ src/apis/profile/queries.ts
@@ -2,7 +2,20 @@ import { getMates, getPraises, getProfile } from './apis';
 import { Keys } from './keys';
 import { useSuspenseQuery } from '@suspensive/react-query';
 
-export const useGetProfile = () => useSuspenseQuery(Keys.getProfile(), getProfile);
+export const useGetProfile = () =>
+  useSuspenseQuery(Keys.getProfile(), getProfile, {
+    select: (data) => {
+      const { birth } = data;
+
+      const formattedBirth = {
+        year: birth.split('.')[0],
+        month: birth.split('.')[1],
+        date: birth.split('.')[2],
+      };
+
+      return { ...data, birth: formattedBirth };
+    },
+  });
 
 export const useGetPraises = () => useSuspenseQuery(Keys.getPraises(), getPraises);
 
