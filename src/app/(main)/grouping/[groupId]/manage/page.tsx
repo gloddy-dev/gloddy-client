3b121a024344a4c7aebe99a95c731991736b2ede
kang <kangju2000@naver.com>
2023-08-30 01:13:51 +0900

    refactor: 모임 지원서 관리 페이지에 api 연결 및 Swiper 적용

--- src/app/(main)/grouping/[groupId]/manage/page.tsx
+++ src/app/(main)/grouping/[groupId]/manage/page.tsx
@@ -1,13 +1,25 @@
 import ManageDetail from './components/ManageDetail.client';
 import ManageHeader from './components/ManageHeader.client';
+import { Keys, getApplies } from '@/apis/groups';
+import { HydrationProvider } from '@/components/common/Provider/HydrationProvider';
 import { Suspense } from 'react';
 
-export default function GroupingManagePage() {
+interface GroupingManagePageProps {
+  params: {
+    groupId: string;
+  };
+}
+
+export default function GroupingManagePage({ params }: GroupingManagePageProps) {
+  const groupId = Number(params.groupId);
+
   return (
     <>
       <ManageHeader />
       <Suspense fallback={null}>
-        <ManageDetail />
+        <HydrationProvider queryFn={() => getApplies(groupId)} queryKey={Keys.getApplies(groupId)}>
+          <ManageDetail />
+        </HydrationProvider>
       </Suspense>
     </>
   );
