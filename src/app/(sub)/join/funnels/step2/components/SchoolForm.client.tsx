fc20f7fc105bd6a35096ac9c9d731378e11432db
Kuesung Park <gueit214@naver.com>
2023-08-30 13:04:27 +0900

    fix : 학교 입력 이슈 해결

--- src/app/(sub)/join/funnels/step2/components/SchoolForm.client.tsx
+++ src/app/(sub)/join/funnels/step2/components/SchoolForm.client.tsx
@@ -7,11 +7,13 @@ import { SchoolSearchResponse, useGetSearchSchool } from '@/apis/auth';
 import { Button, ButtonGroup } from '@/components/Button';
 import { Spacing } from '@/components/common/Spacing';
 import { Flex } from '@/components/Layout';
-import { SearchTextField } from '@/components/TextField';
+import { TextFieldController } from '@/components/TextField';
+import { regexr } from '@/constants/regexr';
 import Image from 'next/image';
 
 export default function SchoolForm() {
-  const { handleSubmit, getFieldState, register, watch, setValue } = useJoinContext();
+  const hookForm = useJoinContext();
+  const { handleSubmit, register, watch, setValue } = hookForm;
   const { nextStep } = useFunnelContext();
 
   const searchWord = watch('schoolInfo.school');
@@ -20,25 +22,24 @@ export default function SchoolForm() {
 
   return (
     <form onSubmit={handleSubmit(nextStep)}>
-      <SearchTextField
+      <TextFieldController
+        hookForm={hookForm}
         register={register('schoolInfo.school', {
           required: true,
-          pattern: /[가-힣]+대학교/,
+          pattern: regexr.school,
         })}
+        leftIcon={<Image src="/icons/24/search.svg" width={24} height={24} alt="search" />}
       />
       {data &&
         data?.schools.map((school) => (
           <SearchResult
             school={school}
             key={school.address}
-            onClick={() => {
-              console.log(school.address);
-              setValue('schoolInfo.school', school.name);
-            }}
+            onClick={() => setValue('schoolInfo.school', school.name)}
           />
         ))}
       <ButtonGroup>
-        <Button disabled={!getFieldState('schoolInfo.school').invalid}>확인</Button>
+        <Button disabled={!searchWord.match(regexr.school)}>확인</Button>
       </ButtonGroup>
     </form>
   );
--- src/constants/regexr.ts
+++ src/constants/regexr.ts
@@ -2,4 +2,5 @@ export const regexr = {
   phoneNumber: /^010-\d{4}-\d{4}$/,
   email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z]*)@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.ac.kr$/,
   verifyNumber: /^\d{6}$/,
+  school: /[가-힣]+대학교/,
 };
