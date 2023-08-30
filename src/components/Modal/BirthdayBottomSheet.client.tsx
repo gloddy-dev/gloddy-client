25ce96d63fe4a47407e41f9d095e1f2a65fbcbb7
Kuesung Park <gueit214@naver.com>
2023-08-30 10:50:41 +0900

    refactor : BottomSheet에서 hook 꺼내 사용하기

--- src/components/Modal/BirthdayBottomSheet.client.tsx
+++ src/components/Modal/BirthdayBottomSheet.client.tsx
@@ -1,26 +1,22 @@
+import { useJoinContext } from '@/app/(sub)/join/components/JoinContext.client';
 import { Button, ButtonGroup } from '@/components/Button';
 import { DateSwipePicker } from '@/components/common/SwipePicker';
 import { BottomSheet } from '@/components/Modal';
 
 import type { DateType } from '@/types';
-import type { UseFormSetValue } from 'react-hook-form';
 
 interface BirthdayBottomSheetProps {
   onClose: () => void;
-  dateValue: DateType;
-  setValue: UseFormSetValue<any>;
-  isBirthDayEntered: boolean;
 }
-export default function BirthdayBottomSheet({
-  onClose,
-  dateValue,
-  setValue,
-  isBirthDayEntered,
-}: BirthdayBottomSheetProps) {
+export default function BirthdayBottomSheet({ onClose }: BirthdayBottomSheetProps) {
+  const { setValue, watch } = useJoinContext();
+  const birth = watch('birth');
+  const isBirthDayEntered = !!birth.year && !!birth.month && !!birth.date;
+
   return (
     <BottomSheet snapPoints={[400, 0]} onClose={onClose} title="생년월일" disableDrag>
       <DateSwipePicker
-        dateValue={dateValue}
+        dateValue={birth}
         setDateValue={(birth: DateType) => setValue('birth', birth)}
       />
       <ButtonGroup>
