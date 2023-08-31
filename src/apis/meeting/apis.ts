3e0027288033873e525ce88aec93091ff3efab49
Kuesung Park <gueit214@naver.com>
2023-08-31 10:59:23 +0900

    feat : 나의 모임 api 및 type 추가

--- src/apis/meeting/apis.ts
+++ src/apis/meeting/apis.ts
@@ -1,1 +1,23 @@
-export const getExample = () => {};
+import {
+  MeetingHostingResponse,
+  MeetingNotEstimatedResponse,
+  MeetingParticipatingResponse,
+  MeetingRejectedResponse,
+  MeetingWaitingResponse,
+} from '.';
+import privateApi from '../config/privateApi';
+
+export const getMeetingWaiting = () =>
+  privateApi.get<MeetingWaitingResponse>('/users/groups/waiting');
+
+export const getMeetingRejected = () =>
+  privateApi.get<MeetingRejectedResponse>('/users/groups/rejected');
+
+export const getMeetingParticipating = () =>
+  privateApi.get<MeetingParticipatingResponse>('/users/groups/participating');
+
+export const getMeetingNotEstimated = () =>
+  privateApi.get<MeetingNotEstimatedResponse>('/users/groups/notEstimated');
+
+export const getMeetingHosting = () =>
+  privateApi.get<MeetingHostingResponse>('/users/groups/hoisting');
