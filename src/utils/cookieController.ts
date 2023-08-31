c9e7c95ed91914298feb2db7476997822000a23d
kang <kangju2000@naver.com>
2023-08-31 14:19:17 +0900

    fix: 로컬에서 쿠키 못 가져오는 문제 수정

--- src/utils/cookieController.ts
+++ src/utils/cookieController.ts
@@ -1,7 +1,8 @@
 export const getLocalCookie = (key: string) => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${key}=`);
-  if (parts.length === 3) return parts.pop()?.split(';').shift();
+
+  return parts.pop()?.split(';').shift();
 };
 
 export const setLocalCookie = (key: string, value: string, options?: { expires?: Date }) => {
