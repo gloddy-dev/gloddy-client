8c2002b12e89c6b3e39b04ce4cadb77944d78301
kang <kangju2000@naver.com>
2023-08-31 14:04:04 +0900

    fix: redirect 에러 수정

--- src/utils/cookieController.ts
+++ src/utils/cookieController.ts
@@ -1,7 +1,7 @@
 export const getLocalCookie = (key: string) => {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${key}=`);
-  if (parts.length === 2) return parts.pop()?.split(';').shift();
+  if (parts.length === 3) return parts.pop()?.split(';').shift();
 };
 
 export const setLocalCookie = (key: string, value: string, options?: { expires?: Date }) => {
