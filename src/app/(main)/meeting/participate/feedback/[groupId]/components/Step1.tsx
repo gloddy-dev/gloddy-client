eb886a4341b2a7ffc3651c85264ad9d506bbcb3b
Kuesung Park <gueit214@naver.com>
2023-09-01 12:01:52 +0900

    feat : TitleSection 분리

--- src/app/(main)/meeting/participate/feedback/[groupId]/components/Step1.tsx
+++ src/app/(main)/meeting/participate/feedback/[groupId]/components/Step1.tsx
@@ -1,11 +1,14 @@
 import NoShowModal from './NoShowModal.client';
 import Step1Header from './Step1Header';
+import TitleSection from './TitleSection';
 import { EstimateResponse } from '@/apis/groups';
 import { Avatar } from '@/components/Avatar';
+import { Button, ButtonGroup } from '@/components/Button';
 import { Spacing } from '@/components/common/Spacing';
 import { Divider } from '@/components/Divider';
 import { Flex } from '@/components/Layout';
 import { Tag } from '@/components/Tag';
+import { DUMMY_DATA_ESTIMATE } from '@/constants/dummyData';
 import { useModal } from '@/hooks/useModal';
 import Image from 'next/image';
 
@@ -13,47 +16,28 @@ interface Step1Props {
   onNextClick: () => void;
 }
 
-const DUMMY_DATA_ESTIMATE: EstimateResponse = {
-  groupMemberList: [
-    {
-      imageUrl: '/public/images/dummy_avatar.png',
-      name: '김지수',
-    },
-    {
-      imageUrl: '/public/images/dummy_avatar.png',
-      name: '김지하',
-    },
-    {
-      imageUrl: '/public/images/dummy_avatar.png',
-      name: '김지상',
-    },
-  ],
-};
-
 const tagList = ['차분함', '친절함', '적극적', '유머러스'];
 
 export default function Step1({ onNextClick }: Step1Props) {
   return (
     <div>
       <Step1Header />
+      <TitleSection message="모임에서 어땠나요?" step={1} />
 
-      <div className="px-20">
-        <Spacing size={32} />
-        닷닷닷
-        <Spacing size={12} />
-        <h3 className="text-h3">모임에서 어땠나요?</h3>
-        <Spacing size={20} />
-      </div>
       <Divider thickness="thick" />
       {DUMMY_DATA_ESTIMATE.groupMemberList.map((member, index) => (
         <MemberCard member={member} key={index} />
       ))}
+      <ButtonGroup>
+        <Button onClick={onNextClick}>다음</Button>
+      </ButtonGroup>
     </div>
   );
 }
 interface MemberCardProps {
   member: EstimateResponse['groupMemberList'][0];
 }
+
 function MemberCard({ member }: MemberCardProps) {
   const { imageUrl, name } = member;
   const { open, close } = useModal();
@@ -61,7 +45,7 @@ function MemberCard({ member }: MemberCardProps) {
     <section className="px-20">
       <Spacing size={16} />
       <Flex align="center">
-        <Avatar size="large" imageUrl={imageUrl} />
+        <Avatar size="medium" imageUrl={imageUrl} />
         <Spacing size={12} direction="horizontal" />
         <div className="flex grow flex-col justify-center">
           <p className="text-paragraph-1">{name}</p>
@@ -80,7 +64,7 @@ function MemberCard({ member }: MemberCardProps) {
 
       <Spacing size={8} />
 
-      <Flex justify="center">
+      <Flex>
         {tagList.map((tag, index) => (
           <Tag key={index} className="mr-4">
             {tag}
