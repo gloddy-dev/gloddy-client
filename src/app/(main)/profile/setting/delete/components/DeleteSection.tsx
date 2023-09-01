618d9c2019b82404405d8eb3503966f5a6be7c20
Kuesung Park <gueit214@naver.com>
2023-09-01 17:35:36 +0900

    feat : delete context

--- src/app/(main)/profile/setting/delete/components/DeleteSection.tsx
+++ src/app/(main)/profile/setting/delete/components/DeleteSection.tsx
@@ -1,5 +1,6 @@
 'use client';
 import DeleteHeader from './DeleteHeader';
+import DeleteProvider from './DeleteProvider.client';
 import Step1 from './Step1';
 import Step2 from './Step2';
 import { useFunnel } from '@/hooks/useFunnel';
@@ -9,15 +10,17 @@ export default function DeleteSection() {
   const { Funnel, nextStep, prevStep } = useFunnel(['1', '2']);
   const router = useRouter();
   return (
-    <Funnel>
-      <Funnel.Step name="1">
-        <DeleteHeader onPrevClick={() => router.push('/profile/setting')} icon="close" />
-        <Step1 onNextClick={nextStep} />
-      </Funnel.Step>
-      <Funnel.Step name="2">
-        <DeleteHeader onPrevClick={prevStep} icon="arrow_back" />
-        <Step2 />
-      </Funnel.Step>
-    </Funnel>
+    <DeleteProvider>
+      <Funnel>
+        <Funnel.Step name="1">
+          <DeleteHeader onPrevClick={() => router.push('/profile/setting')} icon="close" />
+          <Step1 onNextClick={nextStep} />
+        </Funnel.Step>
+        <Funnel.Step name="2">
+          <DeleteHeader onPrevClick={prevStep} icon="arrow_back" />
+          <Step2 />
+        </Funnel.Step>
+      </Funnel>
+    </DeleteProvider>
   );
 }
