7c3f712fb286321a90ae3851bd06e9cefd3fb33e
Kuesung Park <gueit214@naver.com>
2023-09-01 14:53:35 +0900

    refactor : footer style 구조 리팩토링

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -49,31 +49,20 @@ export default function Footer({ page, isSpacing = true }: FooterProps) {
           <Link
             href={tab.url}
             key={tab.id}
-            className="flex w-full flex-col items-center text-center text-caption"
+            className={cn('flex w-full flex-col items-center text-center text-caption', {
+              'text-sign-brand': isSelected(tab),
+              'text-sign-tertiary': !isSelected(tab),
+            })}
           >
-            <div
-              className={cn('text-10', {
-                'text-sign-brand': isSelected(tab),
-                'text-sign-tertiary': !isSelected(tab),
-              })}
-            >
-              <Image
-                src={`/icons/32/footer/${tab.name}${
-                  tab.name === page ? '_selected' : '_default'
-                }.svg`}
-                alt={tab.title}
-                width={32}
-                height={32}
-              />
-              <p
-                className={cn('text-10', {
-                  'text-sign-brand': isSelected(tab),
-                  'text-sign-tertiary': !isSelected(tab),
-                })}
-              >
-                {tab.title}
-              </p>
-            </div>
+            <Image
+              src={`/icons/32/footer/${tab.name}${
+                tab.name === page ? '_selected' : '_default'
+              }.svg`}
+              alt={tab.title}
+              width={32}
+              height={32}
+            />
+            <p>{tab.title}</p>
           </Link>
         ))}
       </footer>
