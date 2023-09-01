d952c142a65073fa9b3bbaac73637ccdc41a8803
kang <kangju2000@naver.com>
2023-09-02 02:05:16 +0900

    fix: 입력 안되는 문제 수정

--- src/app/(main)/grouping/create/funnels/CreateFunnel.client.tsx
+++ src/app/(main)/grouping/create/funnels/CreateFunnel.client.tsx
@@ -2,7 +2,6 @@
 
 import MainStep from './main/MainStep.client';
 import MeetDateStep from './meetDate/MeetDateStep.client';
-import { useCreateGroupContext } from '../components/CreateGroupContext';
 import CreateHeader from '../components/CreateHeader.client';
 import { useFunnel } from '@/hooks/useFunnel';
 import { SubmitHandler } from 'react-hook-form';
@@ -10,24 +9,21 @@ import { SubmitHandler } from 'react-hook-form';
 import type { CreateGroupContextValue } from '../type';
 
 export default function CreateFunnel() {
-  const { handleSubmit } = useCreateGroupContext();
   const { Funnel, currentStep, setStep, prevStep } = useFunnel(['main', 'meetDate']);
 
-  const onSubmit: SubmitHandler<CreateGroupContextValue> = (data) => {
+  const handleSubmit: SubmitHandler<CreateGroupContextValue> = (data) => {
     console.log(data);
   };
 
   return (
     <Funnel>
       <CreateHeader currentStep={currentStep} />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Funnel.Step name="main">
-          <MainStep onSelectMeetDate={() => setStep('meetDate')} />
-        </Funnel.Step>
-        <Funnel.Step name="meetDate">
-          <MeetDateStep onDone={prevStep} />
-        </Funnel.Step>
-      </form>
+      <Funnel.Step name="main">
+        <MainStep onSelectMeetDate={() => setStep('meetDate')} onSubmit={handleSubmit} />
+      </Funnel.Step>
+      <Funnel.Step name="meetDate">
+        <MeetDateStep onDone={prevStep} />
+      </Funnel.Step>
     </Funnel>
   );
 }
--- src/app/(main)/grouping/create/funnels/main/MainStep.client.tsx
+++ src/app/(main)/grouping/create/funnels/main/MainStep.client.tsx
@@ -1,15 +1,22 @@
 import InputSection from './InputSection.client';
 import SettingSection from './SettingSection.client';
 import UploadSection from './UploadSection.client';
+import { useCreateGroupContext } from '../../components/CreateGroupContext';
 import { Button, ButtonGroup } from '@/components/Button';
 import { Spacing } from '@/components/common/Spacing';
 import { Divider } from '@/components/Divider';
 
+import type { CreateGroupContextValue } from '../../type';
+import type { SubmitHandler } from 'react-hook-form';
+
 interface MainStepProps {
   onSelectMeetDate: () => void;
+  onSubmit: SubmitHandler<CreateGroupContextValue>;
 }
 
-export default function MainStep({ onSelectMeetDate }: MainStepProps) {
+export default function MainStep({ onSelectMeetDate, onSubmit }: MainStepProps) {
+  const { handleSubmit } = useCreateGroupContext();
+
   return (
     <>
       <UploadSection />
@@ -18,7 +25,7 @@ export default function MainStep({ onSelectMeetDate }: MainStepProps) {
       <SettingSection onSelectMeetDate={onSelectMeetDate} />
       <Spacing size={60} />
       <ButtonGroup>
-        <Button type="submit">완료</Button>
+        <Button onClick={handleSubmit(onSubmit)}>완료</Button>
       </ButtonGroup>
     </>
   );
