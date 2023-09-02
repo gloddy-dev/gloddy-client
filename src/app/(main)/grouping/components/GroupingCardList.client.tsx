a2d764803237690fae33031a7a5e7acaf1ac89df
Kuesung Park <gueit214@naver.com>
2023-09-02 23:08:41 +0900

    refactor > useInfinityQuery에서 useIntersect로 네이밍 수정

--- src/app/(main)/grouping/components/GroupingCardList.client.tsx
+++ src/app/(main)/grouping/components/GroupingCardList.client.tsx
@@ -2,12 +2,12 @@
 import { useGetGroups } from '@/apis/groups';
 import { GroupingCard } from '@/components/Card';
 import { ItemList } from '@/components/List';
-import useInfinityQuery from '@/hooks/useInfinityQuery';
+import useIntersect from '@/hooks/useIntersect';
 
 export default function GroupingCardList() {
   const { data, fetchNextPage } = useGetGroups();
 
-  const target = useInfinityQuery(fetchNextPage);
+  const target = useIntersect(fetchNextPage);
 
   return (
     <>
