85e94aa3a7116b278c12981f70afabf0eca14c1d
Kuesung Park <gueit214@naver.com>
2023-09-02 23:11:48 +0900

    refactor : 뒤로가기 버튼 router.back()으로 수정

--- src/app/(main)/profile/setting/information/components/InformationHeader.tsx
+++ src/app/(main)/profile/setting/information/components/InformationHeader.tsx
@@ -2,13 +2,15 @@ import { IconButton } from '@/components/Button';
 import { Header } from '@/components/Header';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function InformationHeader() {
+  const router = useRouter();
   return (
     <Header>
       <Header.Left className="px-4">
         <Link href="/profile/setting">
-          <IconButton size="large">
+          <IconButton size="large" onClick={() => router.back()}>
             <Image src="/icons/24/close.svg" width={24} height={24} alt="back" />
           </IconButton>
         </Link>
--- src/app/(main)/profile/setting/service/components/ServiceHeader.tsx
+++ src/app/(main)/profile/setting/service/components/ServiceHeader.tsx
@@ -2,16 +2,16 @@ import { IconButton } from '@/components/Button';
 import { Header } from '@/components/Header';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function ServiceHeader() {
+  const router = useRouter();
   return (
     <Header>
       <Header.Left className="px-4">
-        <Link href="/profile/setting">
-          <IconButton size="large">
-            <Image src="/icons/24/close.svg" width={24} height={24} alt="back" />
-          </IconButton>
-        </Link>
+        <IconButton size="large" onClick={() => router.back()}>
+          <Image src="/icons/24/close.svg" width={24} height={24} alt="back" />
+        </IconButton>
         <p className="text-subtitle-1">서비스 이용 약관</p>
       </Header.Left>
     </Header>
