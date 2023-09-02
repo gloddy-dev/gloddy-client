e2d878d221311e8dfd122cef60d78bd9488bdd6d
Kuesung Park <gueit214@naver.com>
2023-09-02 20:29:04 +0900

    fix : 인증번호 재전송 로직 수정

--- src/app/(sub)/join/funnels/step1/components/NumberVerifyForm.client.tsx
+++ src/app/(sub)/join/funnels/step1/components/NumberVerifyForm.client.tsx
@@ -1,7 +1,7 @@
 import { useJoinContext } from '../../../components/JoinContext.client';
 import { useFunnelContext } from '../../JoinFunnel';
 import { formatWithoutHyphen } from '../util';
-import { LoginResponse, useLoginMutation, useSMSVerifyMutation } from '@/apis/auth';
+import { LoginResponse, useLoginMutation, useSMSMutation, useSMSVerifyMutation } from '@/apis/auth';
 import { Button, ButtonGroup } from '@/components/Button';
 import { useTimerContext } from '@/components/Provider';
 import { TextFieldController } from '@/components/TextField';
@@ -19,12 +19,13 @@ interface NumberVerifyFormProps {
 export default function NumberVerifyForm({ setInputStatus }: NumberVerifyFormProps) {
   const router = useRouter();
   const hookForm = useJoinContext();
-  const { handleSubmit, setError, register } = hookForm;
-  const { time } = useTimerContext();
+  const { handleSubmit, setError, register, watch, resetField } = hookForm;
+  const { time, reset, start } = useTimerContext();
 
   const { nextStep } = useFunnelContext();
   const { mutate: mutateSMSVerify } = useSMSVerifyMutation();
   const { mutate: mutateLogin } = useLoginMutation();
+  const { mutate: mutateSMS } = useSMSMutation();
 
   const handleResend = () => {
     if (time > 120) {
@@ -34,7 +35,14 @@ export default function NumberVerifyForm({ setInputStatus }: NumberVerifyFormPro
       });
       return;
     }
-    setInputStatus('beforeSend');
+    const phoneNumberWithoutHyphen = watch('phoneNumber').replace(/[-\s]/g, '');
+    mutateSMS(
+      { number: phoneNumberWithoutHyphen },
+      { onSuccess: () => setInputStatus('afterSend') }
+    );
+    resetField('verifyNumber');
+    reset();
+    start();
   };
 
   const onSubmit: SubmitHandler<Pick<SignUpState, 'phoneNumber' | 'verifyNumber'>> = (data) => {
