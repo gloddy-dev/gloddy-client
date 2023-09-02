991177944dfe1c86b8c9da048a9f692919bc25d1
Kuesung Park <gueit214@naver.com>
2023-09-02 21:02:01 +0900

    feat : 무한 스크롤 구현

--- src/app/(main)/grouping/components/GroupingCardList.client.tsx
+++ src/app/(main)/grouping/components/GroupingCardList.client.tsx
@@ -1,12 +1,31 @@
 'use client';
 import { useGetGroups } from '@/apis/groups';
 import { GroupingCard } from '@/components/Card';
+import { InfiniteScrollSensor } from '@/components/InfiniteScrollSensor';
 import { ItemList } from '@/components/List';
+import useIntersection from '@/hooks/useIntersection';
+import { useCallback, useEffect, useRef } from 'react';
 
 export default function GroupingCardList() {
-  const { data } = useGetGroups();
+  const { data, fetchNextPage } = useGetGroups();
+
+  const onIntersect = useCallback(
+    async (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      const entry = entries[0];
+      if (entry.isIntersecting) {
+        observer.unobserve(entry.target);
+        await fetchNextPage();
+        observer.observe(entry.target);
+      }
+    },
+    [fetchNextPage]
+  );
+  const target = useIntersection(onIntersect);
 
   return (
-    <ItemList data={data} renderItem={(grouping) => <GroupingCard groupingData={grouping} />} />
+    <>
+      <ItemList data={data} renderItem={(grouping) => <GroupingCard groupingData={grouping} />} />
+      <div ref={target} />
+    </>
   );
 }
