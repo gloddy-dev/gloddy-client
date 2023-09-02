12ef463447f824328ac6752163f23abfdb00a809
Kuesung Park <gueit214@naver.com>
2023-09-02 21:16:51 +0900

    feat : 서비스 이용약관 페이지 구현

--- src/app/(main)/profile/setting/information/components/InformationHeader.tsx
+++ src/app/(main)/profile/setting/information/components/InformationHeader.tsx
@@ -9,7 +9,7 @@ export default function InformationHeader() {
       <Header.Left className="px-4">
         <Link href="/profile/setting">
           <IconButton size="large">
-            <Image src="/icons/24/arrow_back.svg" width={24} height={24} alt="back" />
+            <Image src="/icons/24/close.svg" width={24} height={24} alt="back" />
           </IconButton>
         </Link>
         <p className="text-subtitle-1">개인정보 처리 방침</p>
