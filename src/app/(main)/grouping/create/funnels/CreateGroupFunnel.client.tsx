6739b894d594448d289dcdadc142f50007a2b180
kang <kangju2000@naver.com>
2023-09-02 13:01:38 +0900

    fix: 무한 렌더되는 이슈 해결

--- src/app/(main)/grouping/create/funnels/CreateGroupFunnel.client.tsx
+++ src/app/(main)/grouping/create/funnels/CreateGroupFunnel.client.tsx
@@ -4,22 +4,15 @@ import MainStep from './main/MainStep.client';
 import MeetDateStep from './meetDate/MeetDateStep.client';
 import CreateHeader from '../components/CreateHeader.client';
 import { useFunnel } from '@/hooks/useFunnel';
-import { SubmitHandler } from 'react-hook-form';
-
-import type { CreateGroupContextValue } from '../type';
 
 export default function CreateFunnel() {
   const { Funnel, currentStep, setStep, prevStep } = useFunnel(['main', 'meetDate']);
-
-  const handleSubmit: SubmitHandler<CreateGroupContextValue> = (data) => {
-    console.log(data);
-  };
 
   return (
     <Funnel>
       <CreateHeader currentStep={currentStep} />
       <Funnel.Step name="main">
-        <MainStep onSelectMeetDate={() => setStep('meetDate')} onSubmit={handleSubmit} />
+        <MainStep onSelectMeetDate={() => setStep('meetDate')} />
       </Funnel.Step>
       <Funnel.Step name="meetDate">
         <MeetDateStep onDone={prevStep} />
--- src/app/(main)/grouping/create/funnels/main/MainStep.client.tsx
+++ src/app/(main)/grouping/create/funnels/main/MainStep.client.tsx
@@ -2,20 +2,54 @@ import InputSection from './InputSection.client';
 import SettingSection from './SettingSection.client';
 import UploadSection from './UploadSection.client';
 import { useCreateGroupContext } from '../../components/CreateGroupContext';
+import CreateModal from '../../components/CreateModal.client';
+import { usePostCreateGroup } from '@/apis/groups';
 import { Button, ButtonGroup } from '@/components/Button';
 import { Spacing } from '@/components/common/Spacing';
 import { Divider } from '@/components/Divider';
+import { useModal } from '@/hooks/useModal';
 
 import type { CreateGroupContextValue } from '../../type';
 import type { SubmitHandler } from 'react-hook-form';
 
 interface MainStepProps {
   onSelectMeetDate: () => void;
-  onSubmit: SubmitHandler<CreateGroupContextValue>;
 }
 
-export default function MainStep({ onSelectMeetDate, onSubmit }: MainStepProps) {
+export default function MainStep({ onSelectMeetDate }: MainStepProps) {
   const { handleSubmit } = useCreateGroupContext();
+
+  const { mutate: mutateCreateGroup } = usePostCreateGroup();
+  const { open: openCreateModal, close: closeCreateModal } = useModal();
+
+  const onSubmit: SubmitHandler<CreateGroupContextValue> = (data) => {
+    console.log(data);
+
+    openCreateModal(
+      <CreateModal
+        onCancelClick={closeCreateModal}
+        onOkClick={() => {
+          mutateCreateGroup({
+            placeName: '스타벅스 천호로데오점',
+            placeAddress: '서울 강동구 천호대로 1037',
+            placeLatitude: '37.53777',
+            placeLongitude: '127.12722',
+            content: data.content,
+            maxUser: data.maxUser,
+            meetDate: '2021-10-10',
+            startTime: '14:00',
+            endTime: '16:00',
+            // startTime:
+            //   data.time.fromHour.padStart(2, '0') + ':' + data.time.fromMin.padStart(2, '0'),
+            // endTime: data.time.toHour.padStart(2, '0') + ':' + data.time.toMin.padStart(2, '0'),
+            title: data.title,
+
+            imageUrl: data.imageUrl,
+          });
+        }}
+      />
+    );
+  };
 
   return (
     <>
