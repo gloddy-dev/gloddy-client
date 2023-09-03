f3336bc4450593cd0b2597dccd267f45c3359e9b
Kuesung Park <gueit214@naver.com>
2023-09-03 19:15:53 +0900

    feat : footer 추가

---
+++ src/app/loading.tsx
@@ -0,0 +1,3 @@
+export default function loading() {
+  return <div className="h-full w-full bg-black">Loading...</div>;
+}
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
 import NoMeeting from './(main)/meeting/components/NoMeeting';
+import { Footer } from '@/components/Footer';
 
 export default function NotFound() {
   return (
@@ -7,6 +8,7 @@ export default function NotFound() {
         message={`[404] 페이지를 찾을 수 없습니다.
 입력하신 주소를 다시 확인해 주세요.`}
       />
+      <Footer />
     </div>
   );
 }
