e97acbc7385db1fb8f408ae78e772c4c0f67e7b3
kang <kangju2000@naver.com>
2023-09-03 20:35:36 +0900

    feat: 모임 나가기 기능 구현

--- src/app/(main)/grouping/components/DeleteModal.client.tsx
+++ src/app/(main)/grouping/components/DeleteModal.client.tsx
@@ -1,20 +1,37 @@
 import { Spacing } from '@/components/common/Spacing';
+import { Flex } from '@/components/Layout';
 import { Modal } from '@/components/Modal';
 import Image from 'next/image';
 
 interface DeleteModalProps {
-  content: string;
+  content: string | React.ReactNode;
+  description?: string | React.ReactNode;
   onOkClick: () => void;
+  okDisabled?: boolean;
   onCancelClick: () => void;
 }
 
-export default function DeleteModal({ content, onOkClick, onCancelClick }: DeleteModalProps) {
+export default function DeleteModal({
+  content,
+  description,
+  onOkClick,
+  okDisabled,
+  onCancelClick,
+}: DeleteModalProps) {
   return (
-    <Modal variant="warning" onCancelClick={onCancelClick} onOkClick={onOkClick}>
+    <Modal
+      variant="warning"
+      onCancelClick={onCancelClick}
+      onOkClick={onOkClick}
+      okDisabled={okDisabled}
+    >
       <Spacing size={32} />
       <Image src="/icons/48/warning.svg" alt="warning" width={48} height={48} />
       <Spacing size={12} />
-      <p>{content}</p>
+      <Flex direction="column" justify="center" align="center" className="gap-4">
+        <div>{content}</div>
+        {description && <div>{description}</div>}
+      </Flex>
       <Spacing size={16} />
     </Modal>
   );
