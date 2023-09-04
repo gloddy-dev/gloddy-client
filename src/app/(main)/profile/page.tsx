29c2f3ef21f446ebd8447ccecce61b233dcca12d
Kuesung Park <gueit214@naver.com>
2023-09-04 17:38:42 +0900

    feat : Loading 컴포넌트 구현

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -3,13 +3,14 @@ import ProfileHeader from './components/ProfileHeader.client';
 import { Keys, getProfile } from '@/apis/profile';
 import { HydrationProvider } from '@/components/common/Provider';
 import { Footer } from '@/components/Footer';
+import { Loading } from '@/components/Loading';
 import { QueryAsyncBoundary } from '@suspensive/react-query';
 
 export default function Profile() {
   return (
     <div className="h-full bg-sub">
       <ProfileHeader />
-      <QueryAsyncBoundary rejectedFallback={<div>에러</div>} pendingFallback={null}>
+      <QueryAsyncBoundary rejectedFallback={<div>에러</div>} pendingFallback={<Loading />}>
         <HydrationProvider queryFn={getProfile} queryKey={Keys.getProfile()}>
           <ProfileDetail />
         </HydrationProvider>
