b138d1cd67462199f3427ed9f075d0d479715f01
kang <kangju2000@naver.com>
2023-09-05 10:38:53 +0900

    refactor: 차단 및 신고 완료 모달 분리

---
+++ src/app/(main)/grouping/components/ReportDoneModal.client.tsx
@@ -0,0 +1,17 @@
+import { Spacing } from '@/components/common/Spacing';
+import { Modal } from '@/components/Modal';
+
+interface ReportDoneModalProps {
+  onOkClick: () => void;
+}
+export default function ReportDoneModal({ onOkClick }: ReportDoneModalProps) {
+  return (
+    <Modal variant="ok" okMessage="확인" onOkClick={onOkClick}>
+      <Spacing size={36} />
+      <p>신고가 접수되었습니다.</p>
+      <Spacing size={12} />
+      <p className="text-paragraph-1 text-sign-tertiary">빠른 처리를 위해 노력하겠습니다.</p>
+      <Spacing size={20} />
+    </Modal>
+  );
+}
