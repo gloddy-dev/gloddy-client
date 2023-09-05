8f95f916d3f629a0dd513be856bc37874efc1d28
Kuesung Park <gueit214@naver.com>
2023-09-05 15:43:41 +0900

    feat : 토큰 쿠키 설정

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,2 +1,4 @@
 export const currentKoreaTime = new Date(Date.now() + 9 * 60 * 60 * 1000);
-export const oneDay = 1000 * 60 * 60 * 24;
+export const currentTime = new Date();
+export const day1 = 1000 * 60 * 60 * 24;
+export const day60 = day1 * 60;
