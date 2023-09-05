6576d87a9efa800c885951360724ab101b0eaa23
Kuesung Park <gueit214@naver.com>
2023-09-05 11:39:10 +0900

    feat : mutation 에러 핸들링 토스트로 일괄 처리

--- src/apis/auth/mutations.ts
+++ src/apis/auth/mutations.ts
@@ -12,45 +12,17 @@ import { setTokenAtCookie } from '@/utils/auth/tokenController';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
-export const useLoginMutation = () => {
-  return useMutation(postLogin);
-};
+export const useLoginMutation = () => useMutation(postLogin);
 
-export const useReissueMutation = () => {
-  return useMutation(postReissue);
-};
+export const useReissueMutation = () => useMutation(postReissue);
 
-export const useSMSMutation = () => {
-  return useMutation(postSMS, {
-    onError: (error) => {
-      // TODO : 휴대폰 번호 에러에 대한 처리
-    },
-  });
-};
+export const useSMSMutation = () => useMutation(postSMS);
 
-export const useSMSVerifyMutation = () => {
-  return useMutation(postSMSVerify, {
-    onError: (error) => {
-      // TODO : 인증번호 에러에 대한 처리
-    },
-  });
-};
+export const useSMSVerifyMutation = () => useMutation(postSMSVerify);
 
-export const useEmailMutation = () => {
-  return useMutation(postEmail, {
-    onError: (error) => {
-      // TODO : 이메일 에러에 대한 처리
-    },
-  });
-};
+export const useEmailMutation = () => useMutation(postEmail);
 
-export const useEmailVerifyMutation = () => {
-  return useMutation(postEmailVerify, {
-    onError: (error) => {
-      // TODO : 이메일 에러에 대한 처리
-    },
-  });
-};
+export const useEmailVerifyMutation = () => useMutation(postEmailVerify);
 
 export const useSignUpMutation = () => {
   const router = useRouter();
@@ -62,9 +34,6 @@ export const useSignUpMutation = () => {
       } = data;
       setTokenAtCookie({ accessToken, refreshToken, userId });
       router.push('grouping');
-    },
-    onError: (error) => {
-      // TODO : 회원가입 에러에 대한 처리
     },
   });
 };
