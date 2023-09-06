e84593ac13c201a375dbb41b4f15f6a45efe8626
Kuesung Park <gueit214@naver.com>
2023-09-06 11:24:06 +0900

    feat : 닉네임 중복 체크 api

--- src/apis/auth/queries.ts
+++ src/apis/auth/queries.ts
@@ -1,7 +1,21 @@
-import { Keys, getSchoolSearch } from '.';
+import { Keys, NicknameDuplicateResponse, getNicknameDuplicate, getSchoolSearch } from '.';
 import { useSuspenseQuery } from '@suspensive/react-query';
+import { UseQueryOptions } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export const useGetSearchSchool = (searchWord: string) =>
   useSuspenseQuery(Keys.getSchoolSearch(searchWord), () => getSchoolSearch(searchWord), {
     enabled: !!searchWord,
   });
+
+export const useGetNicknameDuplicate = (
+  nickname: string,
+  { onSuccess }: { onSuccess: Pick<UseQueryOptions, 'onSuccess'> }
+) => {
+  return {
+    ...useSuspenseQuery(Keys.getNicknameDuplicate(nickname), () => getNicknameDuplicate(nickname), {
+      enabled: false,
+      onSuccess,
+    }),
+  };
+};
