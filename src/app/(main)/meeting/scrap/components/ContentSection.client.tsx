28934dad2b34713554ae62af70dc4615d3fbbe63
Kuesung Park <gueit214@naver.com>
2023-09-06 14:51:42 +0900

    feat : 찜한 모임 페이지

--- src/app/(main)/meeting/scrap/components/ContentSection.client.tsx
+++ src/app/(main)/meeting/scrap/components/ContentSection.client.tsx
@@ -1,10 +1,19 @@
 'use client';
 import NoMeeting from '../../components/NoMeeting';
 import { useGetMeetingScrap } from '@/apis/meeting';
+import { GroupingCard } from '@/components/Card';
+import { ItemList } from '@/components/List';
+import { Fragment } from 'react';
 
 export default function ContentSection() {
-  const { data } = useGetMeetingScrap();
-  console.log(data);
-  const arr = [];
-  return <div>{arr.length === 0 && <NoMeeting message="아직 찜한 모임이 없어요." />}</div>;
+  const {
+    data: { contents },
+  } = useGetMeetingScrap();
+
+  return (
+    <Fragment>
+      {contents.length === 0 && <NoMeeting message="아직 찜한 모임이 없어요." />}
+      <ItemList data={contents} renderItem={(content) => <GroupingCard groupingData={content} />} />
+    </Fragment>
+  );
 }
