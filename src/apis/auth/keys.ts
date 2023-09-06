baa313ebb2538950355519fac03d72491d1900c2
Kuesung Park <gueit214@naver.com>
2023-09-06 11:24:06 +0900

    feat : 닉네임 중복 체크 api

--- src/apis/auth/keys.ts
+++ src/apis/auth/keys.ts
@@ -1,3 +1,4 @@
 export const Keys = {
   getSchoolSearch: (searchWord: string) => ['getSchoolSearch', searchWord],
+  getNicknameDuplicate: (nickname: string) => ['getNicknameDuplicate', nickname],
 } as const;
