40d25b62fd55da77e65d18342c7c055c58a975dc
Kuesung Park <gueit214@naver.com>
2023-09-09 13:48:39 +0900

    refactor : 에러 코드 수정

--- src/apis/config/privateApi.ts
+++ src/apis/config/privateApi.ts
@@ -36,17 +36,20 @@ privateApi.interceptors.response.use(
   async (error: AxiosError<ErrorType, InternalAxiosRequestConfig>) => {
     try {
       if (!error.response) return Promise.reject(error);
-      if (error.response.status === AUTH_ERROR_CODES.TOKEN_ERROR) {
+      if (
+        error.response.status === AUTH_ERROR_CODES.UNAUTHORIZED ||
+        error.response.status === AUTH_ERROR_CODES.NOT_FOUND
+      ) {
         window.location.href = '/join?step=1';
       }
-      if (error.response.status === AUTH_ERROR_CODES.EXPIRED_TOKEN_ERROR) {
+      if (error.response.status === AUTH_ERROR_CODES.TOKEN_ERROR) {
         try {
           const { refreshToken, accessToken } = await getTokenFromCookie();
           if (!refreshToken || !accessToken) {
             throw new ApiError(
               '에러 발생',
               '토큰이 없습니다.',
-              AUTH_ERROR_CODES.TOKEN_ERROR,
+              AUTH_ERROR_CODES.UNAUTHORIZED,
               new Date()
             );
           }
@@ -62,7 +65,7 @@ privateApi.interceptors.response.use(
             throw new ApiError(
               '에러 발생',
               'accessToken 발급 중 오류가 발생했습니다.',
-              AUTH_ERROR_CODES.TOKEN_ERROR,
+              AUTH_ERROR_CODES.UNAUTHORIZED,
               new Date()
             );
           }
@@ -72,7 +75,7 @@ privateApi.interceptors.response.use(
             throw new ApiError(
               '에러 발생',
               '이전 정보가 없습니다.',
-              AUTH_ERROR_CODES.TOKEN_ERROR,
+              AUTH_ERROR_CODES.UNAUTHORIZED,
               new Date()
             );
           }
--- src/constants/errorCode.ts
+++ src/constants/errorCode.ts
@@ -1,5 +1,8 @@
 export const AUTH_ERROR_CODES = {
-  UNAUTHORIZED_ERROR: 401,
-  EXPIRED_TOKEN_ERROR: 412,
+  /* 올바르지 않은 토큰 | 토큰이 비어짐 | 토큰 만료 => 토큰 재발급 */
   TOKEN_ERROR: 403,
+  /* 유효하지 않은 토큰 => 회원가입 페이지로 이동 */
+  UNAUTHORIZED: 401,
+  /* 존재하지 않는 유저 => 회원가입 페이지로 이동 */
+  NOT_FOUND: 404,
 } as const;
