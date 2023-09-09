92a87c6f4cdcc83d9ad65eb78c502660ad1e2818
Kuesung Park <gueit214@naver.com>
2023-09-09 20:24:10 +0900

    refactor : 생년월일 수정 불가

--- src/apis/profile/queries.ts
+++ src/apis/profile/queries.ts
@@ -5,17 +5,11 @@ import { useSuspenseQuery } from '@suspensive/react-query';
 export const useGetProfile = () =>
   useSuspenseQuery(Keys.getProfile(), getProfile, {
     select: (data) => {
-      const { birth, introduce } = data;
-
-      const formattedBirth = {
-        year: +birth.split('.')[0] + '년',
-        month: +birth.split('.')[1] + '월',
-        date: +birth.split('.')[2] + '일',
-      };
+      const { introduce } = data;
 
       const defaultIntroduce = introduce ?? '';
 
-      return { ...data, introduce: defaultIntroduce, birth: formattedBirth };
+      return { ...data, introduce: defaultIntroduce };
     },
   });
 
