ac97e30b813a8f6de6fec287b28a123b2248b409
Kuesung Park <gueit214@naver.com>
2023-09-09 14:09:02 +0900

    refactor : input type 설정

--- src/app/(sub)/join/funnels/step1/components/NumberForm.client.tsx
+++ src/app/(sub)/join/funnels/step1/components/NumberForm.client.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useJoinContext } from '../../../components/JoinContext.client';
 import { formatNumber, formatNumberBackSpace } from '../util';
 import { useSMSMutation } from '@/apis/auth';
@@ -68,6 +66,7 @@ export default function NumberForm({ inputStatus, setInputStatus }: NumberSectio
         isSpacing={false}
         readOnly={inputStatus === 'afterSend'}
         isLeftCaptionWrap={false}
+        type="tel"
       />
 
       <Spacing size={8} />
