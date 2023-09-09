c3b1da4b30fcb07be0a9606dfe37a54bb9e74c46
Kuesung Park <gueit214@naver.com>
2023-09-09 15:04:09 +0900

    refactor : 주석 해ㅔ

--- src/app/(main)/grouping/[groupId]/members/page.tsx
+++ src/app/(main)/grouping/[groupId]/members/page.tsx
@@ -2,10 +2,10 @@ import MemeberList from './components/MemberList.client';
 import MembersHeader from './components/MembersHeader.client';
 import { Keys, getGroupMembers } from '@/apis/groups';
 import { RejectedFallback } from '@/components/ErrorBoundary';
+import { Loading } from '@/components/Loading';
 import { PageAnimation } from '@/components/PageAnimation';
 import { HydrationProvider } from '@/components/Provider';
 import { QueryAsyncBoundary } from '@suspensive/react-query';
-import { Loading } from 'antd-mobile';
 
 interface GroupingMembersPageProps {
   params: {
@@ -19,16 +19,16 @@ export default function GroupingMembersPage({ params }: GroupingMembersPageProps
   return (
     <>
       <MembersHeader />
-      {/* <QueryAsyncBoundary rejectedFallback={RejectedFallback} pendingFallback={<Loading />}> */}
-      <PageAnimation>
-        <HydrationProvider
-          queryFn={() => getGroupMembers(groupId)}
-          queryKey={Keys.getGroupMembers(groupId)}
-        >
-          <MemeberList />
-        </HydrationProvider>
-      </PageAnimation>
-      {/* </QueryAsyncBoundary> */}
+      <QueryAsyncBoundary rejectedFallback={RejectedFallback} pendingFallback={<Loading />}>
+        <PageAnimation>
+          <HydrationProvider
+            queryFn={() => getGroupMembers(groupId)}
+            queryKey={Keys.getGroupMembers(groupId)}
+          >
+            <MemeberList />
+          </HydrationProvider>
+        </PageAnimation>
+      </QueryAsyncBoundary>
     </>
   );
 }
