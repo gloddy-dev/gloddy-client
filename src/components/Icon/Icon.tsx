ed96481404e3eb9ca2cf1e3ae0e436a161c05187
kang <kangju2000@naver.com>
2023-09-10 16:13:52 +0900

    refactor: 말줄임 설정

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,12 +1,22 @@
+import cn from '@/utils/cn';
+
 import type { SVGProps } from 'react';
 
 interface IconProps extends SVGProps<SVGSVGElement> {
   id: string;
+  className?: string;
 }
 
-export default function Icon({ id, width = 24, height = 24, fill = 'none', ...rest }: IconProps) {
+export default function Icon({
+  id,
+  width = 24,
+  height = 24,
+  fill = 'none',
+  className,
+  ...rest
+}: IconProps) {
   return (
-    <svg width={width} height={height} fill={fill} {...rest}>
+    <svg width={width} height={height} fill={fill} className={cn('shrink-0', className)} {...rest}>
       <use href={`/sprite/sprite.svg#${id}`} />
     </svg>
   );
