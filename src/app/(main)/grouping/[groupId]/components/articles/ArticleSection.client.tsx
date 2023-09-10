54f7b2174f3b1daf51a55c2273d887c5eb0b2af7
kang <kangju2000@naver.com>
2023-09-11 02:03:17 +0900

    feat: 차단 또는 신고 시 localStorage에 id를 넣어 가려지도록 설정

--- src/app/(main)/grouping/[groupId]/components/articles/ArticleSection.client.tsx
+++ src/app/(main)/grouping/[groupId]/components/articles/ArticleSection.client.tsx
@@ -4,21 +4,27 @@ import { useGetArticles, useGetGroupDetail } from '@/apis/groups/queries';
 import ArticleItem from '@/app/(main)/grouping/components/ArticleItem.client';
 import { ItemList } from '@/components/List';
 import { useNumberParams } from '@/hooks/useNumberParams';
+import { useBlockStore } from '@/store/useBlockStore';
 
 export default function ArticleSection() {
+  const { blockArticleIds } = useBlockStore();
   const { groupId } = useNumberParams<['groupId']>();
-
   const { data: articlesData } = useGetArticles(groupId);
   const { data: groupDetailData } = useGetGroupDetail(groupId);
+
   const { isCaptain } = groupDetailData;
 
   return (
     <section>
       <ItemList
         data={articlesData}
-        renderItem={(article) => (
-          <ArticleItem article={article} isCaptain={isCaptain} groupId={groupId} />
-        )}
+        renderItem={(article) => {
+          return (
+            !blockArticleIds.includes(article.articleId) && (
+              <ArticleItem article={article} isCaptain={isCaptain} groupId={groupId} />
+            )
+          );
+        }}
       />
     </section>
   );
