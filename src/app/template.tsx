8238733939353e39d51bb1ae161d5c79d69e8cb2
Kuesung Park <gueit214@naver.com>
2023-09-11 19:03:02 +0900

    refactor : useSentry

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,8 +1,14 @@
 'use client';
 import { useEasterEgg } from '@/hooks/useEasterEgg';
+import { useSentry } from '@/hooks/useSentry';
 
 export default function Step4Layout({ children }: { children: React.ReactNode }) {
   useEasterEgg();
+
+  useSentry({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || '',
+    allowUrls: ['https://gloddy.vercel.app'],
+  });
 
   return children;
 }
