b87af6c51524c6c26ab0495d5b7b3c5eae079dea
kang <kangju2000@naver.com>
2023-09-12 15:09:22 +0900

    fix: CI 에러 수정

--- src/components/ErrorBoundary/RetryErrorBoundary.tsx
+++ src/components/ErrorBoundary/RetryErrorBoundary.tsx
@@ -10,7 +10,7 @@ export default function RetryErrorBoundary({ children }: { children: React.React
           <p className="text-h5 text-black">문제가 발생했습니다</p>
           <p className="text-b3 text-grey-600 mt-2">페이지를 불러오는데 실패했습니다.</p>
           <button
-            className="bg-grey-200 text-grey-800 mt-16 rounded-lg px-12 py-8 text-13 font-bold"
+            className="bg-grey-200 text-grey-800 text-13 mt-16 rounded-lg px-12 py-8 font-bold"
             onClick={() => resetErrorBoundary()}
           >
             다시 불러오기
