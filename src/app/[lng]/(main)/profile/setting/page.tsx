0c423605576f082573374c1c6fd6fee10e443e32
Kuesung Park <gueit214@naver.com>
2023-09-13 10:16:02 +0900

    fix : 데이터 fetching없는 페이지 애니메이션 제거

--- src/app/[lng]/(main)/profile/setting/page.tsx
+++ src/app/[lng]/(main)/profile/setting/page.tsx
@@ -1,16 +1,13 @@
 import LinkSection from './components/LinkSection.server';
 import ProfileSection from './components/ProfileSection.client';
 import SettingHeader from './components/SettingHeader';
-import { PageAnimation } from '@/components/PageAnimation';
 
 export default function page() {
   return (
     <>
       <SettingHeader />
-      <PageAnimation>
-        <ProfileSection />
-        <LinkSection />
-      </PageAnimation>
+      <ProfileSection />
+      <LinkSection />
     </>
   );
 }
