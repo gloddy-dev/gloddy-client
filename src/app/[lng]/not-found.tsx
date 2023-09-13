17f9ec32bf5dd46f31ba2a3bbbfde3f10ba950f9
Kuesung Park <gueit214@naver.com>
2023-09-13 15:41:57 +0900

    fix : type 에러

--- src/app/[lng]/not-found.tsx
+++ src/app/[lng]/not-found.tsx
@@ -1,14 +1,20 @@
 import NoMeeting from './(main)/meeting/components/NoMeeting';
 import { Footer } from '@/components/Footer';
 
-export default function NotFound() {
+interface NotFoundProps {
+  params: {
+    lng: string;
+  };
+}
+
+export default function NotFound({ params: { lng } }: NotFoundProps) {
   return (
     <div>
       <NoMeeting
         message={`[404] 페이지를 찾을 수 없습니다.
 입력하신 주소를 다시 확인해 주세요.`}
       />
-      <Footer />
+      <Footer lng={lng} />
     </div>
   );
 }
