1538df9b9ed00b809549bfe9ece62a2a79a77177
Kuesung Park <gueit214@naver.com>
2023-09-13 23:19:15 +0900

    translate : 나의 모임 번역

--- src/app/[lng]/(main)/profile/praise/components/PraiseHeader.tsx
+++ src/app/[lng]/(main)/profile/praise/components/PraiseHeader.tsx
@@ -1,9 +1,12 @@
+'use client';
+import { useTranslation } from '@/app/i18n/client';
 import { IconButton } from '@/components/Button';
 import { Header } from '@/components/Header';
 import { Icon } from '@/components/Icon';
 import Link from 'next/link';
 
 export default function PraiseHeader() {
+  const { t } = useTranslation('profile');
   return (
     <Header>
       <Header.Left className="px-4">
@@ -12,7 +15,7 @@ export default function PraiseHeader() {
             <Icon id="24-arrow_back" />
           </IconButton>
         </Link>
-        <p className="text-subtitle-1">받은 칭찬</p>
+        <p className="text-subtitle-1">{t('home.praiseCount')}</p>
       </Header.Left>
     </Header>
   );
