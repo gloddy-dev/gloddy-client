ddec0403b9cdfb7b0027ac4c6330d5ef88f428fd
Kuesung Park <gueit214@naver.com>
2023-09-14 09:50:45 +0900

    fix : type 에러

--- src/app/[lng]/(sub)/join/type.ts
+++ src/app/[lng]/(sub)/join/type.ts
@@ -1,7 +1,5 @@
 import { SignUpRequest } from '@/apis/auth';
 
-import type { DateType } from '@/types';
-
 export interface SignUpState extends Omit<SignUpRequest, 'personalities' | 'birth'> {
   verifyNumber: string;
   verifyEmailNumber: string;
