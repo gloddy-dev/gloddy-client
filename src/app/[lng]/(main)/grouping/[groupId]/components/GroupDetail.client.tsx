6181b2cccf7f4c1d83f5fbe02f6edf7da41ad03a
kang <kangju2000@naver.com>
2023-09-15 20:02:34 +0900

    refactor: DetailContent 페이지 분리

--- src/app/[lng]/(main)/grouping/[groupId]/components/GroupDetail.client.tsx
+++ src/app/[lng]/(main)/grouping/[groupId]/components/GroupDetail.client.tsx
@@ -1,28 +1,25 @@
 'use client';
 
 import { ArticleSection, NoticeSection } from './articles';
-import { LocationSection, MemberSection, TimeSection } from './detail';
+import DetailContent from './detail/DetailContent.client';
 import TopSection from './TopSection.client';
 import { useGetGroupDetail } from '@/apis/groups';
 import { useTranslation } from '@/app/i18n/client';
-import { Button, ButtonGroup, FloatAddButton } from '@/components/Button';
+import { FloatAddButton } from '@/components/Button';
 import { Divider } from '@/components/Divider';
 import { Spacing } from '@/components/Spacing';
 import { Tabs } from '@/components/Tabs';
+import { useNumberParams } from '@/hooks/useNumberParams';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { Suspense } from 'react';
 
-interface GroupDetailProps {
-  groupId: number;
-}
-
-export default function GroupDetailPage({ groupId }: GroupDetailProps) {
+export default function GroupDetailPage() {
+  const pathname = usePathname();
   const { t } = useTranslation('groupDetail');
+  const { groupId } = useNumberParams<['groupId']>();
   const { data: groupDetailData } = useGetGroupDetail(groupId);
-  const router = useRouter();
-
-  const { myGroup, isApplyWaited } = groupDetailData;
+  const { myGroup } = groupDetailData;
 
   return (
     <>
@@ -34,23 +31,14 @@ export default function GroupDetailPage({ groupId }: GroupDetailProps) {
           <Tabs.Tab value="articles" text={t('board.tab')} disabled={!myGroup} />
         </Tabs.List>
         <Tabs.Panel value="detail">
-          <Suspense>
-            <div className="px-20">
-              <Spacing size={20} />
-              <MemberSection />
-              <Spacing size={36} />
-              <TimeSection />
-              <Spacing size={28} />
-              <LocationSection />
-            </div>
-          </Suspense>
+          <DetailContent />
         </Tabs.Panel>
         <Tabs.Panel value="articles">
           <Suspense>
             <NoticeSection />
             <ArticleSection />
             <div className="bottom-fixed flex justify-end">
-              <Link href={`/grouping/${groupId}/write`}>
+              <Link href={`${pathname}/write`}>
                 <FloatAddButton />
               </Link>
             </div>
@@ -59,16 +47,6 @@ export default function GroupDetailPage({ groupId }: GroupDetailProps) {
       </Tabs>
 
       <Spacing size={60} />
-      {!myGroup && (
-        <ButtonGroup>
-          <Button
-            onClick={() => router.push(`/grouping/${groupId}/apply`)}
-            disabled={isApplyWaited}
-          >
-            {t(isApplyWaited ? 'details.wait' : 'details.join')}
-          </Button>
-        </ButtonGroup>
-      )}
     </>
   );
 }
