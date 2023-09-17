c1a662cf1f10b41406f3a9a6df339ff06a2ef2fd
Kuesung Park <gueit214@naver.com>
2023-09-17 13:37:48 +0900

    translate : 회원 탈퇴 page

--- src/app/[lng]/(main)/profile/setting/delete/components/Step1.tsx
+++ src/app/[lng]/(main)/profile/setting/delete/components/Step1.tsx
@@ -1,4 +1,5 @@
 import { useDeleteContext } from './DeleteProvider.client';
+import { useTranslation } from '@/app/i18n/client';
 import { Button, ButtonGroup } from '@/components/Button';
 import { CircleCheckbox } from '@/components/Checkbox';
 import { Flex } from '@/components/Layout';
@@ -9,13 +10,14 @@ interface Step1Props {
   onNextClick: () => void;
 }
 const infoList = [
-  `지금까지 진행했던 모임에서 받은\n모든 칭찬 내역이 사라집니다`,
-  `지금까지 진행했던 모임에서 받은 다른 회원의\n'최고의 짝꿍'  후기 내용이 사라집니다`,
-  `회원 탈퇴시 개인 프로필 내 신뢰도 지표\n데이터가 사라지며 다시 복구할 수 없습니다.`,
-  `회원 탈퇴시 모임 활동을 진행했던\n모든 데이터가 삭제되며 복구할 수 없습니다.`,
+  'deleteAllPraises',
+  'deleteBestPartnerReviews',
+  'deleteTrustScore',
+  'deleteGroupData',
 ];
 
 export default function Step1({ onNextClick }: Step1Props) {
+  const { t } = useTranslation('profile');
   const { setValue, watch } = useDeleteContext();
   const handleDeleteAgree = () => {
     setValue('isDeleteAgree', !watch('isDeleteAgree'));
@@ -24,11 +26,11 @@ export default function Step1({ onNextClick }: Step1Props) {
   return (
     <div>
       <Spacing size={32} />
-      <h3 className="px-20 text-h3">정말 탈퇴하시겠어요?</h3>
+      <h3 className="px-20 text-h3">{t('settings.confirmDeletion')}</h3>
       <Spacing size={16} />
       <div className="px-20">
         {infoList.map((info, index) => (
-          <TextList key={index}>{info}</TextList>
+          <TextList key={index}>{t(`settings.${info}`)}</TextList>
         ))}
       </div>
 
@@ -37,9 +39,7 @@ export default function Step1({ onNextClick }: Step1Props) {
       <Flex className="px-20" onClick={handleDeleteAgree}>
         <CircleCheckbox checked={watch('isDeleteAgree')} />
         <Spacing size={8} direction="horizontal" />
-        <p className="text-subtitle-2 text-sign-secondary">
-          위 내용을 확인하였으며, 이에 동의합니다.
-        </p>
+        <p className="text-subtitle-2 text-sign-secondary">{t('settings.agreeTerms')}</p>
       </Flex>
 
       <ButtonGroup>
