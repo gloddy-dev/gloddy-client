a8b6863c3d499c52e7845ac6154f96d65434148c
Kuesung Park <gueit214@naver.com>
2023-09-17 13:12:33 +0900

    refactor : select > language로 수정

--- src/app/[lng]/(main)/profile/setting/langauge/components/Language.tsx
+++ src/app/[lng]/(main)/profile/setting/langauge/components/Language.tsx
@@ -4,31 +4,31 @@ import { Button, ButtonGroup } from '@/components/Button';
 import { CircleCheckbox } from '@/components/Checkbox';
 import { Flex } from '@/components/Layout';
 import { Spacing } from '@/components/Spacing';
-import { setLocalCookie } from '@/utils/cookieController';
+import { getLocalCookie, setLocalCookie } from '@/utils/cookieController';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function Language() {
-  const { i18n, t } = useTranslation('common');
+  const { t } = useTranslation('common');
   const router = useRouter();
-  const [isKorean, isEnglish] = [i18n.language === 'ko', i18n.language === 'en'];
-  const [select, setSelect] = useState(isKorean ? 'ko' : 'en');
+
+  const [language, setLanguage] = useState(getLocalCookie('i18next') || 'ko');
 
   const handleSubmit = () => {
-    setLocalCookie('i18next', select);
-    router.replace(`/${select}/grouping`);
+    setLocalCookie('i18next', language);
+    router.replace(`/${language}/grouping`);
   };
 
   return (
     <Flex className="px-20 text-subtitle-2 text-sign-secondary" direction="column">
-      <Flex className="py-12" onClick={() => setSelect('ko')}>
-        <CircleCheckbox checked={select === 'ko'} />
+      <Flex className="py-12" onClick={() => setLanguage('ko')}>
+        <CircleCheckbox checked={language === 'ko'} />
         <Spacing size={8} direction="horizontal" />
         <span>한글</span>
       </Flex>
-      <Flex className="py-12" onClick={() => setSelect('en')}>
-        <CircleCheckbox checked={select === 'en'} />
+      <Flex className="py-12" onClick={() => setLanguage('en')}>
+        <CircleCheckbox checked={language === 'en'} />
         <Spacing size={8} direction="horizontal" />
         <span>English</span>
       </Flex>
