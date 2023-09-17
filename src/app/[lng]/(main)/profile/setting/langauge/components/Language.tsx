d80dd7090a697eef5ef9459e17f5321c720b721d
Kuesung Park <gueit214@naver.com>
2023-09-17 13:03:26 +0900

    design : 바뀐 디자인으로 반영

--- src/app/[lng]/(main)/profile/setting/langauge/components/Language.tsx
+++ src/app/[lng]/(main)/profile/setting/langauge/components/Language.tsx
@@ -1,29 +1,40 @@
 'use client';
 
-import { Button } from '@/components/Button';
+import { Button, ButtonGroup } from '@/components/Button';
+import { CircleCheckbox } from '@/components/Checkbox';
 import { Flex } from '@/components/Layout';
+import { Spacing } from '@/components/Spacing';
 import { setLocalCookie } from '@/utils/cookieController';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function Language() {
-  const { i18n } = useTranslation();
-
+  const { i18n, t } = useTranslation('common');
   const router = useRouter();
-  const handleKorean = () => {
-    setLocalCookie('i18next', 'ko');
-    i18n.changeLanguage('ko');
-    router.replace('/ko/grouping');
+  const [isKorean, isEnglish] = [i18n.language === 'ko', i18n.language === 'en'];
+  const [select, setSelect] = useState(isKorean ? 'ko' : 'en');
+
+  const handleSubmit = () => {
+    setLocalCookie('i18next', select);
+    router.replace(`/${select}/grouping`);
   };
-  const handleEnglish = () => {
-    setLocalCookie('i18next', 'en');
-    i18n.changeLanguage('en');
-    router.replace('/en/grouping');
-  };
+
   return (
-    <Flex>
-      <Button onClick={handleKorean}>한글</Button>
-      <Button onClick={handleEnglish}>영어</Button>
+    <Flex className="px-20 text-subtitle-2 text-sign-secondary" direction="column">
+      <Flex className="py-12" onClick={() => setSelect('ko')}>
+        <CircleCheckbox checked={select === 'ko'} />
+        <Spacing size={8} direction="horizontal" />
+        <span>한글</span>
+      </Flex>
+      <Flex className="py-12" onClick={() => setSelect('en')}>
+        <CircleCheckbox checked={select === 'en'} />
+        <Spacing size={8} direction="horizontal" />
+        <span>English</span>
+      </Flex>
+      <ButtonGroup>
+        <Button onClick={handleSubmit}>{t('confirm')}</Button>
+      </ButtonGroup>
     </Flex>
   );
 }
