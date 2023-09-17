9ec1b1f88f9742c3f3c78a6466a521656cf25565
Kuesung Park <gueit214@naver.com>
2023-09-17 11:54:49 +0900

    feat : 언어 변경 페이지

--- src/app/[lng]/(main)/grouping/components/GroupingCardList.client.tsx
+++ src/app/[lng]/(main)/grouping/components/GroupingCardList.client.tsx
@@ -3,6 +3,7 @@ import { useGetGroups } from '@/apis/groups';
 import { GroupingCard } from '@/components/Card';
 import { ItemList } from '@/components/List';
 import { useBlockStore } from '@/store/useBlockStore';
+import { setLocalCookie } from '@/utils/cookieController';
 import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
@@ -18,6 +19,20 @@ export default function GroupingCardList() {
 
   return (
     <>
+      <button
+        onClick={() => {
+          setLocalCookie('i18next', 'ko');
+        }}
+      >
+        한국
+      </button>
+      <button
+        onClick={() => {
+          setLocalCookie('i18next', 'en');
+        }}
+      >
+        영어
+      </button>
       <ItemList
         data={data}
         renderItem={(grouping) =>
---
+++ src/app/[lng]/(main)/profile/setting/langauge/components/Language.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Button } from '@/components/Button';
+import { Flex } from '@/components/Layout';
+import { setLocalCookie } from '@/utils/cookieController';
+import { useRouter } from 'next/navigation';
+import { useTranslation } from 'react-i18next';
+
+export default function Language() {
+  const { i18n } = useTranslation();
+
+  const router = useRouter();
+  const handleKorean = () => {
+    setLocalCookie('i18next', 'ko');
+    i18n.changeLanguage('ko');
+    router.replace('/ko/profile/setting/langauge');
+  };
+  const handleEnglish = () => {
+    setLocalCookie('i18next', 'en');
+    i18n.changeLanguage('en');
+    router.replace('/en/profile/setting/langauge');
+  };
+  return (
+    <Flex>
+      <Button onClick={handleKorean}>한글</Button>
+      <Button onClick={handleEnglish}>영어</Button>
+    </Flex>
+  );
+}
