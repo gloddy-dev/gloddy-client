1dec241b87f9a98a6038726b3ec1e44c28476e4f
Kuesung Park <gueit214@naver.com>
2023-09-20 23:43:09 +0900

    refactor : client 컴포넌트로 전환

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,37 +1,46 @@
+'use client';
 import { postReissue } from '@/apis/auth';
 import { AUTH_KEYS } from '@/constants/token';
 import { getTokenFromCookie } from '@/utils/auth/tokenController';
 import { afterDay60 } from '@/utils/date';
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { NextResponse } from 'next/server';
+import { useCallback, useEffect } from 'react';
 
-export default async function Home() {
-  const { accessToken, refreshToken } = (await getTokenFromCookie()) as {
-    accessToken: string;
-    refreshToken: string;
-  };
+export default function Home() {
+  const router = useRouter();
+  const checkToken = useCallback(async () => {
+    const { accessToken, refreshToken } = (await getTokenFromCookie()) as {
+      accessToken: string;
+      refreshToken: string;
+    };
 
-  if (accessToken && refreshToken) return redirect('/grouping');
+    if (accessToken && refreshToken) router.push('/grouping');
 
-  try {
-    const {
-      token: { accessToken: reIssuedAccessToken, refreshToken: reIssuedRefreshToken },
-    } = await postReissue(
-      { accessToken, refreshToken },
-      { headers: { 'X-AUTH-TOKEN': accessToken } }
-    );
+    try {
+      const {
+        token: { accessToken: reIssuedAccessToken, refreshToken: reIssuedRefreshToken },
+      } = await postReissue(
+        { accessToken, refreshToken },
+        { headers: { 'X-AUTH-TOKEN': accessToken } }
+      );
 
-    const response = NextResponse.next();
-    response.cookies.set(AUTH_KEYS.accessToken, reIssuedAccessToken, {
-      expires: afterDay60,
-    });
-    response.cookies.set(AUTH_KEYS.refreshToken, reIssuedRefreshToken, {
-      expires: afterDay60,
-    });
+      const response = NextResponse.next();
+      response.cookies.set(AUTH_KEYS.accessToken, reIssuedAccessToken, {
+        expires: afterDay60,
+      });
+      response.cookies.set(AUTH_KEYS.refreshToken, reIssuedRefreshToken, {
+        expires: afterDay60,
+      });
 
-    redirect('/grouping');
-  } catch (e) {
-    console.log(e);
-    return redirect('/join');
-  }
+      router.push('/grouping');
+    } catch (e) {
+      console.log(e);
+      router.push('/join');
+    }
+  }, []);
+
+  useEffect(() => {
+    checkToken();
+  }, [checkToken]);
 }
