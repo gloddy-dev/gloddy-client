02a0475bc4690ac07e08d9c97dd4c9d16278d250
Kuesung Park <gueit214@naver.com>
2023-09-22 13:17:00 +0900

    fix : login도 refresh

--- src/app/[lng]/(sub)/login/Login.tsx
+++ src/app/[lng]/(sub)/login/Login.tsx
@@ -27,6 +27,7 @@ export default function Login() {
                 userId,
               });
             }
+            router.refresh();
             router.push('/grouping');
           })();
         },
