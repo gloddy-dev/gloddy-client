75acbee13f29fcf41feec07847ed27662b43e279
Kuesung Park <gueit214@naver.com>
2023-09-22 13:07:02 +0900

    feat : 로그인 후 grouping페이지로 라우팅

--- src/app/[lng]/(sub)/login/Login.tsx
+++ src/app/[lng]/(sub)/login/Login.tsx
@@ -27,7 +27,7 @@ export default function Login() {
                 userId,
               });
             }
-            router.push('/');
+            router.push('/grouping');
           })();
         },
       }
