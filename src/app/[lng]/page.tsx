a077476d032a25fc32c158821582f8353203e70a
Kuesung Park <gueit214@naver.com>
2023-09-26 13:16:57 +0900

    test : 루트 페이지 변경 이전으로 백업

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,41 +1,40 @@
 'use client';
 import { cookieName } from '../i18n/settings';
 import { useDidMount } from '@/hooks/common/useDidMount';
+import { getTokenFromCookie } from '@/utils/auth/tokenController';
 import { setLocalCookie } from '@/utils/cookieController';
 import { afterDay60 } from '@/utils/date';
 import { useRouter } from 'next/navigation';
 
-interface HomeProps {
-  params: {
-    lng: string;
-  };
-}
-
-export default function Home({ params: { lng } }: HomeProps) {
+export default function Home() {
   const router = useRouter();
 
-  const listener = async (event: any) => {
-    console.log(event); // 모바일에서 보낸 데이터 확인하기 위한 로그
-    const response = await JSON.parse(event.data);
-    const { data } = response;
-    setLocalCookie(cookieName, data, {
-      expires: afterDay60,
-    });
+  const checkLanguageCookie = () => {
+    const listener = async (event: any) => {
+      const response = await JSON.parse(event.data);
+      const { data } = response;
+      setLocalCookie(cookieName, data, {
+        expires: afterDay60,
+      });
+    };
 
-    router.replace(`/${lng}/grouping`);
-  };
-
-  useDidMount(() => {
     if (window.ReactNativeWebView) {
       document.addEventListener('message', listener); /* Android */
       window.addEventListener('message', listener); /* iOS */
-
-      return () => {
-        document.removeEventListener('message', listener);
-        window.removeEventListener('message', listener);
-      };
     } else {
-      router.replace(`/${lng}/grouping`);
+      router.push('/join');
     }
+  };
+
+  const checkTokenCookie = async () => {
+    const { accessToken, refreshToken } = await getTokenFromCookie();
+
+    if (accessToken && refreshToken) router.push('/grouping');
+    else router.push('/join?step=1');
+  };
+
+  useDidMount(async () => {
+    await checkLanguageCookie();
+    await checkTokenCookie();
   });
 }
