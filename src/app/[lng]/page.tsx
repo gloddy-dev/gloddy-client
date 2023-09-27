3c74746f768762504dd14dd56e734a97828a277f
kang <kangju2000@naver.com>
2023-09-27 17:03:34 +0900

    fix: 온보딩에서 페이지 넘어갈 때 로직 수정

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { cookieName } from '../i18n/settings';
 import { useDidMount } from '@/hooks/common/useDidMount';
-import { getTokenFromCookie } from '@/utils/auth/tokenController';
 import { setLocalCookie } from '@/utils/cookieController';
 import { afterDay60 } from '@/utils/date';
 import { useRouter } from 'next/navigation';
@@ -9,32 +8,27 @@ import { useRouter } from 'next/navigation';
 export default function Home() {
   const router = useRouter();
 
-  const checkLanguageCookie = () => {
+  const checkLanguageCookie = async () => {
     const listener = async (event: any) => {
       const response = await JSON.parse(event.data);
       const { data } = response;
       setLocalCookie(cookieName, data, {
         expires: afterDay60,
       });
+
+      router.refresh();
+      router.push('/join');
     };
 
     if (window.ReactNativeWebView) {
       document.addEventListener('message', listener); /* Android */
       window.addEventListener('message', listener); /* iOS */
     } else {
-      router.push('/join');
+      router.push('/grouping');
     }
   };
 
-  const checkTokenCookie = async () => {
-    const { accessToken, refreshToken } = await getTokenFromCookie();
-
-    if (accessToken && refreshToken) router.push('/grouping');
-    else router.push('/join?step=1');
-  };
-
-  useDidMount(async () => {
-    await checkLanguageCookie();
-    await checkTokenCookie();
+  useDidMount(() => {
+    checkLanguageCookie();
   });
 }
