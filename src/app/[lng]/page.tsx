68b7a60ea967ac267ea031a643ac7338212c56da
kang <kangju2000@naver.com>
2023-09-27 17:15:53 +0900

    fix: 언마운트 시 이벤트리스너 제거

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -4,31 +4,33 @@ import { useDidMount } from '@/hooks/common/useDidMount';
 import { setLocalCookie } from '@/utils/cookieController';
 import { afterDay60 } from '@/utils/date';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter();
 
-  const checkLanguageCookie = async () => {
-    const listener = async (event: any) => {
-      const response = await JSON.parse(event.data);
-      const { data } = response;
-      setLocalCookie(cookieName, data, {
-        expires: afterDay60,
-      });
+  const listener = async (event: any) => {
+    const { data } = await JSON.parse(event.data);
+    setLocalCookie(cookieName, data, {
+      expires: afterDay60,
+    });
 
-      router.refresh();
-      router.push('/join');
-    };
-
-    if (window.ReactNativeWebView) {
-      document.addEventListener('message', listener); /* Android */
-      window.addEventListener('message', listener); /* iOS */
-    } else {
-      router.push('/grouping');
-    }
+    router.refresh();
+    router.push(`/${data}/join?step=1`);
   };
 
-  useDidMount(() => {
-    checkLanguageCookie();
-  });
+  useEffect(() => {
+    if (!window.ReactNativeWebView) {
+      router.push('/grouping');
+      return;
+    }
+
+    document.addEventListener('message', listener); /* Android */
+    window.addEventListener('message', listener); /* iOS */
+
+    return () => {
+      document.removeEventListener('message', listener); /* Android */
+      window.removeEventListener('message', listener); /* iOS */
+    };
+  }, []);
 }
