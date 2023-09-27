957b701bcae815322c1757c302afe200e9949bb0
Kuesung Park <gueit214@naver.com>
2023-09-27 17:35:27 +0900

    test : 토큰 테스트

--- src/app/[lng]/(sub)/join/funnels/step1/Step1Component.client.tsx
+++ src/app/[lng]/(sub)/join/funnels/step1/Step1Component.client.tsx
@@ -5,12 +5,23 @@ import NoticeSection from './components/NoticeSection.client';
 import JoinTitleTextMessage from '../../components/JoinTitleTextMessage.client';
 import { useTranslation } from '@/app/i18n/client';
 import { Spacing } from '@/components/Spacing';
+import { AUTH_KEYS } from '@/constants/token';
+import { getTokenFromCookie } from '@/utils/auth/tokenController';
+import { getLocalCookie } from '@/utils/cookieController';
+import { usePathname } from 'next/navigation';
 
 export default function Step1Component() {
   const { t } = useTranslation('join');
+  const pathname = usePathname();
+  const accessToken = getLocalCookie(AUTH_KEYS.accessToken);
+  const refreshToken = getLocalCookie(AUTH_KEYS.refreshToken);
+
   return (
     <main>
       <JoinTitleTextMessage>{t('enterPhoneNumber')}</JoinTitleTextMessage>
+      <JoinTitleTextMessage>{pathname}</JoinTitleTextMessage>
+      <JoinTitleTextMessage>{accessToken}</JoinTitleTextMessage>
+      <JoinTitleTextMessage>{refreshToken}</JoinTitleTextMessage>
       <FormSection />
       <Spacing size={16} />
       <NoticeSection />
