8c60ac9bfca3f9646643d87ce91b86f20144fd15
Kuesung Park <gueit214@naver.com>
2023-10-05 14:57:51 +0900

    fix : useTransition import 경로 수정

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,15 +1,15 @@
 'use client';
+import { useTranslation } from '../i18n/client';
 import { cookieName } from '../i18n/settings';
 import { useDidMount } from '@/hooks/common/useDidMount';
 import { hasToken } from '@/utils/auth/tokenController';
 import { getLocalCookie, setLocalCookie } from '@/utils/cookieController';
 import { afterDay60 } from '@/utils/date';
 import { useRouter } from 'next/navigation';
-import { useTranslation } from 'react-i18next';
 
 export default function Home() {
   const router = useRouter();
-  const { i18n } = useTranslation();
+  const { i18n } = useTranslation('common');
 
   useDidMount(async () => {
     const cookieLanguage = getLocalCookie(cookieName);
