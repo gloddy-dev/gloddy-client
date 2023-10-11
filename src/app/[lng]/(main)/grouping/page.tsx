1c2e00783dae53c9aec1175bae911f7085b28349
kang <kangju2000@naver.com>
2023-10-12 00:14:03 +0900

    fix: 매칭 페이지 무한 로딩 이슈 수정

--- src/app/[lng]/(main)/grouping/page.tsx
+++ src/app/[lng]/(main)/grouping/page.tsx
@@ -1,13 +1,10 @@
 import CreateGroupButton from './components/CreateGroupButton.client';
 import GroupingCardList from './components/GroupingCardList.client';
 import GroupingHeader from './components/GroupingHeader';
-import { getGroups } from '@/apis/groups/apis';
-import { Keys } from '@/apis/groups/keys';
 import { RejectedFallback } from '@/components/ErrorBoundary';
 import { Footer } from '@/components/Footer';
 import { Loading } from '@/components/Loading';
 import { PageAnimation } from '@/components/PageAnimation';
-import { HydrationProvider } from '@/components/Provider';
 import { Spacing } from '@/components/Spacing';
 import { QueryAsyncBoundary } from '@suspensive/react-query';
 
@@ -26,11 +23,9 @@ export default function GroupingPage({ params: { lng } }: GroupingPageProps) {
         rejectedFallback={RejectedFallback}
         pendingFallback={<Loading className="h-[calc(100dvh-178px)]" />}
       >
-        <HydrationProvider queryKey={Keys.getGroups()} queryFn={() => getGroups(0)} isInfiniteQuery>
-          <PageAnimation>
-            <GroupingCardList />
-          </PageAnimation>
-        </HydrationProvider>
+        <PageAnimation>
+          <GroupingCardList />
+        </PageAnimation>
       </QueryAsyncBoundary>
       <CreateGroupButton />
       <Spacing size={60} />
