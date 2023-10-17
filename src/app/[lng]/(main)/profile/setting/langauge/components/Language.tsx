e64970e76db3506f96cb0cc5086a74aa6c40a0ef
Kuesung Park <gueit214@naver.com>
2023-10-17 16:31:40 +0900

    refactor : cookieName 변수명 사용

--- src/app/[lng]/(main)/profile/setting/langauge/components/Language.tsx
+++ src/app/[lng]/(main)/profile/setting/langauge/components/Language.tsx
@@ -1,23 +1,23 @@
 'use client';
 
+import { cookieName } from '@/app/i18n/settings';
 import { Button, ButtonGroup } from '@/components/Button';
 import { CircleCheckbox } from '@/components/Checkbox';
 import { Flex } from '@/components/Layout';
 import { Spacing } from '@/components/Spacing';
 import { getLocalCookie, setLocalCookie } from '@/utils/cookieController';
 import { afterDay60 } from '@/utils/date';
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function Language() {
   const { t, i18n } = useTranslation('common');
 
-  const prevLanguage = getLocalCookie('i18next');
+  const prevLanguage = getLocalCookie(cookieName);
   const [language, setLanguage] = useState(prevLanguage || 'en');
 
   const handleSubmit = () => {
-    setLocalCookie('i18next', language, {
+    setLocalCookie(cookieName, language, {
       expires: afterDay60,
     });
     i18n.changeLanguage(language);
