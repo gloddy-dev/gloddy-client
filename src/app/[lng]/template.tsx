6b5a1e3ba5469308773c9f6cdda809882221c005
Kuesung Park <gueit214@naver.com>
2023-11-02 09:34:03 +0900

    feat : 웹에서 업데이트 알림 구현

--- src/app/[lng]/template.tsx
+++ src/app/[lng]/template.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { SENTRY_DSN } from '@/constants';
+import { useCheckUpdate } from '@/hooks/useCheckUpdate';
 import useDisableScrollBounce from '@/hooks/useDisableScrollBounce';
 import { useEasterEgg } from '@/hooks/useEasterEgg';
 import { useSentry } from '@/hooks/useSentry';
@@ -12,6 +13,8 @@ export default function Step4Layout({ children }: { children: React.ReactNode })
     dsn: SENTRY_DSN,
     allowUrls: ['https://gloddy.vercel.app'],
   });
+
+  useCheckUpdate(2.1);
 
   return children;
 }
