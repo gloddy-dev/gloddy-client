50980c556ff37d7dc37657a4a0ba46e84718071a
Kuesung Park <gueit214@naver.com>
2023-11-06 21:38:53 +0900

    hotfix : safari 이슈

--- src/hooks/useBrowser.ts
+++ src/hooks/useBrowser.ts
@@ -1,13 +1,15 @@
 import { useDidMount } from './common/useDidMount';
+import { getMobileDivce } from '@/utils/getMobileDevice';
 import { useState } from 'react';
 
 export default function useBrowser() {
   const [browser, setBrowser] = useState('');
+  const mobileDevice = getMobileDivce();
 
   useDidMount(() => {
     const agent = navigator.userAgent;
     if (agent.includes('Chrome')) setBrowser('chrome');
-    if (agent.includes('Safari')) setBrowser('safari');
+    if (agent.includes('Safari') || mobileDevice === 'ios') setBrowser('safari');
   });
 
   return browser;
