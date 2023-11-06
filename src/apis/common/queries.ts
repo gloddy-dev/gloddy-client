10fee138422a0363f16b3295596343011490e9fa
Kuesung Park <gueit214@naver.com>
2023-11-07 01:10:50 +0900

    feat :  알림페이지 구현

--- src/apis/common/queries.ts
+++ src/apis/common/queries.ts
@@ -1,1 +1,6 @@
-export const useGetExample = () => {};
+import { Keys, getNotification } from '.';
+import { useSuspenseQuery } from '@suspensive/react-query';
+
+export const useGetNotifications = () => {
+  return useSuspenseQuery(Keys.getNotifications(), getNotification);
+};
