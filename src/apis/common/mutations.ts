08dc2d480383c4eb176dace6cc342c3236f21386
Kuesung Park <gueit214@naver.com>
2023-11-07 01:01:20 +0900

    feat : fcmToken API

--- src/apis/common/mutations.ts
+++ src/apis/common/mutations.ts
@@ -1,6 +1,10 @@
-import { postFiles } from './apis';
+import { postFCMToken, postFiles } from './apis';
 import { useMutation } from '@tanstack/react-query';
 
 export const usePostFiles = () => {
   return useMutation(postFiles);
+};
+
+export const usePostFCMToken = () => {
+  return useMutation(postFCMToken);
 };
