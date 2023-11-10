cb2d71d4dff2e41f26e38cd4dbbfe3e4931e0910
Kuesung Park <gueit214@naver.com>
2023-11-10 16:22:31 +0900

    feat : 수정된 DTO에 맞게 화면 구현
    날짜, 이미지 등은 서버 측에서 아직 넘겨주지 않아서 주석 처리

--- src/app/[lng]/(sub)/notification/components/NotiCard.client.tsx
+++ src/app/[lng]/(sub)/notification/components/NotiCard.client.tsx
@@ -1,9 +1,12 @@
 'use client';
 
+import { Notification, NotificationResponse } from '@/apis/notifications';
 import { useTranslation } from '@/app/i18n/client';
 import { Icon } from '@/components/Icon';
 import { Flex } from '@/components/Layout';
 import { Spacing } from '@/components/Spacing';
+import { notifications } from '@/constants/notifications';
+import useAppRouter from '@/hooks/useAppRouter';
 import cn from '@/utils/cn';
 import { format, formatDistanceToNowStrict } from 'date-fns';
 import { enUS, ko } from 'date-fns/esm/locale';
@@ -23,36 +26,44 @@ function foramtDate(locale: Locale, date: string) {
 }
 
 interface NotiCardProps {
-  notiData: {
-    imageUrl: string;
-    title: string;
-    date: string;
-    isRead?: boolean;
-  };
+  notiData: Notification;
 }
 
 export default function NotiCard({ notiData }: NotiCardProps) {
-  const { imageUrl, title, date, isRead } = notiData;
+  const { userId, redirectId, content, type } = notiData;
   const { i18n } = useTranslation('common');
+  const { push } = useAppRouter();
 
   const locale = i18n.language === 'ko' ? ko : enUS;
+
+  const handleNotiCard = () => {
+    switch (type) {
+      case 'APPLY_CREATE':
+        push(`/grouping/${redirectId}/manage`);
+        break;
+      case 'APPLY_APPROVE':
+        push(`/grouping/${redirectId}`);
+        break;
+    }
+  };
 
   return (
     <Flex
       align="center"
       className={cn('px-20 py-16', {
-        'bg-brand-color': !isRead,
+        // 'bg-brand-color': !isRead,
       })}
+      onClick={handleNotiCard}
     >
-      <div className="relative h-48 w-48 overflow-hidden rounded-8">
+      {/* <div className="relative h-48 w-48 overflow-hidden rounded-8">
         <Image src={imageUrl} alt="thumbnail" className="object-cover" fill />
-      </div>
+      </div> */}
       <Spacing direction="horizontal" size={16} />
       <div>
-        <p className="text-paragraph-2 text-sign-secondary">{title}</p>
+        <p className="text-paragraph-2 text-sign-secondary">{content}</p>
         <Flex align="center" gap={4}>
-          <Icon id="16-date_range" width={16} height={16} />
-          <p className="text-caption text-sign-tertiary">{foramtDate(locale, date)}</p>
+          {/* <Icon id="16-date_range" width={16} height={16} /> */}
+          {/* <p className="text-caption text-sign-tertiary">{foramtDate(locale, date)}</p> */}
         </Flex>
       </div>
     </Flex>
