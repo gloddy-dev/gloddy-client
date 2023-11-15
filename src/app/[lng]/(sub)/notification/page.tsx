e84e81fb2122d85da2328168b372dce7d3b1ff8b
Kuesung Park <gueit214@naver.com>
2023-11-15 14:44:43 +0900

    refactor : PageAnimation 제거

--- src/app/[lng]/(sub)/notification/page.tsx
+++ src/app/[lng]/(sub)/notification/page.tsx
@@ -6,18 +6,14 @@ import { Loading } from '@/components/Loading';
 import { HydrationProvider } from '@/components/Provider';
 import { QueryAsyncBoundary } from '@suspensive/react-query';
 
-import { PageAnimation } from '@/components/PageAnimation';
-
 export default async function NotificationPage() {
   return (
     <>
       <NotificationHeader />
       <QueryAsyncBoundary rejectedFallback={RejectedFallback} pendingFallback={<Loading />}>
-        <PageAnimation>
-          {/* <HydrationProvider queryFn={getNotification} queryKey={Keys.getNotifications()}> */}
-          <NotificationSection />
-          {/* </HydrationProvider> */}
-        </PageAnimation>
+        {/* <HydrationProvider queryFn={getNotification} queryKey={Keys.getNotifications()}> */}
+        <NotificationSection />
+        {/* </HydrationProvider> */}
       </QueryAsyncBoundary>
     </>
   );
