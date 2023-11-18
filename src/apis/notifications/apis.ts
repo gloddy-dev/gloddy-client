5375517e5de5f806f43fc23970b98e8ccebb8fc0
Kuesung Park <gueit214@naver.com>
2023-11-19 08:53:38 +0900

    feat : 기존 privateApi로 수정

--- src/apis/notifications/apis.ts
+++ src/apis/notifications/apis.ts
@@ -1,9 +1,8 @@
 import { FCMTokenRequest } from '.';
 import { NotificationResponse } from '.';
-import { privateApiNotification } from '../config/privateApiNotification';
+import privateApi from '../config/privateApi';
 
 export const postFCMToken = ({ token }: FCMTokenRequest) =>
-  privateApiNotification.post('/notifications/tokens', { token });
+  privateApi.post('/notifications/tokens', { token });
 
-export const getNotification = () =>
-  privateApiNotification.get<NotificationResponse>('/notifications');
+export const getNotification = () => privateApi.get<NotificationResponse>('/notifications');
