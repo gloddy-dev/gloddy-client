0c1b3063c018ee5a542dfe67f7502bb4b9a34b6b
Kuesung Park <gueit214@naver.com>
2023-12-05 20:25:29 +0900

    feat : 국가 구현

--- src/app/[lng]/(main)/profile/setting/edit/components/step1/CountryBotoomSheet.tsx
+++ src/app/[lng]/(main)/profile/setting/edit/components/step1/CountryBotoomSheet.tsx
@@ -1,7 +1,66 @@
+import { BottomSheet } from '@/components/Modal';
+import Image from 'next/image';
+import { useState } from 'react';
+
 interface CountryBotoomSheetProps {
   isOpen: boolean;
   onClose: () => void;
 }
+
+interface Country {
+  countryKR: string;
+  countryEN: string;
+  image: string;
+}
+
+const COUNTRY_DUMMY_DATA: Country[] = [
+  {
+    countryEN: 'Korea',
+    countryKR: '대한민국',
+    image:
+      'https://opendata.mofa.go.kr:8444/fileDownload/images/country_images/flags/36/20220318_164831540.gif',
+  },
+  {
+    countryEN: 'Korea',
+    countryKR: '대한민국',
+    image:
+      'https://opendata.mofa.go.kr:8444/fileDownload/images/country_images/flags/36/20220318_164831540.gif',
+  },
+  {
+    countryEN: 'Korea',
+    countryKR: '대한민국',
+    image:
+      'https://opendata.mofa.go.kr:8444/fileDownload/images/country_images/flags/36/20220318_164831540.gif',
+  },
+];
+
 export default function CountryBotoomSheet({ isOpen, onClose }: CountryBotoomSheetProps) {
-  return <div></div>;
+  const [country, setCountry] = useState<string>('Korea');
+
+  const handleCountry = (country: string) => {
+    setCountry(country);
+    onClose();
+  };
+  return (
+    <BottomSheet
+      onClose={onClose}
+      isOpen={isOpen}
+      snapPoints={[800, 0]}
+      isTapOutsideToClose
+      disableDrag
+    >
+      <ul>
+        {COUNTRY_DUMMY_DATA.map((country, index) => (
+          <li
+            key={index}
+            className="py-12 text-subtitle-2"
+            onClick={() => handleCountry(country.countryEN)}
+          >
+            {/* <Image src={country.image} width={24} height={24} alt={country.countryEN} /> */}
+            {country.countryEN}
+          </li>
+        ))}
+      </ul>
+    </BottomSheet>
+  );
 }
