55ba2ec64af9cbd6754a636443e0534afda1d642
Kuesung Park <gueit214@naver.com>
2023-12-14 14:29:59 +0900

    refactor : getBase64유틸 함수 수정

--- src/utils/getBase64.ts
+++ src/utils/getBase64.ts
@@ -1,21 +1,20 @@
 import fs from 'node:fs/promises';
-import { join } from 'path';
+import path from 'node:path';
 
 import { getPlaiceholder } from 'plaiceholder';
 
 const getBase64 = async (src: string) => {
-  const isExternal = src.startsWith('http');
-  let buffer: Buffer;
+  const buffer = await fs.readFile(path.join('./public', src));
 
-  if (isExternal) {
-    buffer = await fetch(src).then(async (res) => Buffer.from(await res.arrayBuffer()));
-  } else {
-    buffer = await fs.readFile(join(process.cwd(), `public${src}`));
-  }
+  const {
+    metadata: { height, width },
+    ...plaiceholder
+  } = await getPlaiceholder(buffer, { size: 10 });
 
-  const { base64 } = await getPlaiceholder(buffer);
-
-  return base64 as `data:image/${string}`;
+  return {
+    ...plaiceholder,
+    img: { src, height, width },
+  };
 };
 
 export default getBase64;
