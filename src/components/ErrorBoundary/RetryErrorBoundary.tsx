87a382bfe06016cb4cec520936dcfe10b3500627
Kuesung Park <gueit214@naver.com>
2023-12-23 14:25:12 +0900

    refactor : react-error-boundary에서 suspensive/react로 라이브러리 교체

--- src/components/ErrorBoundary/RetryErrorBoundary.tsx
+++ src/components/ErrorBoundary/RetryErrorBoundary.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorBoundary } from '@suspensive/react';
 
 export default function RetryErrorBoundary({ children }: { children: React.ReactNode }) {
   return (
     <ErrorBoundary
-      fallbackRender={({ resetErrorBoundary }) => (
+      fallback={({ reset }) => (
         <div className="flex flex-col items-center px-24 py-40">
           <p className="text-h5 text-black">문제가 발생했습니다</p>
           <p className="text-b3 text-grey-600 mt-2">페이지를 불러오는데 실패했습니다.</p>
           <button
             className="bg-grey-200 text-grey-800 text-13 mt-16 rounded-lg px-12 py-8 font-bold"
-            onClick={() => resetErrorBoundary()}
+            onClick={() => reset()}
           >
             다시 불러오기
           </button>
