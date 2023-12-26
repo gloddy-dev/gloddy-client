855a20f11f7bb6caafb78df8c7c1fd5fd90eee25
dev-dong-su <fkdlrj1230@gmail.com>
2023-12-26 14:16:22 +0900

    feat: 게시글 작성취소 확인 모달

--- src/app/[lng]/(main)/community/write/components/WriteHeader.tsx
+++ src/app/[lng]/(main)/community/write/components/WriteHeader.tsx
@@ -1,19 +1,36 @@
 'use client';
 
+import WriteModal from '@/app/[lng]/(main)/grouping/[groupId]/write/components/WriteModal';
 import { useTranslation } from '@/app/i18n/client';
 import { IconButton } from '@/components/Button';
 import { Header } from '@/components/Header';
 import { Icon } from '@/components/Icon';
 import useAppRouter from '@/hooks/useAppRouter';
+import { useModal } from '@/hooks/useModal';
 
 export default function WriteHeader() {
   const { t } = useTranslation('community');
   const { back } = useAppRouter();
+  const { open, exit } = useModal();
 
   return (
     <Header>
       <Header.Left>
-        <IconButton size="large" onClick={() => back()}>
+        <IconButton
+          size="large"
+          onClick={() =>
+            open(() => (
+              <WriteModal
+                type="cancel"
+                onCancelClick={exit}
+                onOkClick={() => {
+                  exit();
+                  back();
+                }}
+              />
+            ))
+          }
+        >
           <Icon id="24-arrow_back" />
         </IconButton>
         <p className="w-full truncate">{t('create.headerTitle')}</p>
--- src/app/[lng]/(main)/community/write/components/WriteModal.tsx
+++ src/app/[lng]/(main)/community/write/components/WriteModal.tsx
@@ -3,19 +3,44 @@ import { Icon } from '@/components/Icon';
 import { Modal } from '@/components/Modal';
 import { Spacing } from '@/components/Spacing';
 
-interface ApplyModalProps {
+import type { ComponentProps } from 'react';
+
+type ModalStyle = {
+  [key in WriteModalProps['type']]: {
+    variant: NonNullable<ComponentProps<typeof Modal>['variant']>;
+    iconId: string;
+    content: string;
+  };
+};
+
+interface WriteModalProps {
+  type: 'write' | 'cancel';
   onOkClick: () => void;
   onCancelClick: () => void;
 }
-export default function WriteModal({ onOkClick, onCancelClick }: ApplyModalProps) {
+
+export default function WriteModal({ type, onOkClick, onCancelClick }: WriteModalProps) {
   const { t } = useTranslation('community');
+  const modalStyle: ModalStyle = {
+    write: {
+      variant: 'success',
+      iconId: '48-check',
+      content: t('submit.content'),
+    },
+    cancel: {
+      variant: 'warning',
+      iconId: '48-warning',
+      content: t('cancel.content'),
+    },
+  };
+  const { variant, iconId, content } = modalStyle[type];
 
   return (
-    <Modal onOkClick={onOkClick} onCancelClick={onCancelClick} variant="success">
+    <Modal variant={variant} onCancelClick={onCancelClick} onOkClick={onOkClick}>
       <Spacing size={32} />
-      <Icon id="48-check" width={48} height={48} />
+      <Icon id={iconId} width={48} height={48} />
       <Spacing size={12} />
-      <p>{t('create.submit.content')}</p>
+      <p className="text-paragraph-1">{content}</p>
       <Spacing size={16} />
     </Modal>
   );
