b1500f409b52485244c78a6ebf86dc1b59e16552
Hee Su <fkdlrj1230@gmail.com>
2023-12-27 15:53:51 +0900

    Feature: 게시글 생성 페이지 퍼블리싱 #535 (#537)
    * feat: ListBox 제작
    
    ListBoxController를 상위로
    
    ListBox
    
    ListBoxOptions를 렌더링 하는 컴포넌트
    
    * refactor:ListBoxController의  Context API 적용
    
    ListBox의 open 상태를 변경하기 위해 props로 내려주던걸 Context API로 제어
    
    * feat: 게시글 생성 페이지 퍼블리싱
    
    * feat: 게시글 작성취소 확인 모달
    
    * refactor: 경로 축약
    
    * refactor: 사용하지 않는 import 제거
    
    * refactor: 컨벤션에 맞게 코드 수정
    
    * feat: ImageSection을 공통 컴포넌트로 분리
    
    * refactor: interface 이름 수정
    
    * refactor: 타입 수정
    
    * fix: modal i18n 수정

---
+++ src/components/ListBox/ListBoxOptions.tsx
@@ -0,0 +1,29 @@
+import React, { Dispatch, SetStateAction } from 'react';
+
+import { useListBoxContext } from '@/components/ListBox/ListBoxController';
+
+interface ListBoxOptionsProps {
+  options: string[];
+  onSelect: (value: string) => void;
+}
+
+export default function ListBoxOptions({ options, onSelect }: ListBoxOptionsProps) {
+  const { setOpen } = useListBoxContext();
+
+  return (
+    <div className="rounded-8 border-1 border-primary bg-white">
+      {options.map((option) => (
+        <div
+          key={option}
+          onClick={() => {
+            onSelect(option);
+            setOpen(false);
+          }}
+          className={'p-16'}
+        >
+          {option}
+        </div>
+      ))}
+    </div>
+  );
+}
