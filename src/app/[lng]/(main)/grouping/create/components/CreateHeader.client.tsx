94c7f6f90a7b4cb0f3a099a455167a5bbcc196c8
dev-dong-su <fkdlrj1230@gmail.com>
2024-01-14 17:30:03 +0900

    fix: 모임 생성 페이지 X button app router로 수정

--- src/app/[lng]/(main)/grouping/create/components/CreateHeader.client.tsx
+++ src/app/[lng]/(main)/grouping/create/components/CreateHeader.client.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from '@/app/i18n/client';
 import { IconButton } from '@/components/Button';
 import { Header } from '@/components/Header';
 import { Icon } from '@/components/Icon';
-import { useRouter } from 'next/navigation';
+import useAppRouter from '@/hooks/useAppRouter';
 
 interface CreateHeaderProps {
   currentStep: 'main' | 'meetDate';
@@ -10,12 +10,12 @@ interface CreateHeaderProps {
 
 export default function CreateHeader({ currentStep }: CreateHeaderProps) {
   const { t } = useTranslation('grouping');
-  const { back } = useRouter();
+  const { back } = useAppRouter();
 
   return (
     <Header className="px-4">
       <Header.Left>
-        <IconButton size="large" onClick={back}>
+        <IconButton size="large" onClick={() => back()}>
           <Icon id="24-close" />
         </IconButton>
         {currentStep === 'main' && <p>{t('create.headerTitle')}</p>}
