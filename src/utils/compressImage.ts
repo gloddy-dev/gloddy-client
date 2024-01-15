d9b70394cc4753ab423c2cd95a26cc63674e4c29
Kuesung Park <gueit214@naver.com>
2024-01-15 14:58:58 +0900

    feat : 이미지 압축 함수

---
+++ src/utils/compressImage.ts
@@ -0,0 +1,11 @@
+import imageCompression from 'browser-image-compression';
+
+export const compressImage = async (file: File) => {
+  const options = {
+    maxSizeMB: 0.2,
+    maxWidthOrHeight: 1920,
+    useWebWorker: true,
+  };
+  const BlobImage = await imageCompression(file, options);
+  return new File([BlobImage], file.name, { type: file.type });
+};
