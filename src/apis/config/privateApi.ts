087598134b2ac1dd1ce4261055818ec33400f00f
Kuesung Park <gueit214@naver.com>
2024-01-18 13:29:56 +0900

    Fix : api 무한 요청 (#574)
    * feat: 회원가입 시 국가 설정 추가 (#563)
    
    * fix : 토큰 재발급 로직 제거

--- src/apis/config/privateApi.ts
+++ src/apis/config/privateApi.ts
@@ -47,54 +47,9 @@ privateApi.interceptors.response.use(
       ) {
         window.location.href = '/join?step=1';
       }
-      /* 토큰 재발급이 필요한 경우 */
-      if (error.response.status === AUTH_ERROR_CODES.TOKEN_ERROR) {
-        try {
-          const { refreshToken, accessToken } = await getTokenFromCookie();
-          if (!refreshToken || !accessToken) {
-            throw new ApiError(
-              '에러 발생',
-              '토큰이 없습니다.',
-              AUTH_ERROR_CODES.UNAUTHORIZED,
-              new Date()
-            );
-          }
-
-          const {
-            token: { accessToken: reIssuedAccessToken, refreshToken: reIssuedRefreshToken },
-          } = await postReissue(
-            { accessToken, refreshToken },
-            { headers: { 'X-AUTH-TOKEN': accessToken } }
-          );
-
-          if (!reIssuedAccessToken || !reIssuedRefreshToken) {
-            throw new ApiError(
-              '에러 발생',
-              'accessToken 발급 중 오류가 발생했습니다.',
-              AUTH_ERROR_CODES.UNAUTHORIZED,
-              new Date()
-            );
-          }
-
-          const prevRequest = error.config;
-          if (!prevRequest) {
-            throw new ApiError(
-              '에러 발생',
-              '이전 정보가 없습니다.',
-              AUTH_ERROR_CODES.UNAUTHORIZED,
-              new Date()
-            );
-          }
-
-          prevRequest.headers['X-AUTH-TOKEN'] = reIssuedAccessToken;
-          return privateApi(prevRequest);
-        } catch (e) {
-          return Promise.reject(e);
-        }
-      }
       return Promise.reject(error.response.data.reason);
-    } catch (e) {
-      return Promise.reject(e);
+    } catch (error) {
+      return Promise.reject(error);
     }
   }
 );
