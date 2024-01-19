d3a59a6dfb71f30789ae9638156a553a297fa116
Kuesung Park <gueit214@naver.com>
2024-01-19 12:02:26 +0900

    Feature : 에러 핸들링 (#576)
    * feat : BaseError 컴포넌트 구현
    
    * feat : 로그아웃 버튼 추가

--- src/utils/auth/tokenController.ts
+++ src/utils/auth/tokenController.ts
@@ -61,3 +61,7 @@ export const hasToken = () => {
 
   return accessToken || refreshToken;
 };
+
+export const removeToken = () => {
+  setTokenAtCookie({ accessToken: '', refreshToken: '', userId: 0 });
+};
