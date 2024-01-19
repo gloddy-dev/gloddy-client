3fc20712fd897d2b2e555f0652c02e323f20d4a3
Kuesung Park <gueit214@naver.com>
2024-01-19 12:02:26 +0900

    Feature : 에러 핸들링 (#576)
    * feat : BaseError 컴포넌트 구현
    
    * feat : 로그아웃 버튼 추가

--- src/app/[lng]/(main)/error.tsx
+++ src/app/[lng]/(main)/error.tsx
@@ -1,9 +1,15 @@
 'use client';
 
 import BaseError, { BaseErrorProps } from '@/components/Error/BaseError';
+import { Footer } from '@/components/Footer';
 
 interface ErrorProps extends BaseErrorProps {}
 
 export default function Error({ error, reset }: ErrorProps) {
-  return <BaseError error={error} reset={reset} />;
+  return (
+    <>
+      <BaseError error={error} reset={reset} />
+      <Footer lng="ko" />
+    </>
+  );
 }
