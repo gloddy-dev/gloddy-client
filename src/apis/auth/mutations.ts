8707a22e6e9e4d26cbfa55792a69d2a4590fd4aa
Kuesung Park <gueit214@naver.com>
2024-01-20 10:11:23 +0900

    feat : auth 마이그레이션

--- src/apis/auth/mutations.ts
+++ src/apis/auth/mutations.ts
@@ -14,22 +14,20 @@ import { useMutation } from '@tanstack/react-query';
 
 export const useLoginMutation = () => useMutation({ mutationFn: postLogin });
 
-export const useReissueMutation = () => useMutation(postReissue);
+export const useReissueMutation = () => useMutation({ mutationFn: postReissue });
 
-export const useSMSMutation = () => useMutation(postSMS);
+export const useSMSMutation = () => useMutation({ mutationFn: postSMS });
 
-export const useSMSVerifyMutation = () =>
-  useMutation(postSMSVerify, {
-    onError: () => {},
-  });
+export const useSMSVerifyMutation = () => useMutation({ mutationFn: postSMSVerify });
 
-export const useEmailMutation = () => useMutation(postEmail);
+export const useEmailMutation = () => useMutation({ mutationFn: postEmail });
 
-export const useEmailVerifyMutation = () => useMutation(postEmailVerify);
+export const useEmailVerifyMutation = () => useMutation({ mutationFn: postEmailVerify });
 
 export const useSignUpMutation = () => {
   const { push } = useAppRouter();
-  return useMutation(postSignUp, {
+  return useMutation({
+    mutationFn: postSignUp,
     onSuccess: (data: SignUpResponse) => {
       const {
         userId,
