515edebe4528991fe417cbc7cd524f7f461a87dc
dev-dong-su <fkdlrj1230@gmail.com>
2024-01-22 21:36:10 +0900

    feat: 게시물 삭제 api 연동

--- src/app/[lng]/(main)/community/[articleId]/components/ArticleDetailHeader.tsx
+++ src/app/[lng]/(main)/community/[articleId]/components/ArticleDetailHeader.tsx
@@ -2,19 +2,19 @@
 
 import { Suspense } from 'react';
 
-import { useGetCommunityArticleDetail } from '@/apis/community';
+import { useGetCommunityArticleDetail, usePostDeleteCommunityArticle } from '@/apis/community';
 import { useTranslation } from '@/app/i18n/client';
 import { IconButton } from '@/components/Button';
+import { DropDown } from '@/components/DropDown';
+import { DropDownOptionType } from '@/components/DropDown/DropDown';
 import { Header } from '@/components/Header';
 import { Icon } from '@/components/Icon';
 import useAppRouter from '@/hooks/useAppRouter';
+import { useNumberParams } from '@/hooks/useNumberParams';
 
-interface ArticleDetailHeaderProps {
-  articleId: number;
-}
-
-export default function ArticleDetailHeader({ articleId }: ArticleDetailHeaderProps) {
+export default function ArticleDetailHeader() {
   const { back } = useAppRouter();
+  const { articleId } = useNumberParams<['articleId']>();
   const { t } = useTranslation('community');
   const { data: articleData } = useGetCommunityArticleDetail(articleId);
 
@@ -31,18 +31,45 @@ export default function ArticleDetailHeader({ articleId }: ArticleDetailHeaderPr
         </Suspense>
       </Header.Left>
       <Header.Right>
-        <Suspense>
-          <IconButtonAction />
-        </Suspense>
+        <IconButtonAction />
       </Header.Right>
     </Header>
   );
 }
 
 function IconButtonAction() {
+  const { articleId } = useNumberParams<['articleId']>();
+  const { data: articleData } = useGetCommunityArticleDetail(articleId);
+  const { mutate: mutateDelete } = usePostDeleteCommunityArticle(articleId);
+
+  const handleBlockArticle = () => {
+    console.log('block');
+  };
+
+  const handleDeleteArticle = () => {
+    mutateDelete();
+  };
+
+  const options: DropDownOptionType[] = [
+    {
+      name: '게시물 차단',
+      onClick: handleBlockArticle,
+    },
+    ...(articleData.data.article.isWriter
+      ? [
+          {
+            name: '게시물 삭제',
+            onClick: handleDeleteArticle,
+          },
+        ]
+      : []),
+  ];
+
   return (
-    <IconButton size="large">
-      <Icon id="24-more" />
-    </IconButton>
+    <DropDown options={options}>
+      <IconButton size="large">
+        <Icon id="24-more" />
+      </IconButton>
+    </DropDown>
   );
 }
