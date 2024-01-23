4faaf34dfc836b8275006cb71079206cb8089cb2
Hee Su <fkdlrj1230@gmail.com>
2024-01-23 23:02:19 +0900

    Feature: community api 연결 (댓글 제외) (#587)
    * feat: api 스팩에 맞춰 수정, 기획안과 차이나는 부분 수정
    
    * feat: community 게시글 생성 api 연결
    
    * feat: api 스팩에 맞춰 수정, 기획안과 차이나는 부분 수정
    
    * feat: community 게시글 생성 api 연결
    
    * feat: 모든 카테고리 조회 페이지 추가
    
    * feat: kpop, qna, lang 페이지 게시글 조회 추가
    
    * fix: 무한 스크롤 안되는 버그 수정
    
    * fix: 상세페이지 api 연결
    
    * fix: page route app router로 변경
    
    * feat : middleware에 community 추가
    
    * feat : 페이지단에서 params 넘겨주기
    
    * feat: 게시글 좋아요 api 연결
    
    * feat: DropDown 컴포넌트 추가
    
    * feat: 게시물 삭제 api 연동
    
    * feat: 생성, 삭제, 좋아요 행위에 조회 쿼리를 모두 reset
    
    * feat: footer community tab 주석 해제
    
    * feat: 상세 페이지 더보기 버튼 다국어 적용
    
    * feat: api 스팩에 맞춰 수정, 기획안과 차이나는 부분 수정
    
    * feat: community 게시글 생성 api 연결
    
    * feat: 모든 카테고리 조회 페이지 추가
    
    * feat: kpop, qna, lang 페이지 게시글 조회 추가
    
    * fix: 무한 스크롤 안되는 버그 수정
    
    * fix: 상세페이지 api 연결
    
    * fix: page route app router로 변경
    
    * feat : middleware에 community 추가
    
    * feat : 페이지단에서 params 넘겨주기
    
    * feat: 게시글 좋아요 api 연결
    
    * feat: DropDown 컴포넌트 추가
    
    * feat: 게시물 삭제 api 연동
    
    * feat: 생성, 삭제, 좋아요 행위에 조회 쿼리를 모두 reset
    
    * feat: footer community tab 주석 해제
    
    * feat: 상세 페이지 더보기 버튼 다국어 적용
    
    * fix: 컨벤션 오류 수정
    
    * fix: 코드리뷰 수정사항 반영, 좋아요 낙관적 업데이트
    
    * fix : 인자로 category id 넘김
    
    * feat: Community 페이지 Hydration적용 으로 성능 개선
    
    ---------
    
    Co-authored-by: Kuesung Park <gueit214@naver.com>

--- src/app/[lng]/(main)/community/[articleId]/components/ArticleItem.tsx
+++ src/app/[lng]/(main)/community/[articleId]/components/ArticleItem.tsx
@@ -1,9 +1,12 @@
-import { CommunityArticle } from '@/apis/groups';
+import { CommunityArticle, usePostCommunityArticleLike } from '@/apis/community';
+import { useTranslation } from '@/app/i18n/client';
 import { CardHeader } from '@/components/Card';
+import { Icon } from '@/components/Icon';
 import { Flex } from '@/components/Layout';
 import { ImageModal } from '@/components/Modal';
 import { Spacing } from '@/components/Spacing';
 import { useModal } from '@/hooks/useModal';
+import cn from '@/utils/cn';
 import Image from 'next/image';
 
 interface ArticleItemProps {
@@ -11,15 +14,55 @@ interface ArticleItemProps {
 }
 
 export default function ArticleItem({ article }: ArticleItemProps) {
+  const { t } = useTranslation('community');
   const { open, exit } = useModal();
-  const { title, content, images } = article;
+
+  const {
+    title,
+    content,
+    images,
+    category,
+    id: articleId,
+    userId,
+    thumbnail,
+    isLiked,
+    createdAt,
+    isWriter,
+    likeCount,
+    commentCount,
+  } = article.article;
+
+  const {
+    nickName,
+    countryName,
+    id: writerId,
+    countryImage,
+    profileImage,
+    isCertifiedStudent,
+    reliabilityLevel,
+  } = article.writer;
+
+  const { mutate: mutateLike } = usePostCommunityArticleLike(articleId, category.id);
+
+  const handleLikeClick = () => {
+    mutateLike();
+  };
 
   return (
     <div className="mx-20 mb-24 mt-16 px-4">
-      <CardHeader showMoreIcon={false} {...article} />
+      <CardHeader
+        showMoreIcon={false}
+        name={nickName}
+        userId={writerId}
+        userImageUrl={profileImage}
+        isWriterCertifiedStudent={isCertifiedStudent}
+        writerReliabilityLevel={reliabilityLevel}
+        isWriterCaptain={isWriter}
+        date={createdAt}
+      />
       <Spacing size={16} />
       <div className={'text-2xl font-semibold'}>{title}</div>
-      <Spacing size={4} />
+      <Spacing size={6} />
       <div className="break-words text-paragraph-1 text-sign-primary">{content}</div>
       {images.length > 0 && (
         <Flex className="my-16 h-160 gap-4 overflow-x-scroll">
@@ -36,6 +79,18 @@ export default function ArticleItem({ article }: ArticleItemProps) {
           ))}
         </Flex>
       )}
+      <Spacing size={6} />
+      <Flex align="center" className="gap-4" onClick={handleLikeClick}>
+        <Icon
+          id="16-favorite_fill"
+          className={cn(isLiked ? 'text-warning' : 'text-sign-caption')}
+          width={27}
+          height={27}
+        />
+        <p className={cn(isLiked ? 'text-warning' : 'text-sign-caption') + ' text-subtitle-3'}>
+          {likeCount.toString().padStart(2, '0') + t('detail.likeCount')}
+        </p>
+      </Flex>
     </div>
   );
 }
