d90e4cc82888ea82613327a015986d033a0a3c21
dev-dong-su <fkdlrj1230@gmail.com>
2024-01-23 16:26:47 +0900

    feat: Community 페이지 Hydration적용 으로 성능 개선

--- src/app/[lng]/(main)/community/components/ContentSection.client.tsx
+++ src/app/[lng]/(main)/community/components/ContentSection.client.tsx
@@ -1,20 +1,15 @@
 'use client';
 
-import { Suspense } from 'react';
-
 import AllContent from '@/app/[lng]/(main)/community/components/AllContent.client';
 import CreateArticleButton from '@/app/[lng]/(main)/community/components/CreateArticleButton';
 import KpopContent from '@/app/[lng]/(main)/community/components/KpopContent';
 import LanguageContent from '@/app/[lng]/(main)/community/components/LanguageContent.client';
 import QuestionContent from '@/app/[lng]/(main)/community/components/QuestionContent.client';
 import { useTranslation } from '@/app/i18n/client';
-import { Loading } from '@/components/Loading';
 import { Tabs } from '@/components/Tabs';
-import useAppRouter from '@/hooks/useAppRouter';
 
 export default function ContentSection() {
   const { t } = useTranslation('community');
-  const { push } = useAppRouter();
 
   return (
     <>
@@ -26,24 +21,16 @@ export default function ContentSection() {
           <Tabs.Tab text={t('category.Language')} value="language" />
         </Tabs.List>
         <Tabs.Panel value="all">
-          <Suspense fallback={<Loading />}>
-            <AllContent />
-          </Suspense>
+          <AllContent />
         </Tabs.Panel>
         <Tabs.Panel value="kpop">
-          <Suspense fallback={<Loading />}>
-            <KpopContent />
-          </Suspense>
+          <KpopContent />
         </Tabs.Panel>
         <Tabs.Panel value="question">
-          <Suspense fallback={<Loading />}>
-            <QuestionContent />
-          </Suspense>
+          <QuestionContent />
         </Tabs.Panel>
         <Tabs.Panel value="language">
-          <Suspense fallback={<Loading />}>
-            <LanguageContent />
-          </Suspense>
+          <LanguageContent />
         </Tabs.Panel>
       </Tabs>
       <CreateArticleButton />
--- src/app/[lng]/(main)/community/page.tsx
+++ src/app/[lng]/(main)/community/page.tsx
@@ -1,6 +1,11 @@
+import { Suspense } from 'react';
+
 import CommunityHeader from './components/CommunityHeader';
 import ContentSection from './components/ContentSection.client';
+import { Keys, getCommunityArticles } from '@/apis/community';
 import { Footer } from '@/components/Footer';
+import { Loading } from '@/components/Loading';
+import { HydrationProvider } from '@/components/Provider';
 import { Spacing } from '@/components/Spacing';
 
 interface CommunityPageProps {
@@ -13,7 +18,25 @@ export default function CommunityPage({ params: { lng } }: CommunityPageProps) {
   return (
     <>
       <CommunityHeader lng={lng} />
-      <ContentSection />
+      <Suspense fallback={<Loading />}>
+        <HydrationProvider
+          queryMultipleFn={[
+            () => getCommunityArticles({ categoryId: 0, pageParam: 0 }),
+            () => getCommunityArticles({ categoryId: 1, pageParam: 0 }),
+            () => getCommunityArticles({ categoryId: 2, pageParam: 0 }),
+            () => getCommunityArticles({ categoryId: 3, pageParam: 0 }),
+          ]}
+          queryMultipleKey={[
+            Keys.getCommunityArticles(0),
+            Keys.getCommunityArticles(1),
+            Keys.getCommunityArticles(2),
+            Keys.getCommunityArticles(3),
+          ]}
+          isInfiniteQuery
+        >
+          <ContentSection />
+        </HydrationProvider>
+      </Suspense>
       <Spacing size={60} />
       <Footer page="community" lng={lng} />
     </>
