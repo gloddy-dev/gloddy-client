5a46d1b34526f00e0c82bfdddb2a0b9be083bea6
Hee Su <fkdlrj1230@gmail.com>
2024-01-26 13:53:40 +0900

    Feature : 커뮤니티 댓글 기능 추가 (#595)
    * feat: Community 게시글 댓글 조회 API 연동
    
    * feat: 댓글 작성 API 연동
    
    * feat: 댓글 좋아요 API 연동
    
    * feat: 댓글 삭제 API 연동
    
    * feat: CardHeader가 children을 선택적으로 받을 수 있도록 수정
    
    * feat: 대댓글(Reply) 생성 API 연결
    
    * feat: 대댓글(Reply) 조회 API 연결
    
    * feat: 생성 날짜 포멧 변경
    
    * feat: Avatar에서 국기 이미지를 보여주도록 수정
    
    * feat: 글자 튀어나감 방지
    
    * feat: CommentForm i18n 적용
    
    * refactor: CommentProvider 이름 변경
    
    * refactor: ArticleDetail commentsList -> commentList 수정
    
    * refactor: 경로 수정, button -> submit, form -> onSubmit 수정
    
    * refactor: ArticleDetail CommentSection -> CommentProvider
    
    * refactor: CommentList pb-102 -> Spacing 컴포넌트로 수정
    
    * refactor: className + 제거 cn 으로 수정
    
    * refactor: children && 연산 제거
    
    * refactor: CommentForm input -> textarea 수정
    
    * refactor: CardHeader children -> PropsWithChildren수정
    
    ---------
    
    Co-authored-by: Kuesung Park <gueit214@naver.com>

---
+++ src/app/[lng]/(main)/community/[articleId]/components/ReplyList.tsx
@@ -0,0 +1,19 @@
+import { Reply } from '@/apis/community';
+import ReplyItem from '@/app/[lng]/(main)/community/[articleId]/components/ReplyItem';
+import { ItemList } from '@/components/List';
+
+interface ReplyListProps {
+  replyList: Reply[];
+  articleWriterId: number;
+}
+
+export default function ReplyList({ replyList, articleWriterId }: ReplyListProps) {
+  return (
+    <ItemList
+      data={replyList}
+      renderItem={(reply: Reply) => <ReplyItem reply={reply} articleWriterId={articleWriterId} />}
+      renderEmpty={() => <div></div>}
+      className={'w-full'}
+    />
+  );
+}
--- src/app/[lng]/(main)/grouping/[groupId]/articles/[articleId]/components/ArticleDetail.client.tsx
+++ src/app/[lng]/(main)/grouping/[groupId]/articles/[articleId]/components/ArticleDetail.client.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useGetArticle, useGetComments, useGetGroupDetail } from '@/apis/groups';
-import CommentList from '@/app/[lng]/(main)/community/[articleId]/components/CommentList';
+import CommentList from './CommentList.client';
+import { useGetArticle, useGetGroupDetail } from '@/apis/groups';
 import ArticleItem from '@/app/[lng]/(main)/grouping/components/ArticleItem.client';
 import { useTranslation } from '@/app/i18n/client';
 import { Divider } from '@/components/Divider';
@@ -13,7 +13,6 @@ export default function ArticleDetail() {
   const { articleId, groupId } = useNumberParams<['articleId', 'groupId']>();
   const { data: groupDetailData } = useGetGroupDetail(groupId);
   const { data: articleData } = useGetArticle(groupId, articleId);
-  const { data: commentData } = useGetComments(groupId, articleId);
 
   const { isCaptain } = groupDetailData;
   const { commentCount } = articleData;
@@ -31,7 +30,7 @@ export default function ArticleDetail() {
       <p className="px-24">{t('board.commentCount', { commentCount })}</p>
       <Spacing size={8} />
       <Divider thickness="thin" />
-      <CommentList commentList={commentData.comments} />
+      <CommentList />
     </>
   );
 }
