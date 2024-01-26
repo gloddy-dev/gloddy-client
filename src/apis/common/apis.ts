ae71d65b120e790dff093b60911d60df7a2cfe68
Kuesung Park <gueit214@naver.com>
2024-01-26 18:54:53 +0900

    Hotfix : 이미지 업로드 이슈 해결 (#598)

--- src/apis/common/apis.ts
+++ src/apis/common/apis.ts
@@ -1,14 +1,10 @@
 import publicApi from '../config/publicApi';
-import { compressImage } from '@/utils/compressImage';
 
 import type { FilesRequest, FilesResponse } from '.';
 
 export const postFiles = async ({ fileList }: FilesRequest) => {
   const formData = new FormData();
-
-  const compressedFileList = await Promise.all(fileList.map((file) => compressImage(file)));
-
-  compressedFileList.forEach((file) => {
+  fileList.forEach((file) => {
     formData.append('fileList', file);
   });
 
