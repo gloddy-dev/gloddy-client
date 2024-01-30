eaa72d8055eeed36414e1afe175c5da0542b0201
Kuesung Park <gueit214@naver.com>
2024-01-30 16:35:01 +0900

    feat : Footer Lazy Loading

--- src/app/[lng]/(main)/profile/page.tsx
+++ src/app/[lng]/(main)/profile/page.tsx
@@ -2,16 +2,10 @@ import ProfileDetail from './components/ProfileDetail.client';
 import ProfileHeader from './components/ProfileHeader.client';
 import { Keys, getProfile } from '@/apis/profile';
 import { LocalSuspenseErrorBoundary } from '@/components/ErrorBoundary';
-import { Footer } from '@/components/Footer';
-import { Loading } from '@/components/Loading';
 import { HydrationProvider } from '@/components/Provider';
-import { HydrationProvider } from '@/components/Provider';
-import { Spacing } from '@/components/Spacing';
 import dynamic from 'next/dynamic';
-import { Suspense } from 'react';
 
 const Footer = dynamic(() => import('@/components/Footer/Footer'), { ssr: false });
-
 interface ProfilePageProps {
   params: {
     lng: string;
