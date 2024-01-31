337fa09cb57801fb53828430623292730bdafba4
Kuesung Park <gueit214@naver.com>
2024-01-31 14:46:27 +0900

    Feature : 모노레포 적용 (#606)
    * feat : packages/web으로 폴더 묶기
    
    * feat : 루트 workspace 관리
    
    * feat : gitignore 추가
    
    * feat : 의존성 모듈 설치
    
    * refactor : .github폴더 루트로 이동
    
    * fix : 배포 경로 수정
    
    * refactor : husky 루트로 이동
    
    * feat : packages/app 추가
    
    * refactor : 앱 name 수정
    
    * refactor : 형식 변환
    
    * chore : yarn 버전 수정 및 nohoist
    
    * feat : node-modules 사용할 수 있도록 설정
    
    * feat : yarn ignore 설정
    
    * feat : 앱 패키지 설치
    
    * feat : 모듈 설치
    
    * refactor : yarn 버전 수정
    
    * feat : yarn 버전 3.6.0 통일
    
    * refactor : web의 .yarnrc 파일 제거

---
+++ packages/app/src/hooks/useDidMount.ts
@@ -0,0 +1,12 @@
+import {useEffect, useRef} from 'react';
+
+export const useDidMount = (callback: () => void) => {
+  const mounted = useRef(false);
+  useEffect(() => {
+    mounted.current = true;
+    callback();
+    return () => {
+      mounted.current = false;
+    };
+  }, []);
+};
