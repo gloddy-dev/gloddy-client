7633bb2aad4f42b7a9f3240b63a722b3c3b72747
dev-dong-su <fkdlrj1230@gmail.com>
2024-02-12 21:01:22 +0900

    fix: 온보딩 수정, 웹뷰 주석 제거

--- packages/app/src/components/WebViewContainer.tsx
+++ packages/app/src/components/WebViewContainer.tsx
@@ -83,16 +83,6 @@ export default function WebViewContainer() {
     return true;
   };
 
-  // const handleOnLoad = async () => {
-  //   const accessToken = await AsyncStorage.getItem('accessToken');
-  //   const refreshToken = await AsyncStorage.getItem('refreshToken');
-  //   const userId = await AsyncStorage.getItem('userId');
-  //   sendMessageToWebview(webViewRef, {
-  //     type: 'TOKEN',
-  //     data: { accessToken, refreshToken, userId },
-  //   });
-  // };
-
   /* 페이지 이동 */
   const requestOnMessage = async (event) => {
     const nativeEvent = JSON.parse(event.nativeEvent.data);
@@ -160,13 +150,6 @@ export default function WebViewContainer() {
         }
         break;
       }
-      // case 'TOKEN': {
-      //   const { accessToken, refreshToken, userId } = data;
-      //   await AsyncStorage.setItem('accessToken', accessToken);
-      //   await AsyncStorage.setItem('refreshToken', refreshToken);
-      //   await AsyncStorage.setItem('userId', `${userId}`);
-      //   break;
-      // }
     }
   };
 
@@ -205,7 +188,6 @@ export default function WebViewContainer() {
         bounces={false}
         onError={onWebViewError}
         webviewDebuggingEnabled
-        // onLoad={handleOnLoad}
       />
     </SafeAreaView>
   );
@@ -214,14 +196,8 @@ export default function WebViewContainer() {
 const useAppError = () => {
   const [isError, setIsError] = useState(false);
 
-  const onWebViewError = (syntheticEvent) => {
-    const { nativeEvent } = syntheticEvent;
-    console.error('WebView error: ', nativeEvent);
-
-    // 사용자에게 오류 메시지를 표시
-    Alert.alert('Error', 'An error occurred while loading the page.');
-
-    // setIsError(true); // 오류 상태 업데이트
+  const onWebViewError = () => {
+    setIsError(true);
   };
 
   return { isError, setIsError, onWebViewError };
