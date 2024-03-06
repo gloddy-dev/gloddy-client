320f4f062a743cf041be47dc915ef5f4a1f35ae8
dev-dong-su <fkdlrj1230@gmail.com>
2024-03-06 15:44:09 +0900

    fix: hydration error

--- packages/web/src/app/[lng]/(main)/community/components/AllContent.client.tsx
+++ packages/web/src/app/[lng]/(main)/community/components/AllContent.client.tsx
@@ -7,17 +7,17 @@ import { useGetCommunityArticles } from '@/apis/community/queries';
 import ArticleItem from '@/app/[lng]/(main)/community/components/ArticleItem.client';
 import Empty from '@/app/[lng]/(main)/community/components/Empty';
 import { ItemList } from '@/components/List';
-import { Loading } from '@/components/Loading';
 import { useBlockStore } from '@/store/useBlockStore';
 
 export default function AllContent() {
   const { ref, inView } = useInView();
   const { blockCommunityArticleIds } = useBlockStore();
-  const { data: articleList, fetchNextPage, isFetching, hasNextPage } = useGetCommunityArticles(0);
+  const { data: articleList, fetchNextPage, hasNextPage } = useGetCommunityArticles(0);
 
   useEffect(() => {
     if (inView && hasNextPage) fetchNextPage();
   }, [inView, fetchNextPage]);
+
   return (
     <>
       <ItemList
@@ -32,7 +32,6 @@ export default function AllContent() {
         renderEmpty={() => <Empty />}
       />
       <div ref={ref} />
-      {isFetching && <Loading className="h-10" />}
     </>
   );
 }
