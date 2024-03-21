9eee4af2c7447605ec8b2ca2630a0e1a2cff11e0
dev-dong-su <fkdlrj1230@gmail.com>
2024-03-21 17:14:09 +0900

    refactor: swiper -> react-glider로 변경

--- packages/web/src/app/[lng]/(main)/grouping/create/components/LocationBottomSheet.client.tsx
+++ packages/web/src/app/[lng]/(main)/grouping/create/components/LocationBottomSheet.client.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useParams } from 'next/navigation';
 import { useState } from 'react';
 import usePlacesService from 'react-google-autocomplete/lib/usePlacesAutocompleteService';
